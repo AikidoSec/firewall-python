9ae0d0f669c03003ff5fe0934aa7d8df2c53f75f
Wout Feys <me@woutfeys.be>
2024-07-09 16:34:12 +0200

    Add connection to database (Sample app should be done now)

--- sample-apps/flask-mysql/app.py
+++ sample-apps/flask-mysql/app.py
@@ -1,22 +1,34 @@
 from flask import Flask, render_template
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+app.config['MYSQL_DATABASE_USER'] = 'user'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+app.config['MYSQL_DATABASE_DB'] = 'db'
+mysql.init_app(app)
 
 @app.route("/")
 def homepage():
-    dogs = [
-        {'id': 1, 'dog_name': 'Buddy'},
-        {'id': 2, 'dog_name': 'Max'},
-        {'id': 3, 'dog_name': 'Charlie'}
-    ]
+    cursor = mysql.get_db().cursor()
+    cursor.execute("SELECT * FROM db.dogs")
+    dogs = cursor.fetchall()
     return render_template('index.html', title='Homepage', dogs=dogs)
 
 
 @app.route('/dogpage/<int:dog_id>')
 def get_dogpage(dog_id):
-    dog = {'id': 3, 'dog_name': 'Charlie'}
-    return render_template('dogpage.html', title=f'Dog | {dog["dog_name"]}', dog=dog)
+    cursor = mysql.get_db().cursor()
+    cursor.execute("SELECT * FROM db.dogs")
+    dog = cursor.fetchmany(1)[0]
+    return render_template('dogpage.html', title=f'Dog', dog=dog)
 
 @app.route('/create/<dog_name>')
 def create_dog(dog_name):
-    return f'Dog {escape(dog_name)}'
+    connection = mysql.get_db()
+    cursor = connection.cursor()
+    cursor.execute(''' INSERT INTO dogs (dog_name) VALUES(%s)''',(dog_name))
+    connection.commit()
+    return f'Dog {(dog_name)}'
