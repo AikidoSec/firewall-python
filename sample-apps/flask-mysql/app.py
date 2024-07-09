c75c167e25fe5e067b5dc4d020a43c239a3936da
Wout Feys <me@woutfeys.be>
2024-07-09 15:52:11 +0200

    Provide some templates and same routes as django sample app

--- sample-apps/flask-mysql/app.py
+++ sample-apps/flask-mysql/app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def homepage():
+    dogs = [
+        {'id': 1, 'dog_name': 'Buddy'},
+        {'id': 2, 'dog_name': 'Max'},
+        {'id': 3, 'dog_name': 'Charlie'}
+    ]
+    return render_template('index.html', title='Homepage', dogs=dogs)
+
+
+@app.route('/dogpage/<int:dog_id>')
+def get_dogpage(dog_id):
+    dog = {'id': 3, 'dog_name': 'Charlie'}
+    return render_template('dogpage.html', title=f'Dog | {dog["dog_name"]}', dog=dog)
+
+@app.route('/create/<dog_name>')
+def create_dog(dog_name):
+    return f'Dog {escape(dog_name)}'
