641da8f32b1bff18200f8db8bcd6601be4ec6f60
Wout Feys <me@woutfeys.be>
2024-07-09 15:07:44 +0200

    Add data, display list of dogs, and add option to create

--- sample-apps/django-mysql/sample_app/views.py
+++ sample-apps/django-mysql/sample_app/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import loader
+from .models import Dogs
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    dogs = Dogs.objects.all()
+    print(dogs)
+    template = loader.get_template("app/index.html")
+    context = {
+        "dogs": dogs,
+    }
+    return HttpResponse(template.render(context, request))
+
 
 def dog_page(request, dog_id):
-    return HttpResponse("Your dog with id %s is lovely." % dog_id)
+    dog = get_object_or_404(Dogs, pk=dog_id)
+    return HttpResponse("Your dog, %s, is lovely." % dog.dog_name)
 
 def create_dogpage(request, dog_name):
-    return HttpResponse("Dog page created with name %s" % dog_name)
+    dog = Dogs(dog_name=dog_name, dog_boss="Unset")
+    dog.save()
+    return HttpResponse("Dog page created with id %s" % dog.id)
