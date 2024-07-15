6585511f3c6196359ec320e55c6033d541260691
Wout Feys <me@woutfeys.be>
2024-07-15 10:08:15 +0200

    Show boss name on page, switch to django raw queries

--- sample-apps/django-mysql/README.md
+++ sample-apps/django-mysql/README.md
@@ -6,3 +6,10 @@ With docker-compose installed run
 docker-compose up --build
 ```
 This will expose a Django web server at [localhost:8080](http://localhost:8080)
+
+## URLS : 
+- Homepage : `http://localhost:8080/app`
+- Create a dog : `http://localhost:8080/app/create/<dog_name>`
+- MySQL attack : `http://localhost:8080/app/create/Malicious dog", "Injected wrong boss name"); --%20`
+
+To verify your attack was successfully note that the boss_name usualy is 'N/A', if you open the dog page (you can do this from the home page). You should see a "malicious dog" with a boss name that is not permitted.
--- sample-apps/django-mysql/sample_app/views.py
+++ sample-apps/django-mysql/sample_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 from .models import Dogs
+from django.db import connection
 # Create your views here.
 
 
@@ -17,9 +18,14 @@ def index(request):
 
 def dog_page(request, dog_id):
     dog = get_object_or_404(Dogs, pk=dog_id)
-    return HttpResponse("Your dog, %s, is lovely." % dog.dog_name)
+    return HttpResponse("Your dog, %s, is lovely. Boss name is : %s" % (dog.dog_name, dog.dog_boss))
 
 def create_dogpage(request, dog_name):
-    dog = Dogs(dog_name=dog_name, dog_boss="Unset")
-    dog.save()
-    return HttpResponse("Dog page created with id %s" % dog.id)
+#    dog = Dogs(dog_name=dog_name, dog_boss="Unset")
+#    dog.save()
+    # Using custom sql to create a dog :
+    with connection.cursor() as cursor:
+        query = 'INSERT INTO sample_app_dogs (dog_name, dog_boss) VALUES ("%s", "N/A")' % dog_name
+        print("QUERY : ", query)
+        cursor.execute(query)
+    return HttpResponse("Dog page created")
