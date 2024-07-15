0dfe123fcafd50d3615a0d27ab1f4f5b2a6f5f6a
Wout Feys <me@woutfeys.be>
2024-07-15 10:18:08 +0200

    Add an isAdmin column to dogs

--- sample-apps/flask-mysql/app.py
+++ sample-apps/flask-mysql/app.py
@@ -23,12 +23,12 @@ def get_dogpage(dog_id):
     cursor = mysql.get_db().cursor()
     cursor.execute("SELECT * FROM db.dogs WHERE id = " + str(dog_id))
     dog = cursor.fetchmany(1)[0]
-    return render_template('dogpage.html', title=f'Dog', dog=dog)
+    return render_template('dogpage.html', title=f'Dog', dog=dog, isAdmin=("Yes" if dog[2] else "No"))
 
 @app.route('/create/<dog_name>')
 def create_dog(dog_name):
     connection = mysql.get_db()
     cursor = connection.cursor()
-    cursor.execute(f"INSERT INTO dogs (dog_name) VALUES('%s')" % (dog_name))
+    cursor.execute(f'INSERT INTO dogs (dog_name, isAdmin) VALUES("%s", 0)' % (dog_name))
     connection.commit()
     return f'Dog {(dog_name)}'
