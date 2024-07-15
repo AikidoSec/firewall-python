03b035d7525e352b8eafcbe6cc87538aabcc278b
Wout Feys <me@woutfeys.be>
2024-07-15 12:16:04 +0200

    Trying to modify the Flask class, (frustrating)

--- src/sources/flask.py
+++ src/sources/flask.py
@@ -1,7 +1,15 @@
 import importhook
 
+def generate_flask_middleware(app):
+    def flask_middleware(environ, start_response):
+        print("Flask middleware ran")
+        return app(environ, start_response)
+    return flask_middleware
+
+
 @importhook.on_import('flask')
 def on_django_import(flask):
     modified_flask = importhook.copy_module(flask)
+    setattr(modified_flask.Flask, "__init__", generate_flask_init(flask))
     print("Modified flask")
     return modified_flask
