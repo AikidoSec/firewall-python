cd6af9a498c036be0de04584b84b82d1aac38b64
Wout Feys <me@woutfeys.be>
2024-07-15 12:34:30 +0200

    Modify Flask class (working now)

--- src/sources/flask.py
+++ src/sources/flask.py
@@ -1,15 +1,22 @@
 import importhook
-
-def generate_flask_middleware(app):
+from flask import Flask
+def install_flask_middleware(app):
     def flask_middleware(environ, start_response):
         print("Flask middleware ran")
         return app(environ, start_response)
-    return flask_middleware
+    app.wsgi_app = flask_middleware
 
+def aikido_flask_init(_self, *args, **kwargs):
+        super(Flask, _self).__init__(*args, **kwargs)
+        # Insert here (e.g.)
+        @_self.route("/aikido")
+        def homepage2():
+            return "Aikido link"
+        # End insert
 
 @importhook.on_import('flask')
-def on_django_import(flask):
+def on_flask_import(flask):
     modified_flask = importhook.copy_module(flask)
-    setattr(modified_flask.Flask, "__init__", generate_flask_init(flask))
+    setattr(modified_flask.Flask, "__init__", aikido_flask_init)
     print("Modified flask")
     return modified_flask
