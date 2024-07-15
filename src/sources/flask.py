56f368e93125eec0fe887276115530380546b162
Wout Feys <me@woutfeys.be>
2024-07-15 12:42:45 +0200

    Install middleware successfully (it works)

--- src/sources/flask.py
+++ src/sources/flask.py
@@ -1,18 +1,16 @@
 import importhook
 from flask import Flask
-def install_flask_middleware(app):
-    def flask_middleware(environ, start_response):
-        print("Flask middleware ran")
-        return app(environ, start_response)
-    app.wsgi_app = flask_middleware
+class AikidoMiddleware(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        response = self.app(environ, start_response)
+        return response
 
 def aikido_flask_init(_self, *args, **kwargs):
         super(Flask, _self).__init__(*args, **kwargs)
-        # Insert here (e.g.)
-        @_self.route("/aikido")
-        def homepage2():
-            return "Aikido link"
-        # End insert
+        _self.wsgi_app = AikidoMiddleware(_self.wsgi_app)
 
 @importhook.on_import('flask')
 def on_flask_import(flask):
