afeea55191fdbfcc5e9c87002cc3d696d25334b3
Wout Feys <me@woutfeys.be>
2024-07-15 16:39:09 +0200

    Add a comment when middleware is activated

--- src/sources/flask.py
+++ src/sources/flask.py
@@ -7,6 +7,7 @@ class AikidoMiddleware(object):
         self.app = app
 
     def __call__(self, environ, start_response):
+        print("-- Aikido middleware interjected --")
         response = self.app(environ, start_response)
         return response
 
@@ -16,11 +17,13 @@ class AikidoMiddleware(object):
 def on_flask_import(flask):
     flask._original = "Hello!"
     modified_flask = importhook.copy_module(flask)
+
     prev_flask_init = copy.deepcopy(flask.Flask.__init__)
     def aikido_flask_init(_self, *args, **kwargs):
         prev_flask_init(_self, *args, **kwargs)
         print("Flask version : ", version("flask"))
         _self.wsgi_app = AikidoMiddleware(_self.wsgi_app)
+    
     setattr(modified_flask.Flask, "__init__", aikido_flask_init)
     print("Modified flask")
     return modified_flask
