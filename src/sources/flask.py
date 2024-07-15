0a0df4122f5df4c3255d537b8fec02396e91c41b
Wout Feys <me@woutfeys.be>
2024-07-15 16:34:54 +0200

    Update bug with flask

--- src/sources/flask.py
+++ src/sources/flask.py
@@ -1,4 +1,5 @@
 import importhook
+import copy
 from importlib.metadata import version
 
 class AikidoMiddleware(object):
@@ -9,14 +10,17 @@ class AikidoMiddleware(object):
         response = self.app(environ, start_response)
         return response
 
-def aikido_flask_init(_self, *args, **kwargs):
-        super(_self.__class__, _self).__init__(*args, **kwargs)
-        print("Flask version : ", version("flask"))
-        _self.wsgi_app = AikidoMiddleware(_self.wsgi_app)
+
 
 @importhook.on_import('flask')
 def on_flask_import(flask):
+    flask._original = "Hello!"
     modified_flask = importhook.copy_module(flask)
+    prev_flask_init = copy.deepcopy(flask.Flask.__init__)
+    def aikido_flask_init(_self, *args, **kwargs):
+        prev_flask_init(_self, *args, **kwargs)
+        print("Flask version : ", version("flask"))
+        _self.wsgi_app = AikidoMiddleware(_self.wsgi_app)
     setattr(modified_flask.Flask, "__init__", aikido_flask_init)
     print("Modified flask")
     return modified_flask
