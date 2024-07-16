df76f16d66d7fa63a16a58678c3cc92901db4990
Wout Feys <me@woutfeys.be>
2024-07-16 15:21:37 +0200

    Simplify logger "AIKIDO_DEBUG" find

--- aikido_firewall/helpers/logging.py
+++ aikido_firewall/helpers/logging.py
@@ -13,15 +13,14 @@ APPLICATION_NAME = "aikido_firewall"
 aikido_debug_env = os.getenv("AIKIDO_DEBUG")
 LOG_LEVEL = logging.INFO
 if aikido_debug_env is not None:
-    if aikido_debug_env.lower() in ["true", "false", "1", "0"]:
-        debug = aikido_debug_env.lower() in ["true", "1"]
-        LOG_LEVEL = logging.DEBUG if debug else logging.INFO
+    debug = aikido_debug_env.lower() in ["true", "1"]
+    LOG_LEVEL = logging.DEBUG if debug else logging.INFO
 
 # Create a logger
 logger = logging.getLogger(APPLICATION_NAME)
 
 # Configure format
-fmt = logging.Formatter("%(name)s: %(asctime)s | %(levelname)s |> %(message)s")
+fmt = logging.Formatter("%(name)s: %(asctime)s | %(levelname)s > %(message)s")
 # Get stdout handler
 stdout = logging.StreamHandler(stream=sys.stdout)
 stdout.setLevel(LOG_LEVEL)
