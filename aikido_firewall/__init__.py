53ba73f16569136038f2f71eae1f3ab6334cba8f
Wout Feys <me@woutfeys.be>
2024-07-16 15:11:16 +0200

    Add a startup message for Aikido

--- aikido_firewall/__init__.py
+++ aikido_firewall/__init__.py
@@ -8,3 +8,8 @@ import aikido_firewall.sources.flask
 
 # Import middleware
 import aikido_firewall.middleware.django
+
+# Import logger
+from aikido_firewall.helpers.logging import logger
+
+logger.info("Aikido python firewall started")
