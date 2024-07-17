56212bfade9a571d0259ab528894fbe85dc01026
Wout Feys <me@woutfeys.be>
2024-07-17 10:39:07 +0200

    Fix helper escape_string_regexp, it now works

--- aikido_firewall/helpers/escape_string_regexp.py
+++ aikido_firewall/helpers/escape_string_regexp.py
@@ -6,9 +6,11 @@ import re
 
 
 def escape_string_regexp(string):
-    """
-    Escape characters with special meaning either inside or outside character sets.
-    Use a simple backslash escape when it's always valid, and a '\\xnn' escape when the simpler form would be disallowed by Unicode patterns' stricter grammar.
-    Taken from https://github.com/sindresorhus/escape-string-regexp/
-    """
-    return re.sub(r"[|\\{}()[\]^$+*?.]", r"\\\g<0>", "\\$&").replace("-", "\\x2d")
+   """
+   Escape characters with special meaning either inside or outside character sets.
+   Use a simple backslash escape when it's always valid, and a '\\xnn' escape when the simpler form would be disallowed by Unicode patterns' stricter grammar.
+   Taken from https://github.com/sindresorhus/escape-string-regexp/
+   """
+   pattern = re.compile(r"[|\\{}()[\]^$+*?.]")
+   replace1 = re.sub(pattern, r"\\\g<0>", string)
+   return re.sub(r"-", r"\\x2d", replace1)
