150b053b3ea584915d61bafa4925451a28c020f8
Wout Feys <me@woutfeys.be>
2024-07-17 21:16:58 +0200

    aikido_firewall init update, only protect needs to import sources and sinks

--- aikido_firewall/__init__.py
+++ aikido_firewall/__init__.py
@@ -4,15 +4,6 @@ Aggregates from the different modules
 
 from dotenv import load_dotenv
 
-# Import sources
-import aikido_firewall.sources.django
-import aikido_firewall.sources.flask
-
-# Import sinks
-import aikido_firewall.sinks.pymysql
-
-# Import middleware
-import aikido_firewall.middleware.django
 
 # Import logger
 from aikido_firewall.helpers.logging import logger
@@ -26,5 +17,11 @@ load_dotenv()
 
 def protect():
     """Start Aikido agent"""
+    # Import sources
+    import aikido_firewall.sources.django
+    import aikido_firewall.sources.flask
+
+    # Import sinks
+    import aikido_firewall.sinks.pymysql
     logger.info("Aikido python firewall started")
     start_agent()
