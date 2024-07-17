edc5aeb81cd02ca45403f714558a51da6c3ecc5e
Wout Feys <me@woutfeys.be>
2024-07-17 13:39:05 +0200

    Create empty function "parse_query_params"

---
+++ aikido_firewall/helpers/__init__.py
@@ -0,0 +1,5 @@
+"""
+__init__.py for helpers, imports funcs
+"""
+
+from aikido_firewall.helpers.parse_query_params import parse_query_params
---
+++ aikido_firewall/helpers/parse_query_params.py
@@ -0,0 +1,10 @@
+"""
+Helper function file, see function docstring
+"""
+
+
+def parse_query_params(str):
+    """
+    Parses a query parameter string and turns it into a key-value dict
+    """
+    return {}
