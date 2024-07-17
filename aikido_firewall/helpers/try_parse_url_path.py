78d7e2e8ed1c7a7e13c55861f53c6ec81590b85f
Wout Feys <me@woutfeys.be>
2024-07-17 17:52:22 +0200

    Add the try_parse_url_path function

---
+++ aikido_firewall/helpers/try_parse_url_path.py
@@ -0,0 +1,27 @@
+"""
+Includes try_parse_url which will try and parse
+a url using urlparse.
+Includes try_parse_url_path
+"""
+
+from urllib.parse import urlparse
+
+
+def try_parse_url(url):
+    """try to parse Url with urlparse"""
+    try:
+        return urlparse(url)
+    except ValueError:
+        return None
+
+
+def try_parse_url_path(url):
+    """Try and parse url path"""
+    parsed = try_parse_url(f"http://localhost{url}" if url.startswith("/") else url)
+
+    if not parsed or not parsed.scheme:
+        return None
+    print(parsed)
+    if parsed.path == "":
+        return "/"
+    return parsed.path
