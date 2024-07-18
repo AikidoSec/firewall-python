2854b4d01348573d5c5b1f458c2c7d66f4a1635c
Wout Feys <me@woutfeys.be>
2024-07-18 15:11:49 +0200

    Fix some bugs with userinput_occ_safely_enc

--- aikido_firewall/vulnerabilities/sql_injection/userinput_occurrences_safely_encapsulated.py
+++ aikido_firewall/vulnerabilities/sql_injection/userinput_occurrences_safely_encapsulated.py
@@ -2,11 +2,20 @@
 ????
 """
 
+import regex
+from aikido_firewall.helpers.escape_string_regexp import escape_string_regexp
 from aikido_firewall.helpers import get_current_and_next_segments
-from aikido_firewall.vulnerabilities.sql_injection.consts import SQL_STRING_CHARS
+from aikido_firewall.vulnerabilities.sql_injection.consts import (
+    SQL_STRING_CHARS,
+    SQL_ESCAPE_SEQUENCES,
+)
+
+escape_sequences_pattern = regex.escape(
+    "|".join(map(escape_string_regexp, SQL_ESCAPE_SEQUENCES))
+)
+escape_sequences_regex = regex.compile(escape_sequences_pattern, flags=regex.MULTILINE)
 
 
-# escape_sequences_regex define
 def userinput_occurrences_safely_encapsulated(query, user_input):
     """
     ???
@@ -23,6 +32,12 @@ def userinput_occurrences_safely_encapsulated(query, user_input):
             (char for char in SQL_STRING_CHARS if char == char_before_user_input), None
         )
 
+        # Special case for when the user input starts with a single quote
+        # If the user input is `'value`
+        # And the single quote is properly escaped with a backslash we split the following
+        # `SELECT * FROM table WHERE column = '\'value'`
+        # Into [`SELECT * FROM table WHERE column = '\`, `'`]
+        # The char before the user input will be `\` and the char after the user input will be `'`
         for char in ['"', "'"]:
             if (
                 not quote_char
@@ -32,6 +47,8 @@ def userinput_occurrences_safely_encapsulated(query, user_input):
             ):
                 quote_char = char
                 char_before_user_input = current_seg[-2]
+                # Remove the escaped quote from the user input
+                # otherwise we'll flag the user input as NOT safely encapsulated
                 input_str = input_str[1:]
                 break
 
