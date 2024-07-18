fdecf6f9d1de69cf59c0d9f37944ace3d528d4fa
Wout Feys <me@woutfeys.be>
2024-07-18 14:05:53 +0200

    Django update settings and config

--- sample-apps/django-mysql-gunicorn/manage.py
+++ sample-apps/django-mysql-gunicorn/manage.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-import aikido_firewall # Aikido module
 import os
 import sys
 
 
 def main():
     """Run administrative tasks."""
+    import aikido_firewall # Aikido module
+    aikido_firewall.protect()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sample-django-mysql-gunicorn-app.settings')
     try:
         from django.core.management import execute_from_command_line
