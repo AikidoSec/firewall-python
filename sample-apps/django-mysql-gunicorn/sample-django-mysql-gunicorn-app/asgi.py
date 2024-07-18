40f035b6343be5555a3878ef22c3d8d2020ddcf5
Wout Feys <me@woutfeys.be>
2024-07-18 11:47:18 +0200

    Renaming in django-mysql-gunicorn app

--- sample-apps/django-mysql-gunicorn/sample-django-mysql-gunicorn-app/asgi.py
+++ sample-apps/django-mysql-gunicorn/sample-django-mysql-gunicorn-app/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for sample-django-mysql-app project.
+ASGI config for sample-django-mysql-gunicorn-app project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sample-django-mysql-app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sample-django-mysql-gunicorn-app.settings')
 
 application = get_asgi_application()
--- sample-apps/django-mysql-gunicorn/sample-django-mysql-gunicorn-app/urls.py
+++ sample-apps/django-mysql-gunicorn/sample-django-mysql-gunicorn-app/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for sample-django-mysql-app project.
+URL configuration for sample-django-mysql-gunicorn-app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
