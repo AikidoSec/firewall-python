bcd1fc095b1b4772a5b8b4ae51db854694c82bb8
Wout Feys <me@woutfeys.be>
2024-07-18 14:05:37 +0200

    Flask update settings and config

--- sample-apps/flask-mysql-uwsgi/app.py
+++ sample-apps/flask-mysql-uwsgi/app.py
@@ -6,7 +6,7 @@ from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 if __name__ == '__main__':
-    app.run(threaded=True) # Run threaded so we can test our agent's capabilities
+    app.run() # Run threaded so we can test our agent's capabilities
 mysql = MySQL()
 
 app.config['MYSQL_DATABASE_HOST'] = 'db'
