6e85c032594c5089b8c214b0c795dc0332a94142
Wout Feys <me@woutfeys.be>
2024-07-18 15:07:29 +0200

    Integrate the new helper function with userinput_occ code

--- aikido_firewall/vulnerabilities/sql_injection/userinput_occurrences_safely_encapsulated.py
+++ aikido_firewall/vulnerabilities/sql_injection/userinput_occurrences_safely_encapsulated.py
@@ -14,12 +14,11 @@ def userinput_occurrences_safely_encapsulated(query, user_input):
     segments_in_between = get_current_and_next_segments(query.split(user_input))
 
     for segment in segments_in_between:
-        current_segment = segment["currentSegment"]
-        next_segment = segment["nextSegment"]
+        current_seg, next_seg = segment
 
         input_str = user_input
-        char_before_user_input = current_segment[-1]
-        char_after_user_input = next_segment[0]
+        char_before_user_input = current_seg[-1]
+        char_after_user_input = next_seg[0]
         quote_char = next(
             (char for char in SQL_STRING_CHARS if char == char_before_user_input), None
         )
@@ -28,11 +27,11 @@ def userinput_occurrences_safely_encapsulated(query, user_input):
             if (
                 not quote_char
                 and input_str.startswith(char)
-                and current_segment[-2:] == f"{char}\\"
+                and current_seg[-2:] == f"{char}\\"
                 and char_after_user_input == char
             ):
                 quote_char = char
-                char_before_user_input = current_segment[-2]
+                char_before_user_input = current_seg[-2]
                 input_str = input_str[1:]
                 break
 
