4220ec6d2b08be457ed0f92cb599fcfff87b9e8b
Wout Feys <me@woutfeys.be>
2024-07-23 14:16:53 +0200

    Add some tests for protect() funtion

---
+++ aikido_firewall/init_test.py
@@ -0,0 +1,12 @@
+import pytest
+from aikido_firewall import protect
+from aikido_firewall.agent import get_ipc
+def test_protect_with_django(monkeypatch, caplog):
+    monkeypatch.setitem(globals(), 'aikido_firewall.sources.django', 'dummy_django_module')
+    monkeypatch.setitem(globals(), 'aikido_firewall.sinks.pymysql', 'dummy_pymysql_module')
+
+    protect(module="django")
+
+    assert "Aikido python firewall started" in caplog.text
+    assert get_ipc() != None
+    get_ipc().agent_proc.kill()
