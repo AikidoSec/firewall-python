87a71aec0593af24fa296fbbfcd29ec0952fb6a8
Wout Feys <me@woutfeys.be>
2024-07-24 11:29:56 +0200

    update the way to_api_response() function works

--- aikido_firewall/agent/api/__init__.py
+++ aikido_firewall/agent/api/__init__.py
@@ -2,20 +2,19 @@
 init.py file for api/ folder. Includes abstract class ReportingApi
 """
 
-import json
-
 
 class ReportingApi:
     """This is the super class for the reporting API's"""
 
-    def to_api_response(self, status, data):
+    def to_api_response(self, res):
         """Converts results into an Api response obj"""
+        status = res.status_code
         if status == 429:
             return {"success": False, "error": "rate_limited"}
         elif status == 401:
             return {"success": False, "error": "invalid_token"}
         try:
-            return json.loads(data)
+            return res.json()
         except Exception:
             return {"success": False, "error": "unknown_error"}
 
