465c7465f514b7b37eefef232dfa8aa7532ca749
Wout Feys <me@woutfeys.be>
2024-07-24 11:29:44 +0200

    Give back api response in http_api file

--- aikido_firewall/agent/api/http_api.py
+++ aikido_firewall/agent/api/http_api.py
@@ -13,12 +13,18 @@ class ReportingApiHTTP(ReportingApi):
         self.reporting_url = reporting_url
 
     def report(self, token, event, timeout_in_sec):
-        res = requests.post(
-            self.reporting_url + "api/runtime/events",
-            data=event,
-            timeout=timeout_in_sec,
-            headers=get_headers(token),
-        )
+        try:
+            res = requests.post(
+                self.reporting_url + "api/runtime/events",
+                data=event,
+                timeout=timeout_in_sec,
+                headers=get_headers(token),
+            )
+        except requests.exceptions.Timeout:
+            return {"success": False, "error": "timeout"}
+        except Exception as e:
+            raise e
+        return self.to_api_response(res)
 
 
 def get_headers(token):
