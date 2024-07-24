211421ca041b52fbfe711c33b760cef15312ada6
Wout Feys <me@woutfeys.be>
2024-07-24 11:13:34 +0200

    Linting

--- aikido_firewall/agent/api/__init__.py
+++ aikido_firewall/agent/api/__init__.py
@@ -1,16 +1,18 @@
 """ 
 init.py file for api/ folder. Includes abstract class ReportingApi
 """
+
 import json
+
 
 class ReportingApi:
     """This is the super class for the reporting API's"""
 
     def to_api_response(self, status, data):
         """Converts results into an Api response obj"""
-        if(status == 429):
-            return { "success": False, "error": "rate_limited"}
-        elif(status == 401):
+        if status == 429:
+            return {"success": False, "error": "rate_limited"}
+        elif status == 401:
             return {"success": False, "error": "invalid_token"}
         try:
             return json.loads(data)
