adbfa830062a561a8ef1faf2c033a3186593e84b
Wout Feys <me@woutfeys.be>
2024-07-24 11:19:28 +0200

    Send data using requests.post

--- aikido_firewall/agent/api/http_api.py
+++ aikido_firewall/agent/api/http_api.py
@@ -1,6 +1,7 @@
 """
 Exports the HTTP API class
 """
+
 import requests
 from aikido_firewall.agent.api import ReportingApi
 
@@ -12,4 +13,14 @@ class ReportingApiHTTP(ReportingApi):
         self.reporting_url = reporting_url
 
     def report(self, token, event, timeout_in_sec):
-        print("Do something here")
+        res = requests.post(
+            self.reporting_url + "api/runtime/events",
+            data=event,
+            timeout=timeout_in_sec,
+            headers=get_headers(token),
+        )
+
+
+def get_headers(token):
+    """Returns headers"""
+    return {"Content-Type": "application/json", "Authorization": str(token)}
