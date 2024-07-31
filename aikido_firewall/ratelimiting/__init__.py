659225f621cb35287dbd9330a764d17bbabf58ec
Wout Feys <me@woutfeys.be>
2024-07-31 13:05:57 +0200

    Implement ratelimiting w/ IP

--- aikido_firewall/ratelimiting/__init__.py
+++ aikido_firewall/ratelimiting/__init__.py
@@ -11,13 +11,21 @@ def should_ratelimit_request(context, reporter):
     if not match:
         return {"block": False}
     endpoint = match["endpoint"]
-    route = match.get["route"]
+    route = match["route"]
     if not endpoint["rateLimiting"] or not endpoint["rateLimiting"]["enabled"]:
         return {"block": False}
 
     # Production logic, still missing
 
-    max_requests = int(endpoint["rateLimiting"]["maxRequests"])
-    windows_size_in_ms = int(endpoint["rateLimiting"]["windowSizeInMS"])
+    if context.remote_address:
+        max_requests = int(endpoint["rateLimiting"]["maxRequests"])
+        windows_size_in_ms = int(endpoint["rateLimiting"]["windowSizeInMS"])
+        allowed = reporter.rate_limiter.is_allowed(
+            f"{context.method}:{route}:ip:{context.remote_address}",
+            windows_size_in_ms,
+            max_requests,
+        )
+        if not allowed:
+            return {"block": True, "trigger": "ip"}
 
     return {"block": False}
