3367c58498fa26bd9515401e7d419bce8ed497f8
Wout Feys <me@woutfeys.be>
2024-07-31 12:15:31 +0200

    Update the should_ratelimit_request function, parses endpoint

--- aikido_firewall/ratelimiting/__init__.py
+++ aikido_firewall/ratelimiting/__init__.py
@@ -7,4 +7,17 @@ def should_ratelimit_request(context, reporter):
     """
     Checks if the request should be ratelimited or not
     """
-    pass
+    match = reporter.conf.get_endpoint(context)
+    if not match:
+        return {"block": False}
+    endpoint = match["endpoint"]
+    route = match.get["route"]
+    if not endpoint["rateLimiting"] or not endpoint["rateLimiting"]["enabled"]:
+        return {"block": False}
+
+    # Production logic, still missing
+
+    max_requests = int(endpoint["rateLimiting"]["maxRequests"])
+    windows_size_in_ms = int(endpoint["rateLimiting"]["windowSizeInMS"])
+
+    return {"block": False}
