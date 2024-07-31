58c61ceb1579834949a850d2ec1a0da6e86e4055
Wout Feys <me@woutfeys.be>
2024-07-31 14:36:37 +0200

    Update to 0.0.2

--- aikido_firewall/__init__.py
+++ aikido_firewall/__init__.py
@@ -5,7 +5,7 @@ Aggregates from the different modules
 from dotenv import load_dotenv
 
 # Constants
-PKG_VERSION = "0.0.1"
+PKG_VERSION = "0.0.2"
 
 # Import logger
 from aikido_firewall.helpers.logging import logger
