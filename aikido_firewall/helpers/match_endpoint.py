3dfb130185eb82497587a54af8ff1e952d5be6ab
Wout Feys <me@woutfeys.be>
2024-07-31 11:49:28 +0200

    Port over the match_endpoint function

---
+++ aikido_firewall/helpers/match_endpoint.py
@@ -0,0 +1,48 @@
+"""
+Helper function file, see function docstring
+"""
+
+import regex as re
+from .try_parse_url_path import try_parse_url_path
+
+
+def match_endpoint(context, endpoints):
+    """Check if the context matches one of the endpoints"""
+    if not context.method:
+        return None
+
+    possible = [
+        endpoint
+        for endpoint in endpoints
+        if endpoint["method"] == "*" or endpoint["method"] == context.method
+    ]
+
+    endpoint = next(
+        (endpoint for endpoint in possible if endpoint["route"] == context.route), None
+    )
+
+    if endpoint:
+        return {"endpoint": endpoint, "route": endpoint["route"]}
+
+    if not context.url:
+        return None
+
+    path = try_parse_url_path(context.url)
+
+    if not path:
+        return None
+
+    wildcards = sorted(
+        (endpoint for endpoint in possible if "*" in endpoint["route"]),
+        key=lambda e: e["route"].count("*"),
+        reverse=True,
+    )
+
+    for wildcard in wildcards:
+        route = wildcard["route"]
+        regex = re.compile(f"^{route.replace('*', '(.*)')}\/?$", re.IGNORECASE)
+
+        if regex.match(path):
+            return {"endpoint": wildcard, "route": route}
+
+    return None
