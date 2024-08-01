39280e22a618063ccd248abac8a8391cc02ec9ba
Wout Feys <me@woutfeys.be>
2024-08-01 10:14:47 +0200

    Create a test release 0.0.5 with app.aikido.dev as url

--- aikido_firewall/__init__.py
+++ aikido_firewall/__init__.py
@@ -5,7 +5,7 @@ Aggregates from the different modules
 from dotenv import load_dotenv
 
 # Constants
-PKG_VERSION = "0.0.4"
+PKG_VERSION = "0.0.5"
 
 # Import logger
 from aikido_firewall.helpers.logging import logger
