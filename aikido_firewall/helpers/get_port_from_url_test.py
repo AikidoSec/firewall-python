068dcf646ae819a995630295ead3c4a65e16c374
Wout Feys <me@woutfeys.be>
2024-08-01 15:54:57 +0200

    Add helper function get_port_from_url

---
+++ aikido_firewall/helpers/get_port_from_url_test.py
@@ -0,0 +1,9 @@
+import pytest
+from .get_port_from_url import get_port_from_url
+
+
+def test_get_port_from_url():
+    assert get_port_from_url("http://localhost:4000") == 4000
+    assert get_port_from_url("http://localhost") == 80
+    assert get_port_from_url("https://localhost") == 443
+    assert get_port_from_url("ftp://localhost") is None
