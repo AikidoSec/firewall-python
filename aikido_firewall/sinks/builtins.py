0c07e4e91be43c4e4742fe299732798bc212f893
Wout Feys <me@woutfeys.be>
2024-08-02 16:58:58 +0200

    Create a path traversal error and actually check for pt and raise it

--- aikido_firewall/sinks/builtins.py
+++ aikido_firewall/sinks/builtins.py
@@ -5,6 +5,11 @@ Sink module for `builtins`, python's built-in function
 import copy
 import importhook
 from aikido_firewall.helpers.logging import logger
+from aikido_firewall.vulnerabilities.path_traversal.check_context_for_path_traversal import (
+    check_context_for_path_traversal,
+)
+from aikido_firewall.context import get_current_context
+from aikido_firewall.errors import AikidoPathTraversal
 
 
 @importhook.on_import("builtins")
@@ -19,6 +24,11 @@ def on_builtins_import(builtins):
     former_open = copy.deepcopy(builtins.open)
 
     def aikido_new_open(*args, **kwargs):
+        result = check_context_for_path_traversal(
+            filename=args[0], operation="builtins.open", context=get_current_context()
+        )
+        if len(result) != 0:
+            raise AikidoPathTraversal()
         logger.info("File name openend : %s, in mode : `%s`", args[0], args[1])
         return former_open(*args, **kwargs)
 
