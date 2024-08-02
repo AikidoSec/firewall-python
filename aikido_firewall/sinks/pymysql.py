00009d5fd0bb5acd5967591a11f43a9962eccb95
Wout Feys <me@woutfeys.be>
2024-08-02 14:42:39 +0200

    Parametrized receive=True

--- aikido_firewall/sinks/pymysql.py
+++ aikido_firewall/sinks/pymysql.py
@@ -42,7 +42,7 @@ def on_flask_import(mysql):
         if contains_injection:
             get_comms().send_data_to_bg_process("ATTACK", (contains_injection, context))
             should_block = get_comms().send_data_to_bg_process(
-                "READ_PROPERTY", "block", True
+                action="READ_PROPERTY", obj="block", receive=True
             )
             if should_block:
                 raise AikidoSQLInjection("SQL Injection [aikido_firewall]")
