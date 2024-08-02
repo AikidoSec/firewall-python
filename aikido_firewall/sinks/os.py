937bc2a76652324b1c6f5549c3f4fedc232c3939
Wout Feys <me@woutfeys.be>
2024-08-02 16:59:09 +0200

    Add list of functions to wrap in os and os.path

---
+++ aikido_firewall/sinks/os.py
@@ -0,0 +1,29 @@
+# File functions :
+OS_FILE_FUNCTIONS = [
+    "access",
+    "chmod",
+    "chown",
+    "lstat",
+    "mkdir",
+    "listdir",
+    "readlink",
+    "unlink",
+    "rename",
+    "rmdir",
+    "remove",
+    "symlink",
+    "stat",
+    "utime",
+    "link",
+    "makedirs",
+    "walk",
+]
+OS_PATH_FUNCTIONS = [
+    "exists",
+    "realpath",
+    "getsize",
+    "getmtime",
+    "getatime",
+    "getctime",
+]
+# os.path.join(path, *paths)
