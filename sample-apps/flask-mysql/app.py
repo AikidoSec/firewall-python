1f33c7fa45c16d84508549cfb246c729f8194cda
Wout Feys <me@woutfeys.be>
2024-08-05 11:43:48 +0200

    Add /shell route to sample app

--- sample-apps/flask-mysql/app.py
+++ sample-apps/flask-mysql/app.py
@@ -1,6 +1,7 @@
 import aikido_firewall # Aikido package import
 aikido_firewall.protect()
 
+import subprocess
 from flask import Flask, render_template, request
 from flaskext.mysql import MySQL
 
@@ -42,3 +43,12 @@ def create_dog():
     cursor.execute(f'INSERT INTO dogs (dog_name, isAdmin) VALUES ("%s", 0)' % (dog_name))
     connection.commit()
     return f'Dog {dog_name} created successfully'
+
+@app.route("/shell", methods=['GET'])
+def show_shell_form():
+    return render_template('shell.html')
+@app.route("/shell", methods=['POST'])
+def execute_command():
+    command = request.form['command']
+    result = subprocess.run(command.split(), capture_output=True, text=True)
+    return str(result.stdout)
