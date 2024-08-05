47c24d998b60617612b92b5b71456af09c75b8f7
Wout Feys <me@woutfeys.be>
2024-08-05 10:32:54 +0200

    Update module imports

--- aikido_firewall/__init__.py
+++ aikido_firewall/__init__.py
@@ -35,11 +35,14 @@ def protect(module="any", server=True):
     if not module in ["django", "django-gunicorn"]:
         import aikido_firewall.sources.flask
 
-    # Import sinks
+    # Import DB Sinks
     import aikido_firewall.sinks.pymysql
     import aikido_firewall.sinks.mysqlclient
     import aikido_firewall.sinks.pymongo
     import aikido_firewall.sinks.psycopg2
+
+    # Import shell sinks
     import aikido_firewall.sinks.os_system
+    import aikido_firewall.sinks.subprocess
 
     logger.info("Aikido python firewall started")
