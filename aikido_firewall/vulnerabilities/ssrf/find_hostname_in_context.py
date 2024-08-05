698508119893227742b23ce9b3bd640aa3062421
Wout Feys <me@woutfeys.be>
2024-08-05 14:23:30 +0200

    Create new file with function find_hostname_in_context

---
+++ aikido_firewall/vulnerabilities/ssrf/find_hostname_in_context.py
@@ -0,0 +1,27 @@
+"""
+Mainly exports function `find_hostname_in_context`
+"""
+
+from aikido_firewall.context import UINPUT_SOURCES
+from aikido_firewall.helpers.extract_strings_from_user_input import (
+    extract_strings_from_user_input,
+)
+from .find_hostname_in_userinput import find_hostname_in_userinput
+
+
+def find_hostname_in_context(hostname, context, port):
+    """Tries to locate the given hostname from context"""
+    for source in UINPUT_SOURCES:
+        if not hasattr(context, source):
+            continue
+        user_inputs = extract_strings_from_user_input(context, source)
+        if not user_inputs:
+            continue
+        for user_input, path in user_inputs.items():
+            found = find_hostname_in_userinput(user_input, hostname, port)
+            if found:
+                return {
+                    "source": source,
+                    "pathToPayload": path,
+                    "payload": user_input,
+                }
