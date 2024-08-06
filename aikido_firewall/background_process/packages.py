08b5cf9c829a977956088f8b21412cc939805214
Wout Feys <me@woutfeys.be>
2024-08-06 11:44:34 +0200

    Create a new function add_wrapped_package

---
+++ aikido_firewall/background_process/packages.py
@@ -0,0 +1,28 @@
+"""Helper functions for packages"""
+
+from importlib.metadata import version
+from aikido_firewall.helpers.logging import logger
+from .comms import get_comms
+
+MAX_REPORT_TRIES = 5
+
+
+def add_wrapped_package(pkg_name):
+    """Reports a newly wrapped package to the bg process"""
+    pkg_version = version(pkg_name)
+    logger.info("Successfully wrapped package `%s` version (%s)", pkg_name, pkg_version)
+    attempts = 0
+    while attempts < MAX_REPORT_TRIES:
+        res = get_comms().send_data_to_bg_process(
+            "WRAPPED_PACKAGE",
+            {
+                "name": pkg_name,
+                "options": {
+                    "version": pkg_version,
+                    "supported": True,  #  We can change this later if we validate package versions?
+                },
+            },
+        )
+        if res["success"]:
+            break
+        attempts += 1
