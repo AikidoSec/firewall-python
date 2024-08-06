02c37b87a6954db7e033933ccbce6382669e9415
Wout Feys <me@woutfeys.be>
2024-08-06 15:41:43 +0200

    Use is_blocking_enabled function globally

--- aikido_firewall/sinks/os_system.py
+++ aikido_firewall/sinks/os_system.py
@@ -12,6 +12,7 @@ from aikido_firewall.vulnerabilities.shell_injection.check_context_for_shell_inj
 from aikido_firewall.helpers.logging import logger
 from aikido_firewall.background_process import get_comms
 from aikido_firewall.errors import AikidoShellInjection
+from aikido_firewall.helpers.blocking_enabled import is_blocking_enabled
 
 
 @importhook.on_import("os")
@@ -37,10 +38,7 @@ def on_os_import(os):
         logger.debug("Shell injection results : %s", json.dumps(contains_injection))
         if contains_injection:
             get_comms().send_data_to_bg_process("ATTACK", (contains_injection, context))
-            should_block_res = get_comms().send_data_to_bg_process(
-                action="READ_PROPERTY", obj="block", receive=True
-            )
-            if should_block_res["success"] and should_block_res["data"]:
+            if is_blocking_enabled():
                 raise AikidoShellInjection()
 
         return former_system_func(*args, **kwargs)
--- aikido_firewall/sinks/psycopg2.py
+++ aikido_firewall/sinks/psycopg2.py
@@ -13,6 +13,7 @@ from aikido_firewall.vulnerabilities.sql_injection.context_contains_sql_injectio
 from aikido_firewall.vulnerabilities.sql_injection.dialects import Postgres
 from aikido_firewall.background_process import get_comms
 from aikido_firewall.errors import AikidoSQLInjection
+from aikido_firewall.helpers.blocking_enabled import is_blocking_enabled
 
 
 class MutableAikidoConnection:
@@ -46,10 +47,7 @@ def execute_sql_detection_code(sql):
     logger.info("sql_injection results : %s", json.dumps(contains_injection))
     if contains_injection:
         get_comms().send_data_to_bg_process("ATTACK", (contains_injection, context))
-        should_block_res = get_comms().send_data_to_bg_process(
-            action="READ_PROPERTY", obj="block", receive=True
-        )
-        if should_block_res["success"] and should_block_res["data"]:
+        if is_blocking_enabled():
             raise AikidoSQLInjection("SQL Injection [aikido_firewall]")
 
 
