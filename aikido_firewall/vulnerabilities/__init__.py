14e272865ef71eefed5f999dcaef99ad5991578f
Wout Feys <me@woutfeys.be>
2024-08-07 14:05:00 +0200

    Create a new function run_vulnerability_scan

---
+++ aikido_firewall/vulnerabilities/__init__.py
@@ -0,0 +1,38 @@
+"""
+Exports `run_vulnerability_scan` function
+"""
+
+from aikido_firewall.context import get_current_context
+from aikido_firewall.errors import (
+    AikidoException,
+    AikidoSQLInjection,
+    AikidoNoSQLInjection,
+)
+from aikido_firewall.background_process import get_comms
+from aikido_firewall.helpers.blocking_enabled import is_blocking_enabled
+from .sql_injection.context_contains_sql_injection import context_contains_sql_injection
+from .nosql_injection import detect_nosql_injection
+
+
+def run_vulnerability_scan(kind, op, args):
+    """
+    Generally checks context for the provided "kind" of injection,
+    raises error if blocking is enabled, communicates it with reporter
+    """
+    context = get_current_context()
+    error_type = AikidoException  # Default error
+    injection_results = {}
+
+    if kind == "sql_injection":
+        injection_results = context_contains_sql_injection(
+            sql=args[0], dialect=args[1], operation=op, context=context
+        )
+        error_type = AikidoSQLInjection
+    elif kind == "nosql_injection":
+        injection_results = detect_nosql_injection(request=context, _filter=args[0])
+        error_type = AikidoNoSQLInjection
+
+    if injection_results:
+        get_comms().send_data_to_bg_process("ATTACK", (injection_results, context))
+        if is_blocking_enabled():
+            raise error_type()
