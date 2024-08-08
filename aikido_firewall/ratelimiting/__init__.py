2b45d61b31b35e84d6e214b893ea1f5677f6b69d
Wout Feys <me@woutfeys.be>
2024-08-08 11:44:32 +0200

    Add is_allowed_ip and user-based blocking

--- aikido_firewall/ratelimiting/__init__.py
+++ aikido_firewall/ratelimiting/__init__.py
@@ -16,10 +16,10 @@ def should_ratelimit_request(context, reporter):
         return {"block": False}
 
     # Production logic, still missing
-
-    if context.remote_address:
-        max_requests = int(endpoint["rateLimiting"]["maxRequests"])
-        windows_size_in_ms = int(endpoint["rateLimiting"]["windowSizeInMS"])
+    is_allowed_ip = reporter.conf.is_allowed_ip(context.remote_addr)
+    max_requests = int(endpoint["rateLimiting"]["maxRequests"])
+    windows_size_in_ms = int(endpoint["rateLimiting"]["windowSizeInMS"])
+    if context.remote_address and not is_allowed_ip:
         allowed = reporter.rate_limiter.is_allowed(
             f"{context.method}:{route}:ip:{context.remote_address}",
             windows_size_in_ms,
@@ -27,5 +27,13 @@ def should_ratelimit_request(context, reporter):
         )
         if not allowed:
             return {"block": True, "trigger": "ip"}
-
+    if context.user:
+        uid = context.user["id"]
+        allowed = reporter.rate_limiter.is_allowed(
+            f"{context.method}:{route}:user:{uid}",
+            windows_size_in_ms,
+            max_requests,
+        )
+        if not allowed:
+            return {"block": True, "trigger": "user"}
     return {"block": False}
