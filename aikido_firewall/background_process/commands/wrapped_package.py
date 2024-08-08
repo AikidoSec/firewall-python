34bb0e647e56d92361ff682e728bcfc64bbf4edd
Wout Feys <me@woutfeys.be>
2024-08-08 14:25:30 +0200

    Update the way wrapped_package works and add tests

--- aikido_firewall/background_process/commands/wrapped_package.py
+++ aikido_firewall/background_process/commands/wrapped_package.py
@@ -1,12 +1,17 @@
 """Exports `process_wrapped_package`"""
 
+from aikido_firewall.helpers.logging import logger
+
 
 def process_wrapped_package(bg_process, data, conn):
     """A package has been wrapped"""
-    if bg_process.reporter:
+    try:
         pkg_name = data["name"]
         pkg_details = data["details"]
         bg_process.reporter.packages[pkg_name] = pkg_details
         conn.send(True)
-    else:
+    except KeyError:
+        logger.info("Package info was not formatted correctly : %s", data)
+        conn.send(False)
+    except AttributeError:
         conn.send(False)
