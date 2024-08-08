08fd601977ff4b91158bf823b6c5aa83d55fe866
Wout Feys <me@woutfeys.be>
2024-08-08 14:17:14 +0200

    Add tests for read_property function

--- aikido_firewall/background_process/commands/read_property.py
+++ aikido_firewall/background_process/commands/read_property.py
@@ -8,9 +8,9 @@ def process_read_property(bg_process, data, conn):
     Takes in one arg : name of property on reporter, tries to read it.
     Meant to get config props
     """
-    if hasattr(bg_process.reporter, data):
+    try:
         conn.send(bg_process.reporter.__dict__[data])
-    else:
+    except KeyError:
         logger.debug(
             "Reporter has no attribute %s, current reporter: %s",
             data,
