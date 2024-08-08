71403d6134bdf9e5f984fb0abb6eb5d4150de36c
Wout Feys <me@woutfeys.be>
2024-08-08 16:04:31 +0200

    Fix typo : "hostnmae"

--- aikido_firewall/background_process/commands/hostnames_add.py
+++ aikido_firewall/background_process/commands/hostnames_add.py
@@ -6,4 +6,4 @@ from aikido_firewall.helpers.logging import logger
 def process_hostnames_add(bg_process, data, conn):
     """Add a hostname to hostnames object in reporter"""
     logger.debug("Adding a hostname : %s:%s", data[0], data[1])
-    bg_process.reporter.hostnames.add(hostnmae=data[0], port=data[1])
+    bg_process.reporter.hostnames.add(hostname=data[0], port=data[1])
