a4766305204f42f2a94e7095562fde74b2b2cfb7
Wout Feys <me@woutfeys.be>
2024-08-08 14:29:10 +0200

    Update import for should_ratelimit and add tests

--- aikido_firewall/background_process/commands/should_ratelimit.py
+++ aikido_firewall/background_process/commands/should_ratelimit.py
@@ -1,10 +1,13 @@
 """Exports `process_should_ratelimit`"""
 
-from aikido_firewall.ratelimiting import should_ratelimit_request
+import aikido_firewall.ratelimiting as ratelimiting
 
 
 def process_should_ratelimit(bg_process, data, conn):
     """
     Called to check if the context passed along as data should be rate limited
     """
-    conn.send(should_ratelimit_request(context=data, reporter=bg_process.reporter))
+    should_ratelimit = ratelimiting.should_ratelimit_request(
+        context=data, reporter=bg_process.reporter
+    )
+    conn.send(should_ratelimit)
