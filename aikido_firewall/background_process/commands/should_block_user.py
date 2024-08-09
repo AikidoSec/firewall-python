c7c2e9ee81dab852c460bd93e9d2a7c1bc1fba73
Wout Feys <me@woutfeys.be>
2024-08-09 14:40:23 +0200

    It's background_process.reporter and not .conf :0

--- aikido_firewall/background_process/commands/should_block_user.py
+++ aikido_firewall/background_process/commands/should_block_user.py
@@ -3,5 +3,5 @@
 
 def process_should_block_user(bg_process, data, conn):
     """Checks if the user id should be blocked or not"""
-    should_block = bg_process.conf.is_user_blocked(data)
+    should_block = bg_process.reporter.conf.is_user_blocked(data)
     conn.send(should_block)
