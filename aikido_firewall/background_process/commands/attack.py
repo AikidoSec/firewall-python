9e7cb7f6946a2e270fe635a1f9e43afa8c2b0970
Wout Feys <me@woutfeys.be>
2024-08-12 13:54:34 +0200

    Pass along blocked or not to "ATTACK", which now reports blocked ttacks

--- aikido_firewall/background_process/commands/attack.py
+++ aikido_firewall/background_process/commands/attack.py
@@ -1,6 +1,10 @@
 """Main export is process_attack"""
 
+from aikido_firewall.helpers.logging import logger
 
-def process_attack(bg_process, data):
+
+def process_attack(bg_process, data, conn):
     """Adds ATTACK data object to queue"""
     bg_process.queue.put(data)
+    if bg_process.reporter.statistics:
+        bg_process.reporter.statistics.on_detected_attack(blocked=data[2])
