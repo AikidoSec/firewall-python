8c1d4b2ff420f2a64909ea7f0dd4bc496ec774a0
Wout Feys <me@woutfeys.be>
2024-08-12 12:35:20 +0200

    Make a copy of the sample EMPTY_STATS_OBJECT

--- aikido_firewall/background_process/statistics/ensure_sink_stats.py
+++ aikido_firewall/background_process/statistics/ensure_sink_stats.py
@@ -1,5 +1,7 @@
 """Exports ensure_sink_stats function"""
 
+import copy
+
 EMPTY_STATS_OBJECT = {
     "withoutContext": 0,
     "total": 0,
@@ -16,4 +18,4 @@ EMPTY_STATS_OBJECT = {
 def ensure_sink_stats(statistics_obj, sink):
     """Makes sure to initalize sink if it's not there"""
     if sink not in statistics_obj.stats:
-        statistics_obj.stats[sink] = EMPTY_STATS_OBJECT
+        statistics_obj.stats[sink] = copy.deepcopy(EMPTY_STATS_OBJECT)
