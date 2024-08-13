5829c0e9b2172dc45152c08f6b42ea6d045cba3f
Wout Feys <me@woutfeys.be>
2024-08-13 14:34:43 +0200

    Use a docker-compose composite file for firewall_disabled

--- sample-apps/flask-mysql/app.py
+++ sample-apps/flask-mysql/app.py
@@ -1,6 +1,4 @@
-from dotenv import load_dotenv
 import os
-load_dotenv()
 firewall_disabled = os.getenv("FIREWALL_DISABLED")
 if firewall_disabled is not None:
     if firewall_disabled.lower() != "1":
