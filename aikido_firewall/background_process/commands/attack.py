da902c42905abacfdb4ed294025414b2315ab686
Wout Feys <me@woutfeys.be>
2024-08-13 15:12:34 +0200

    Add comment explaining input for process_attack function

--- aikido_firewall/background_process/commands/attack.py
+++ aikido_firewall/background_process/commands/attack.py
@@ -2,7 +2,10 @@
 
 
 def process_attack(bg_process, data, conn):
-    """Adds ATTACK data object to queue"""
+    """
+    Adds ATTACK data object to queue
+    Expected data object : [injection_results, context, blocked_or_not]
+    """
     bg_process.queue.put(data)
     if bg_process.reporter.statistics:
         bg_process.reporter.statistics.on_detected_attack(blocked=data[2])
