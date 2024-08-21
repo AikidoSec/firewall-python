3fc61e0125f31bed5cc6717a2c3233bdf5a12188
Wout Feys <wout@aikido.dev>
2024-08-21 12:02:26 +0200

    Add a "create_interval" helper function

---
+++ aikido_firewall/helpers/create_interval.py
@@ -0,0 +1,32 @@
+"""Exports create_interval"""
+from aikido_firewall.helpers.logging import logger
+
+def create_interval(event_scheduler, interval_in_secs, function, args):
+    """
+    This function creates an interval which first runs "function"
+    after waiting "interval_in_secs" seconds and after that keeps
+    executing the function every "interval_in_secs" seconds.
+    """
+    # Sleep interval_in_secs seconds before starting the loop :
+    event_scheduler.enter(
+        interval_in_secs,
+        1,
+        interval_loop,
+        (event_scheduler, interval_in_secs, function, args),
+    )
+
+
+def interval_loop(event_scheduler, interval_in_secs, function, args):
+    """
+    This is the actual interval loop which executes and schedules the function
+    """
+    logger.critical("Interval loop executing")
+    # Execute function :
+    function(*args)
+    # Schedule the execution of the function in interval_in_secs seconds :
+    event_scheduler.enter(
+        interval_in_secs,
+        1,
+        interval_loop,
+        (event_scheduler, interval_in_secs, function, args),
+    )
