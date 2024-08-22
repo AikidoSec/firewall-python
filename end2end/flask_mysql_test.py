ad74e72e91fae560b76fb890add273c73abaa56d
Wout Feys <wout@aikido.dev>
2024-08-22 10:10:10 +0200

    Create working flask_mysql e2e test

--- end2end/flask_mysql_test.py
+++ end2end/flask_mysql_test.py
@@ -1,14 +1,28 @@
 import pytest
 import requests
 # e2e tests for flask_mysql sample app
+post_url_fw = "http://localhost:8086/create"
+post_url_nofw = "http://localhost:8087/create"
+
 def test_safe_response_with_firewall():
-    pass
+    dog_name = "Bobby Tables"
+    res = requests.post(post_url_fw, data={'dog_name': dog_name})
+    assert res.status_code == 200
+
 
 def test_safe_response_without_firewall():
-    pass
+    dog_name = "Bobby Tables"
+    res = requests.post(post_url_nofw, data={'dog_name': dog_name})
+    assert res.status_code == 200
+
 
 def test_dangerous_response_with_firewall():
-    pass
+    dog_name = 'Dangerous bobby", 1); -- '
+    res = requests.post(post_url_fw, data={'dog_name': dog_name})
+    assert res.status_code == 500
 
 def test_dangerous_response_without_firewall():
-    pass
+    dog_name = 'Dangerous bobby", 1); -- '
+    res = requests.post(post_url_nofw, data={'dog_name': dog_name})
+    assert res.status_code == 200
+
