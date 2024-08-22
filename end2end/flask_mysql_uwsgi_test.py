f65c05568f8da2646c5da94ae83001a60f308bcd
Wout Feys <wout@aikido.dev>
2024-08-22 10:24:31 +0200

    Add end2end test for flask_mysql_uwsgi

---
+++ end2end/flask_mysql_uwsgi_test.py
@@ -0,0 +1,28 @@
+import pytest
+import requests
+# e2e tests for flask_mysql_uwsgi sample app
+post_url_fw = "http://localhost:8088/create"
+post_url_nofw = "http://localhost:8089/create"
+
+def test_safe_response_with_firewall():
+    dog_name = "Bobby Tables"
+    res = requests.post(post_url_fw, data={'dog_name': dog_name})
+    assert res.status_code == 200
+
+
+def test_safe_response_without_firewall():
+    dog_name = "Bobby Tables"
+    res = requests.post(post_url_nofw, data={'dog_name': dog_name})
+    assert res.status_code == 200
+
+
+def test_dangerous_response_with_firewall():
+    dog_name = 'Dangerous bobby", 1); -- '
+    res = requests.post(post_url_fw, data={'dog_name': dog_name})
+    assert res.status_code == 500
+
+def test_dangerous_response_without_firewall():
+    dog_name = 'Dangerous bobby", 1); -- '
+    res = requests.post(post_url_nofw, data={'dog_name': dog_name})
+    assert res.status_code == 200
+
