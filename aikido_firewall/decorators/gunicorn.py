16a3cf8a1dd6222d91640db60848aafa90fb5261
Wout Feys <wout@aikido.dev>
2024-08-22 18:08:29 +0200

    Send KILL when SIGINT/SIGTERM

--- aikido_firewall/decorators/gunicorn.py
+++ aikido_firewall/decorators/gunicorn.py
@@ -2,7 +2,16 @@
 Includes all the wrappers for gunicorn config file
 """
 
+import signal
 import aikido_firewall
+from aikido_firewall.background_process import get_comms
+from aikido_firewall.helpers.logging import logger
+
+
+def signal_handler(sig, frame):
+    """Signal handler on SIGTERM/SIGKILL"""
+    logger.critical("Killing background process... (Received SIGINT/SIGTERM)")
+    get_comms().send_data_to_bg_process("KILL", None)
 
 
 def when_ready(prev_func):
@@ -12,6 +21,9 @@ def when_ready(prev_func):
     """
 
     def aik_when_ready(server):
+        signal.signal(signal.SIGINT, signal_handler)  # Handle Ctrl+C
+        signal.signal(signal.SIGTERM, signal_handler)  # Handle termination signal
+
         aikido_firewall.protect("background-process-only")
         prev_func(server)
 
