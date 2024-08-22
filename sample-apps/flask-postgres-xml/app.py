5c40acbadce79def0b3c8b0f210aabd10aa39740
Wout Feys <wout@aikido.dev>
2024-08-22 17:44:04 +0200

    Use lxml in sample app

--- sample-apps/flask-postgres-xml/app.py
+++ sample-apps/flask-postgres-xml/app.py
@@ -10,6 +10,7 @@ if firewall_disabled is not None:
 from flask import Flask, render_template, request
 import psycopg2
 import xml.etree.ElementTree as ET
+import lxml.etree as ET2
 
 app = Flask(__name__)
 if __name__ == '__main__':
@@ -61,6 +62,7 @@ def post_upload_xml():
 def post_xml():
     raw_xml = request.data.decode('utf-8')
     root = ET.fromstring(raw_xml)
+    ET2.fromstring(raw_xml)
     conn = get_db_connection()
     cursor =  conn.cursor()
     for dog in root.findall('dog'):
