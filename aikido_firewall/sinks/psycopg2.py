0cb83ad8e572a51eb4003c3de1db5e28d3a3c3c7
Wout Feys <wout@aikido.dev>
2024-08-24 11:58:47 +0200

    Move to modifying the cursor_factory

--- aikido_firewall/sinks/psycopg2.py
+++ aikido_firewall/sinks/psycopg2.py
@@ -7,29 +7,22 @@ import importhook
 from aikido_firewall.vulnerabilities.sql_injection.dialects import Postgres
 from aikido_firewall.background_process.packages import add_wrapped_package
 from aikido_firewall.vulnerabilities import run_vulnerability_scan
-from aikido_firewall.helpers.logging import logger
 
 
-def generate_aikido_cursor(cursor_class):
+def wrap_cursor_factory(cursor_factory):
+    former_cursor_factory = copy.deepcopy(cursor_factory)
+    import psycopg2.extensions as ext
 
-    class AikidoCursor(cursor_class):
-        """Aikido's mutable cursor class"""
-
-        def __init__(self, *args, **kwargs):
-            # Initialize the base cursor class :
-            super().__init__(*args, **kwargs)
-
-            # Return a function dynamically
-
+    class AikidoWrappedCursor(ext.cursor):
         def execute(self, *args, **kwargs):
             """Aikido's wrapped execute function"""
-            logger.critical(args[0])
-
             run_vulnerability_scan(
                 kind="sql_injection",
                 op="psycopg2.Connection.Cursor.execute",
                 args=(args[0], Postgres()),  #  args[0] : sql
             )
+            if former_cursor_factory and hasattr(former_cursor_factory, "execute"):
+                return former_cursor_factory.execute(*args, **kwargs)
             return super().execute(*args, **kwargs)
 
         def executemany(self, *args, **kwargs):
@@ -40,24 +33,30 @@ def generate_aikido_cursor(cursor_class):
                     op="psycopg2.Connection.Cursor.executemany",
                     args=(sql, Postgres()),
                 )
+            if former_cursor_factory and hasattr(former_cursor_factory, "executemany"):
+                return former_cursor_factory.executemany(*args, **kwargs)
             return super().executemany(*args, **kwargs)
 
-    return AikidoCursor
+    return AikidoWrappedCursor
 
 
 @importhook.on_import("psycopg2")
 def on_psycopg2_import(psycopg2):
     """
-    Hook 'n wrap on `psycopg2._ext`, we modify the cursor class to our own subclass which
-    overwrites execute() and executemany() to first run aikido code.
+    Hook 'n wrap on `psycopg2.connect` function, we modify the cursor_factory
+    of the result of this connect function.
     """
     modified_psycopg2 = importhook.copy_module(psycopg2)
+    former_connect_function = copy.deepcopy(psycopg2.connect)
 
-    aikido_cursor = generate_aikido_cursor(psycopg2._ext.cursor)
+    def aikido_connect(*args, **kwargs):
+        former_conn = former_connect_function(*args, **kwargs)
+        former_conn.cursor_factory = wrap_cursor_factory(former_conn.cursor_factory)
+        return former_conn
 
     # pylint: disable=no-member
-    setattr(psycopg2._ext, "cursor", aikido_cursor)
-    setattr(modified_psycopg2._ext, "cursor", aikido_cursor)
+    setattr(psycopg2, "connect", aikido_connect)
+    setattr(modified_psycopg2, "connect", aikido_connect)
     add_wrapped_package("psycopg2")
     add_wrapped_package("psycopg2-binary")
     return modified_psycopg2
