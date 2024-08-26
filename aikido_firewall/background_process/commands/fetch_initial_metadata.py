673561c01e55d3a291baa0eb210222152f4b0fad
Wout Feys <wout@aikido.dev>
2024-08-26 14:40:00 +0200

    Update fetch_initial_metadata command

--- aikido_firewall/background_process/commands/fetch_initial_metadata.py
+++ aikido_firewall/background_process/commands/fetch_initial_metadata.py
@@ -1,11 +1,13 @@
 """Exports `process_fetch_initial_metadata`"""
 
 
-def process_fetch_initial_metadata(bg_process, data, conn):
+def process_fetch_initial_metadata(reporter, data, conn):
     """Fetches initial metadata"""
+    if not reporter:
+        return conn.send({"bypassed_ips": [], "matched_endpoints": []})
     route_metadata = data["route_metadata"]
-    bypassed_ips = bg_process.reporter.conf.bypassed_ips
-    matched_endpoints = bg_process.reporter.conf.get_endpoints(route_metadata)
+    bypassed_ips = reporter.conf.bypassed_ips
+    matched_endpoints = reporter.conf.get_endpoints(route_metadata)
     conn.send(
         {
             "bypassed_ips": bypassed_ips,
