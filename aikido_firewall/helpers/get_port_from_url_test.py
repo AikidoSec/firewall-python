fe522e2365e370342a08568f68c65a40ff099d38
Wout Feys <wout@aikido.dev>
2024-08-26 11:37:12 +0200

    Add another test for get_port_from_url (from node PR)

--- aikido_firewall/helpers/get_port_from_url_test.py
+++ aikido_firewall/helpers/get_port_from_url_test.py
@@ -5,5 +5,6 @@ from .get_port_from_url import get_port_from_url
 def test_get_port_from_url():
     assert get_port_from_url("http://localhost:4000") == 4000
     assert get_port_from_url("http://localhost") == 80
+    assert get_port_from_url("https://test.com:8080/test?abc=123") == 8080
     assert get_port_from_url("https://localhost") == 443
     assert get_port_from_url("ftp://localhost") is None
