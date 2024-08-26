0bf8e7de5059da20c5d2b2e0778217fa43752be2
Wout Feys <wout@aikido.dev>
2024-08-26 14:41:50 +0200

    Update users command

--- aikido_firewall/background_process/commands/user.py
+++ aikido_firewall/background_process/commands/user.py
@@ -1,7 +1,7 @@
 """Exports `process_user`"""
 
 
-def process_user(bg_process, data, conn):
+def process_user(reporter, data, conn):
     """Adds a user to the users object of the reporter"""
-    if bg_process.reporter:
-        bg_process.reporter.users.add_user(data)
+    if reporter:
+        reporter.users.add_user(data)
--- aikido_firewall/background_process/commands/user_test.py
+++ aikido_firewall/background_process/commands/user_test.py
@@ -11,30 +11,20 @@ def mock_reporter():
     return reporter
 
 
-@pytest.fixture
-def mock_bg_process(mock_reporter):
-    """Fixture to create a mock background process with a reporter."""
-    bg_process = MagicMock()
-    bg_process.reporter = mock_reporter
-    return bg_process
-
-
-def test_process_user_adds_user(mock_bg_process):
+def test_process_user_adds_user(mock_reporter):
     """Test that process_user adds a user when reporter is present."""
     user_data = {"username": "test_user", "email": "test@example.com"}
 
-    process_user(mock_bg_process, user_data, None)  # conn is not used in this function
+    process_user(mock_reporter, user_data, None)  # conn is not used in this function
 
     # Check that the add_user method was called with the correct arguments
-    mock_bg_process.reporter.users.add_user.assert_called_once_with(user_data)
+    mock_reporter.users.add_user.assert_called_once_with(user_data)
 
 
 def test_process_user_no_reporter():
     """Test that process_user does nothing when reporter is not present."""
-    bg_process = MagicMock()
-    bg_process.reporter = None
     user_data = {"username": "test_user", "email": "test@example.com"}
 
-    process_user(bg_process, user_data, None)  # conn is not used in this function
+    process_user(None, user_data, None)  # conn is not used in this function
 
     # Make sure there were no errors
