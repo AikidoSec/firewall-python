33aecf6a0fe832f10bca734f365f0325dbf85d70
Wout Feys <wout@aikido.dev>
2024-08-26 14:40:06 +0200

    Update hostnames_add command

--- aikido_firewall/background_process/commands/hostnames_add.py
+++ aikido_firewall/background_process/commands/hostnames_add.py
@@ -3,7 +3,8 @@
 from aikido_firewall.helpers.logging import logger
 
 
-def process_hostnames_add(bg_process, data, conn):
+def process_hostnames_add(reporter, data, conn):
     """Add a hostname to hostnames object in reporter"""
     logger.debug("Adding a hostname : %s:%s", data[0], data[1])
-    bg_process.reporter.hostnames.add(hostname=data[0], port=data[1])
+    if reporter:
+        reporter.hostnames.add(hostname=data[0], port=data[1])
