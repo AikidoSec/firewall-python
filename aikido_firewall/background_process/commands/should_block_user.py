8c429ca144be99abfa31f2e333b72ecfcf1f5ec7
Wout Feys <wout@aikido.dev>
2024-08-26 14:40:48 +0200

    Update should_block_user command

--- aikido_firewall/background_process/commands/should_block_user.py
+++ aikido_firewall/background_process/commands/should_block_user.py
@@ -1,7 +1,9 @@
 """Mainly exports `process_should_block_user`"""
 
 
-def process_should_block_user(bg_process, data, conn):
+def process_should_block_user(reporter, data, conn):
     """Checks if the user id should be blocked or not"""
-    should_block = bg_process.reporter.conf.is_user_blocked(data)
+    if not reporter:
+        return conn.send(False)
+    should_block = reporter.conf.is_user_blocked(data)
     conn.send(should_block)
