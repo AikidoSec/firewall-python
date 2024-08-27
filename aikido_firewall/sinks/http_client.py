d614a4f14cae5ffa977f9e83c124bdc3f83d0cbd
Wout Feys <wout@aikido.dev>
2024-08-27 14:56:49 +0200

    Update http_client wrapping : wrap getresponse and try-catch everything a bit better

--- aikido_firewall/sinks/http_client.py
+++ aikido_firewall/sinks/http_client.py
@@ -6,6 +6,11 @@ import copy
 import importhook
 from aikido_firewall.helpers.logging import logger
 from aikido_firewall.vulnerabilities import run_vulnerability_scan
+from aikido_firewall.vulnerabilities.ssrf.handle_http_response import (
+    handle_http_response,
+)
+from aikido_firewall.helpers.try_parse_url import try_parse_url
+from aikido_firewall.errors import AikidoException
 
 
 @importhook.on_import("http.client")
@@ -18,15 +23,41 @@ def on_http_import(http):
     """
     modified_http = importhook.copy_module(http)
     former_putrequest = copy.deepcopy(http.HTTPConnection.putrequest)
+    former_getresponse = copy.deepcopy(http.HTTPConnection.getresponse)
 
-    def aik_new_putrequest(_self, method, url, *args, **kwargs):
-        logger.debug("HTTP Request [%s] %s:%s %s", method, _self.host, _self.port, url)
-        url_object = {"href": url, "hostname": _self.host}
-        run_vulnerability_scan(
-            kind="ssrf", op="http.client.putrequest", args=(url_object, _self.port)
-        )
-        return former_putrequest(_self, method, url, *args, **kwargs)
+    def aik_new_putrequest(_self, method, path, *args, **kwargs):
+        #  Aikido putrequest, gets called before the request went through
+        try:
+            # Set path for aik_new_getresponse :
+            _self.aikido_attr_path = path
+
+            # Create a URL Object :
+            assembled_url = f"http://{_self.host}:{_self.port}{path}"
+            url_object = try_parse_url(assembled_url)
+
+            run_vulnerability_scan(
+                kind="ssrf", op="http.client.putrequest", args=(url_object, _self.port)
+            )
+        except AikidoException as e:
+            raise e
+        except Exception as e:
+            logger.debug("Exception occured in custom putrequest function : %s", e)
+        return former_putrequest(_self, method, path, *args, **kwargs)
+
+    def aik_new_getresponse(_self):
+        #  Aikido getresponse, gets called after the request is complete
+        # And fetches the response
+        response = former_getresponse(_self)
+        try:
+            assembled_url = f"http://{_self.host}:{_self.port}{_self.aikido_attr_path}"
+            source_url = try_parse_url(assembled_url)
+            handle_http_response(http_response=response, source=source_url)
+        except Exception as e:
+            logger.debug("Exception occured in custom getresponse function : %s", e)
+        return response
 
     # pylint: disable=no-member
     setattr(http.HTTPConnection, "putrequest", aik_new_putrequest)
+    # pylint: disable=no-member
+    setattr(http.HTTPConnection, "getresponse", aik_new_getresponse)
     return modified_http
