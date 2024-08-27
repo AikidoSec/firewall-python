210d2358c79040aa811f87663d0694fd85b85b37
Wout Feys <wout@aikido.dev>
2024-08-27 14:59:11 +0200

    update handle_http_response to make it work with an actual response

--- aikido_firewall/vulnerabilities/ssrf/handle_http_response.py
+++ aikido_firewall/vulnerabilities/ssrf/handle_http_response.py
@@ -3,29 +3,37 @@
 from aikido_firewall.context import get_current_context
 from aikido_firewall.helpers.is_redirect_status_code import is_redirect_status_code
 from aikido_firewall.helpers.try_parse_url import try_parse_url
-from aikido_firewall.helpers.get_port_from_url import get_port_from_url
 from .find_hostname_in_context import find_hostname_in_context
 from .get_redirect_origin import get_redirect_origin
+
+
+def normalize_headers(headers):
+    """Normalize headers to a consistent format."""
+    return {k.lower(): v for k, v in headers.items()}
 
 
 def handle_http_response(http_response, source):
     """Handles a response object from http"""
     context = get_current_context()
-    if not context:
+    if not context or not source:
         return
-    status_code = 200  # TO DO
+    status_code = http_response.code
     if not status_code or not is_redirect_status_code(status_code):
         #  The status code needs to be a redirect status code
         # Otherwise there is no real danger for SSRF attacks
         return
-    location = "http://example.com"  # YEah
+
+    raw_headers = getattr(http_response, "headers", {})
+    headers = normalize_headers(dict(raw_headers))
+    location = headers.get("location", None)
     if not isinstance(location, str):
         return
-    destination = try_parse_url(location)
-    if not destination:
+
+    parsed_location = try_parse_url(location)
+    if not parsed_location:
         return
 
-    add_redirect_to_context(source, destination, context)
+    add_redirect_to_context(source, parsed_location, context)
 
 
 def add_redirect_to_context(source, destination, context):
@@ -34,11 +42,10 @@ def add_redirect_to_context(source, destination, context):
     SSRF attacks with redirects.
     """
     redirect_origin = None
-    source_port = get_port_from_url(source["href"])
 
     # Check if the source hostname is in the context - is true if its the first redirect
     # in the chain and the user input is the source
-    found = find_hostname_in_context(source["hostname"], context, source_port)
+    found = find_hostname_in_context(source.hostname, context, source.port)
 
     # If the source hostname is not in the context, check if it's a redirect in
     # an already existing chain
