576bd6baddbe811f576323c98232ee8840809694
Wout Feys <wout@aikido.dev>
2024-08-27 16:52:18 +0200

    Add comments clarifying attack changes

--- aikido_firewall/background_process/commands/attack.py
+++ aikido_firewall/background_process/commands/attack.py
@@ -7,8 +7,11 @@ def process_attack(connection_manager, data, queue):
     Expected data object : [injection_results, context, blocked_or_not, stacktrace]
     """
     if queue:
+        # If the queue exists, add it to the queue so the 2nd background thread
+        # can report it to the Aikido server's.
         queue.put(data)
     else:
+        # No queue is set, report attack directly :
         connection_manager.on_detected_attack(
             attack=data[0], context=data[1], blocked=data[2], stack=data[3]
         )
