d70039cf8005601d72bcf20ab70b84c8a36f4863
Wout Feys <wout@aikido.dev>
2024-08-27 14:59:53 +0200

    Add logging for redirects and simplify code

--- aikido_firewall/vulnerabilities/ssrf/__init__.py
+++ aikido_firewall/vulnerabilities/ssrf/__init__.py
@@ -1,21 +1,22 @@
 """Exports scan_for_ssrf_in_request function"""
 
+from aikido_firewall.helpers.logging import logger
 from .check_context_for_ssrf import check_context_for_ssrf
 from .is_redirect_to_private_ip import is_redirect_to_private_ip
 
 
 def scan_for_ssrf_in_request(url, port, operation, context):
     """Scans for SSRF attacks"""
-    hostname = url["hostname"]
 
     # Check if the request is a SSRF :
     context_contains_ssrf_results = check_context_for_ssrf(
-        hostname, port, operation, context
+        url.hostname, port, operation, context
     )
     if context_contains_ssrf_results:
         return context_contains_ssrf_results
 
     # Check if the request is a SSRF with redirects :
+    logger.debug("Redirects : %s", context.outgoing_req_redirects)
     redirected_ssrf_results = is_redirect_to_private_ip(url, context)
     if redirected_ssrf_results:
         return {
