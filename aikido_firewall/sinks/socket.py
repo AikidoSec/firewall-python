6b51411f72502939449f4e0655799f78a66f3594
Wout Feys <wout@aikido.dev>
2024-08-29 18:32:46 +0200

    Use run_vulnerability_scan in socket.py wrapping

--- aikido_firewall/sinks/socket.py
+++ aikido_firewall/sinks/socket.py
@@ -5,9 +5,7 @@ Sink module for `socket`
 import copy
 import importhook
 from aikido_firewall.helpers.logging import logger
-from aikido_firewall.vulnerabilities.ssrf.inspect_getaddrinfo_result import (
-    inspect_getaddrinfo_result,
-)
+from aikido_firewall.vulnerabilities import run_vulnerability_scan
 
 SOCKET_OPERATIONS = [
     "gethostbyname",
@@ -25,8 +23,9 @@ def generate_aikido_function(former_func, op):
     def aik_new_func(*args, **kwargs):
         res = former_func(*args, **kwargs)
         if op is "getaddrinfo":
-            inspect_getaddrinfo_result(dns_results=res, hostname=args[0], port=args[1])
-        logger.debug("Res %s", res)
+            run_vulnerability_scan(
+                kind="ssrf", op="socket.getaddrinfo", args=(res, args[0], args[1])
+            )
         return res
 
     return aik_new_func
@@ -36,9 +35,7 @@ def generate_aikido_function(former_func, op):
 def on_socket_import(socket):
     """
     Hook 'n wrap on `socket`
-    Our goal is to wrap the following socket functions that take a hostname :
-      -  gethostbyname() -- map a hostname to its IP number
-      -  gethostbyaddr() -- map an IP number or hostname to DNS info
+    Our goal is to wrap the getaddrinfo socket function
     https://github.com/python/cpython/blob/8f19be47b6a50059924e1d7b64277ad3cef4dac7/Lib/socket.py#L10
     Returns : Modified socket object
     """
