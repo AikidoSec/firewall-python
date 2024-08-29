292ed43c8b2380ae82126cf66975b2ce0635306f
nadiraikido <166383531+nadiraikido@users.noreply.github.com>
2024-08-29 13:03:37 +0200

    Check if shell property is set

--- aikido_firewall/sinks/subprocess.py
+++ aikido_firewall/sinks/subprocess.py
@@ -18,15 +18,21 @@ def generate_aikido_function(op, former_func):
 
     def aikido_new_func(*args, op=op, former_func=former_func, **kwargs):
         logger.debug("Wrapper - `subprocess` on %s() function", op)
+        shell_enabled = kwargs.get('shell')
+
         if isinstance(args[0], list):
             command = " ".join(args[0])
         else:
             command = args[0]
-        run_vulnerability_scan(
-            kind="shell_injection",
-            op=f"subprocess.{op}",
-            args=(command,),
-        )
+        
+        # For all operations above: call, run, check_call, Popen, check_output, default value
+        # of the shell property is False.
+        if shell_enabled is True:
+            run_vulnerability_scan(
+                kind="shell_injection",
+                op=f"subprocess.{op}",
+                args=(command,),
+            )
         return former_func(*args, **kwargs)
 
     return aikido_new_func
