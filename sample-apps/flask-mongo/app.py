2d171f5803b185a286ea8d800e26547146b36554
Wout Feys <wout@aikido.dev>
2024-08-30 16:00:32 +0200

    Move mongo database outside of flask-mongo

--- sample-apps/flask-mongo/app.py
+++ sample-apps/flask-mongo/app.py
@@ -15,7 +15,7 @@ from bson import ObjectId
 app = Flask(__name__)
 if __name__ == '__main__':
     app.run(threaded=True) # Run threaded so we can test how our bg process works
-app.config["MONGO_URI"] = "mongodb://admin:password@db:27017/my_database?authSource=admin"
+app.config["MONGO_URI"] = "mongodb://admin:password@host.docker.internal:27017/my_database?authSource=admin"
 mongo = PyMongo(app)
 
 @app.route("/")
