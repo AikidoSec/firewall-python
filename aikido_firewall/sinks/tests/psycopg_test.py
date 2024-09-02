da6e24075f494a095be6457e887f0de6f9b114f2
Wout Feys <wout@aikido.dev>
2024-09-02 12:22:38 +0200

    Update psycopg tests to check for operation and kind

--- aikido_firewall/sinks/tests/psycopg_test.py
+++ aikido_firewall/sinks/tests/psycopg_test.py
@@ -4,9 +4,6 @@ import aikido_firewall.sinks.psycopg
 from aikido_firewall.background_process.comms import reset_comms
 from aikido_firewall.vulnerabilities.sql_injection.dialects import Postgres
 
-kind = "sql_injection"
-op = "pymysql.connections.query"
-
 
 @pytest.fixture
 def database_conn():
@@ -26,9 +23,11 @@ def test_cursor_execute(database_conn):
         query = "SELECT * FROM dogs"
         cursor.execute(query)
 
-        called_with_args = mock_run_vulnerability_scan.call_args[1]["args"]
-        assert called_with_args[0] == query
-        assert isinstance(called_with_args[1], Postgres)
+        called_with = mock_run_vulnerability_scan.call_args[1]
+        assert called_with["args"][0] == query
+        assert isinstance(called_with["args"][1], Postgres)
+        assert called_with["op"] == "psycopg.Cursor.execute"
+        assert called_with["kind"] == "sql_injection"
         mock_run_vulnerability_scan.assert_called_once()
 
         cursor.fetchall()
