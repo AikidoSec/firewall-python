2d5cbcc0cce7341c31f1d4810d0b13d971275778
Wout Feys <wout@aikido.dev>
2024-09-02 10:14:53 +0200

    Add extra tests for pymysql

--- aikido_firewall/sinks/tests/pymysql_test.py
+++ aikido_firewall/sinks/tests/pymysql_test.py
@@ -32,3 +32,71 @@ def test_cursor_execute(database_conn):
         cursor.fetchall()
         database_conn.close()
         mock_run_vulnerability_scan.assert_called_once()
+
+
+def test_cursor_execute_context(database_conn):
+    reset_comms()
+    with patch(
+        "aikido_firewall.vulnerabilities.run_vulnerability_scan"
+    ) as mock_run_vulnerability_scan:
+        with database_conn.cursor() as cursor:
+            query = "SELECT * FROM dogs"
+            cursor.execute(query)
+
+            called_with_args = mock_run_vulnerability_scan.call_args[1]["args"]
+            assert called_with_args[0] == query
+            assert isinstance(called_with_args[1], MySQL)
+            mock_run_vulnerability_scan.assert_called_once()
+
+            cursor.fetchall()
+            database_conn.close()
+            mock_run_vulnerability_scan.assert_called_once()
+
+
+def test_cursor_execute_parameterized(database_conn):
+    reset_comms()
+    with patch(
+        "aikido_firewall.vulnerabilities.run_vulnerability_scan"
+    ) as mock_run_vulnerability_scan:
+        with database_conn.cursor() as cursor:
+            query = "INSERT INTO dogs (dog_name, isAdmin) VALUES (%s, %s)"
+            cursor.execute(query, ("doggo", False))
+
+            called_with_args = mock_run_vulnerability_scan.call_args[1]["args"]
+            assert (
+                called_with_args[0]
+                == "INSERT INTO dogs (dog_name, isAdmin) VALUES ('doggo', 0)"
+            )
+            assert isinstance(called_with_args[1], MySQL)
+            mock_run_vulnerability_scan.assert_called_once()
+
+            database_conn.commit()
+            database_conn.close()
+            mock_run_vulnerability_scan.assert_called_once()
+
+
+def test_cursor_executemany(database_conn):
+    reset_comms()
+    with patch(
+        "aikido_firewall.vulnerabilities.run_vulnerability_scan"
+    ) as mock_run_vulnerability_scan:
+        cursor = database_conn.cursor()
+        data = [
+            ("Doggy", False),
+            ("Doggy 2", True),
+            ("Dogski", True),
+        ]
+        cursor.executemany("INSERT INTO dogs (dog_name, isAdmin) VALUES (%s, %s)", data)
+        called_with_args = mock_run_vulnerability_scan.call_args[1]["args"]
+        assert (
+            called_with_args[0]
+            == "INSERT INTO dogs (dog_name, isAdmin) VALUES ('Doggy', 0),('Doggy 2', 1),('Dogski', 1)"
+        )
+        assert isinstance(called_with_args[1], MySQL)
+        mock_run_vulnerability_scan.assert_called_once()
+        database_conn.commit()
+        mock_run_vulnerability_scan.assert_called_once()
+
+        cursor.close()
+        database_conn.close()
+        mock_run_vulnerability_scan.assert_called_once()
