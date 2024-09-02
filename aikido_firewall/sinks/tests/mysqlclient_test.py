edd5985f437f5bf1f2080ec90be4393a207c1d10
Wout Feys <wout@aikido.dev>
2024-09-02 09:58:56 +0200

    Add executemany and callproc unit tests for mysqlclient

--- aikido_firewall/sinks/tests/mysqlclient_test.py
+++ aikido_firewall/sinks/tests/mysqlclient_test.py
@@ -34,3 +34,63 @@ def test_cursor_execute(database_conn):
         cursor.close()
         database_conn.close()
         mock_run_vulnerability_scan.assert_called_once()
+
+
+def test_cursor_executemany(database_conn):
+    reset_comms()
+    with patch(
+        "aikido_firewall.vulnerabilities.run_vulnerability_scan"
+    ) as mock_run_vulnerability_scan:
+        cursor = database_conn.cursor()
+        data = [
+            ("Doggy", False),
+            ("Doggy 2", True),
+            ("Dogski", True),
+        ]
+        cursor.executemany("INSERT INTO dogs (dog_name, isAdmin) VALUES (%s, %s)", data)
+        called_with_args = mock_run_vulnerability_scan.call_args[1]["args"]
+        assert (
+            called_with_args[0]
+            == "INSERT INTO dogs (dog_name, isAdmin) VALUES ('Doggy', 0),('Doggy 2', 1),('Dogski', 1)"
+        )
+        assert isinstance(called_with_args[1], MySQL)
+        mock_run_vulnerability_scan.assert_called_once()
+        database_conn.commit()
+        mock_run_vulnerability_scan.assert_called_once()
+
+        cursor.close()
+        database_conn.close()
+        mock_run_vulnerability_scan.assert_called_once()
+
+
+def test_cursor_callproc(database_conn):
+    reset_comms()
+    with patch(
+        "aikido_firewall.vulnerabilities.run_vulnerability_scan"
+    ) as mock_run_vulnerability_scan:
+        import MySQLdb
+
+        cursor = database_conn.cursor()
+        with pytest.raises(MySQLdb.OperationalError):
+            cursor.callproc("test_procedure", (5, 10, 2))
+
+        calls = mock_run_vulnerability_scan.call_args_list
+
+        first_call_args = calls[0][1]["args"]
+        assert (
+            first_call_args[0]
+            == "SET @_test_procedure_0=5,@_test_procedure_1=10,@_test_procedure_2=2"
+        )
+        assert isinstance(first_call_args[1], MySQL)
+
+        second_call_args = calls[1][1]["args"]
+        assert (
+            second_call_args[0]
+            == "CALL test_procedure(@_test_procedure_0,@_test_procedure_1,@_test_procedure_2)"
+        )
+        assert isinstance(second_call_args[1], MySQL)
+        mock_run_vulnerability_scan.assert_called()
+
+        database_conn.commit()
+        cursor.close()
+        database_conn.close()
