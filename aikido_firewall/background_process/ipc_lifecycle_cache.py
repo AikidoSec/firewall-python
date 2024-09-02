f61e16a81e14e26810e1db6d43a5a6b360bd88cc
Wout Feys <wout@aikido.dev>
2024-09-02 15:24:01 +0200

    Add extra unit tests for ipc_lifecycle_cache.py

--- aikido_firewall/background_process/ipc_lifecycle_cache.py
+++ aikido_firewall/background_process/ipc_lifecycle_cache.py
@@ -3,7 +3,7 @@ Contains the `IPCLifecycleCache` cache for the duration of a single request
 """
 
 import threading
-from .comms import get_comms
+import aikido_firewall.background_process.comms as comms
 
 #  cache needs to be per thread, not shared in the entire process
 local = threading.local()
@@ -29,7 +29,7 @@ class IPCLifecycleCache:
     def populate(self, context):
         """Fetches data over IPC"""
         # Fetch bypassed ips:
-        res = get_comms().send_data_to_bg_process(
+        res = comms.get_comms().send_data_to_bg_process(
             action="FETCH_INITIAL_METADATA",
             obj={"route_metadata": context.get_route_metadata()},
             receive=True,
