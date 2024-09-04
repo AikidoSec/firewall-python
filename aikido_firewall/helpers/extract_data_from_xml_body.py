c95c4f1ff21a0124d27166b8acbe3140c75edd26
Wout Feys <wout@aikido.dev>
2024-09-04 10:32:25 +0200

    Remove leftover print statements

--- aikido_firewall/helpers/extract_data_from_xml_body.py
+++ aikido_firewall/helpers/extract_data_from_xml_body.py
@@ -11,9 +11,7 @@ def extract_data_from_xml_body(user_input, root_element):
 
     extracted_xml_attrs = context.xml
     for el in root_element:
-        print(extracted_xml_attrs)
         for k, v in el.items():
-            print("Key : %s, Value : %s", k, v)
             if not extracted_xml_attrs.get(k):
                 extracted_xml_attrs[k] = set()
             extracted_xml_attrs[k].add(v)
