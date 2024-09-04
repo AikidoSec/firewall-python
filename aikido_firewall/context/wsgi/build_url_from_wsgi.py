e1663989c55c67908fbafe2d297208ab82ad1277
nadiraikido <166383531+nadiraikido@users.noreply.github.com>
2024-09-04 16:54:07 +0200

    Formatting

--- aikido_firewall/context/wsgi/build_url_from_wsgi.py
+++ aikido_firewall/context/wsgi/build_url_from_wsgi.py
@@ -5,14 +5,14 @@ def build_url_from_wsgi(request):
     """Builds a URL from the different parts in a WSGI request"""
     scheme = request["wsgi.url_scheme"]
     uri = request["PATH_INFO"]
-    
+
     host = ""
     if "HTTP_HOST" not in request or request["HTTP_HOST"] == "":
         host = request["SERVER_NAME"]
         port = request["SERVER_PORT"]
-        
+
         host = f"{host}:{port}"
     else:
         host = request["HTTP_HOST"]
-    
+
     return f"{scheme}://{host}{uri}"
