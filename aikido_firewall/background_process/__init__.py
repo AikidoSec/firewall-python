8fee7571b3d6f123fa26427f03b57e35beb24f60
Wout Feys <wout@aikido.dev>
2024-09-05 17:06:33 +0200

    Move starting code over to __init__.py
    Start the background process in __init__.py instead of comms.py. Also use the new hash function and update IS_ALIVE logic

--- aikido_firewall/background_process/__init__.py
+++ aikido_firewall/background_process/__init__.py
@@ -4,22 +4,18 @@ and listen for data sent by our sources and sinks
 """
 
 import os
+from multiprocessing import Process
 from aikido_firewall.helpers.token import get_token_from_env
 from aikido_firewall.helpers.get_temp_dir import get_temp_dir
+from aikido_firewall.helpers.hash_aikido_token import hash_aikido_token
 from aikido_firewall.helpers.logging import logger
+
 from aikido_firewall.background_process.comms import (
     AikidoIPCCommunications,
     get_comms,
     reset_comms,
 )
-
-
-def ipc_address_with_counter(counter):
-    """Generates a random UDS file"""
-    temp_dir = get_temp_dir()
-    prefix = "aikido_python"
-    index = str(counter)
-    return f"{temp_dir}/{prefix}_{index}.sock"
+from .aikido_background_process import AikidoBackgroundProcess
 
 
 def start_background_process():
@@ -29,21 +25,38 @@ def start_background_process():
 
     # Generate a secret key :
     secret_key_bytes = str.encode(str(get_token_from_env()))
-
-    # Remove the socket file if it already exists
-    i = 1
-    ipc_address = ipc_address_with_counter(i)
-    while os.path.exists(ipc_address):
-        # Removing this UDS File will result in that bg process terminating. See
-        # aikido_background_process.py > socket_file_exists_check()
-        logger.debug(
-            "Unix Domain Socket file %s already exists, deleting.", ipc_address
+    if secret_key_bytes == b"None":
+        logger.warning(
+            "You are running without AIKIDO_TOKEN set, not starting background process.."
         )
-        os.remove(ipc_address)
+        return
 
-        i += 1
-        ipc_address = ipc_address_with_counter(i)
+    uds_filename = get_uds_filename()
+    comms = AikidoIPCCommunications(uds_filename, secret_key_bytes)
 
-    logger.debug("Communication starting on UDS File : %s", ipc_address)
-    comms = AikidoIPCCommunications(ipc_address, secret_key_bytes)
-    comms.start_aikido_listener()
+    if os.path.exists(uds_filename):
+        # Send out a PING to check if the daemon running is still active :
+        res = comms.send_data_to_bg_process(action="PING", obj=tuple(), receive=True)
+        if res["success"] and res["data"] == "Received":
+            # Ping is active, reset comms and return
+            logger.debug("UDS file exists and is live, returning.")
+            reset_comms()
+            return
+        # Ping must have failed, remove the existing file to ensure ther eis no corruption.
+        os.remove(uds_filename)
+
+    logger.debug("Communication starting on UDS File : %s", uds_filename)
+    comms = AikidoIPCCommunications(uds_filename, secret_key_bytes)
+    #  Daemon is set to True so that the process kills itself when the main process dies
+    background_process = Process(
+        target=AikidoBackgroundProcess, args=(comms.address, comms.key), daemon=True
+    )
+    background_process.start()
+
+
+def get_uds_filename():
+    """Returns the address for UDS file"""
+    temp_dir = get_temp_dir()
+    prefix = "aikido_python"
+    name = hash_aikido_token()
+    return f"{temp_dir}/{prefix}_{name}.sock"
