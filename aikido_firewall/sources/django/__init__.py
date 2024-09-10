37f82e49aeaffe4889f5e38a5aef7916126cbf3d
Wout Feys <wout@aikido.dev>
2024-09-10 10:44:44 +0200

    django __init__.py file cleanup comments

--- aikido_firewall/sources/django/__init__.py
+++ aikido_firewall/sources/django/__init__.py
@@ -15,22 +15,19 @@ def on_django_gunicorn_import(django):
     Hook 'n wrap on `django.core.handlers.base`
     Our goal is to wrap the `_get_response` function
     # https://github.com/django/django/blob/5865ff5adcf64da03d306dc32b36e87ae6927c85/django/core/handlers/base.py#L174
-    So we can change the _view_middleware function before anything else
     Returns : Modified django.core.handlers.base object
     """
     modified_django = importhook.copy_module(django)
 
     former__get_response = copy.deepcopy(django.BaseHandler._get_response)
 
-    # Synchronous :
-    def aikido__get_response(self, request):
-        logger.debug("_get_response django function being called")
-        #  We do some initial request handling :
-        run_init_stage(request)
+    def aikido__get_response(self, request):  # Synchronous (WSGI)
+        run_init_stage(request)  # We do some initial request handling
 
-        # The rate limiting middleware needs to be added as the last middleware in the chain :
         if ratelimiting_middleware not in self._view_middleware:
+            # The rate limiting middleware needs to be last in the chain.
             self._view_middleware += [ratelimiting_middleware]
+
         res = former__get_response(self, request)
         if hasattr(res, "status_code"):
             request_handler(stage="post_response", status_code=res.status_code)
