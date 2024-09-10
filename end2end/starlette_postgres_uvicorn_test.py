f121752450ced8f4eefb7cab537e13696eb34a27
Wout Feys <wout@aikido.dev>
2024-09-11 01:15:29 +0200

    Add e2e tests for new sample app w/ starlette

---
+++ end2end/starlette_postgres_uvicorn_test.py
@@ -0,0 +1,28 @@
+import pytest
+import requests
+# e2e tests for flask_postgres sample app
+post_url_fw = "http://localhost:8102/create"
+post_url_nofw = "http://localhost:8103/create"
+
+def test_safe_response_with_firewall():
+    dog_name = "Bobby Tables"
+    res = requests.post(post_url_fw, data={'dog_name': dog_name})
+    assert res.status_code == 201
+
+
+def test_safe_response_without_firewall():
+    dog_name = "Bobby Tables"
+    res = requests.post(post_url_nofw, data={'dog_name': dog_name})
+    assert res.status_code == 201
+
+
+def test_dangerous_response_with_firewall():
+    dog_name = "Dangerous Bobby', TRUE); -- "
+    res = requests.post(post_url_fw, data={'dog_name': dog_name})
+    assert res.status_code == 500
+
+def test_dangerous_response_without_firewall():
+    dog_name = "Dangerous Bobby', TRUE); -- "
+    res = requests.post(post_url_nofw, data={'dog_name': dog_name})
+    assert res.status_code == 201
+
