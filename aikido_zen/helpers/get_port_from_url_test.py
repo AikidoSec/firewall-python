c84207b673c42b3d309125058b4d93e2b79e109e
nadiraikido <166383531+nadiraikido@users.noreply.github.com>
2024-09-10 16:49:00 +0200

    Formatting

--- aikido_zen/helpers/get_port_from_url_test.py
+++ aikido_zen/helpers/get_port_from_url_test.py
@@ -21,5 +21,7 @@ def test_get_port_from_parsed_url():
     )
     assert get_port_from_url(urlparse("https://localhost"), True) == 443
     assert get_port_from_url(urlparse("ftp://localhost"), True) is None
-    assert get_port_from_url(urlparse("http://localhost:1337\\u0000asd.php"), True) is None
+    assert (
+        get_port_from_url(urlparse("http://localhost:1337\\u0000asd.php"), True) is None
+    )
     assert get_port_from_url(urlparse("http://localhost:123123/asd.php"), True) is None
