8a338674140c8e16b50f5962ce182225b41e1e85
Wout Feys <wout@aikido.dev>
2024-09-11 10:03:15 +0200

    Add comments showing which sections gets the response for starlette

--- aikido_zen/sources/starlette/starlette_routing.py
+++ aikido_zen/sources/starlette/starlette_routing.py
@@ -47,6 +47,8 @@ def aik_route_func_wrapper(func):
                     return response
         except Exception as e:
             logger.debug("Exception occured in pre_response stage starlette : %s", e)
+
+        # Calling the function, check if it's async or not (same checks as in codebase starlette)
         try:
             import functools
             from starlette.concurrency import run_in_threadpool
@@ -57,7 +59,7 @@ def aik_route_func_wrapper(func):
         res = None
         if is_async_callable(func):
             res = await func(*args, **kwargs)
-        else:
+        else:  # Convert to async function and await
             res = await functools.partial(run_in_threadpool, func)(*args, **kwargs)
 
         # Code after response (post_response stage)
