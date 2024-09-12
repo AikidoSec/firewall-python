cd37ece4e07166323161ac839a8b86796084ef47
Wout Feys <wout@aikido.dev>
2024-09-12 12:06:41 +0200

    Django check version requirements using pkg_compat_check

--- aikido_zen/sources/django/__init__.py
+++ aikido_zen/sources/django/__init__.py
@@ -3,10 +3,12 @@
 import copy
 import aikido_zen.importhook as importhook
 from aikido_zen.helpers.logging import logger
-from aikido_zen.background_process.packages import add_wrapped_package
+from aikido_zen.background_process.packages import pkg_compat_check
 from ..functions.request_handler import request_handler
 from .run_init_stage import run_init_stage
 from .pre_response_middleware import pre_response_middleware
+
+REQUIRED_DJANGO_VERSION = "0.0.0"
 
 
 @importhook.on_import("django.core.handlers.base")
@@ -17,6 +19,8 @@ def on_django_gunicorn_import(django):
     # https://github.com/django/django/blob/5865ff5adcf64da03d306dc32b36e87ae6927c85/django/core/handlers/base.py#L174
     Returns : Modified django.core.handlers.base object
     """
+    if not pkg_compat_check("django", REQUIRED_DJANGO_VERSION):
+        return django
     modified_django = importhook.copy_module(django)
 
     former__get_response = copy.deepcopy(django.BaseHandler._get_response)
@@ -37,5 +41,4 @@ def on_django_gunicorn_import(django):
     setattr(modified_django.BaseHandler, "_get_response", aikido__get_response)
     setattr(django.BaseHandler, "_get_response", aikido__get_response)
 
-    add_wrapped_package("django")
     return modified_django
