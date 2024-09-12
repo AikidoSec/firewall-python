d194df460272c8b25e9ce7df38680af0b8c7b49d
Wout Feys <wout@aikido.dev>
2024-09-12 11:25:31 +0200

    Add checks for versioning in packages.py

--- aikido_zen/background_process/packages.py
+++ aikido_zen/background_process/packages.py
@@ -1,23 +1,26 @@
 """Helper functions for packages"""
 
 import importlib.metadata as metadata
+from packaging.version import Version
 from aikido_zen.helpers.logging import logger
 import aikido_zen.background_process.comms as comms
 
 MAX_REPORT_TRIES = 5
 
 
-def add_wrapped_package(pkg_name):
+def add_wrapped_package(pkg_name, required_version="0.0.0"):
     """Reports a newly wrapped package to the bg process"""
     try:
         pkg_version = metadata.version(pkg_name)
     except metadata.PackageNotFoundError:
         logger.debug(
-            "Package `%s` was wrapped but could not find a version, aborting", pkg_name
+            "Package `%s` was wrapped but could not find a version, not monkeypatching.",
+            pkg_name,
         )
-        return
+        return False  # We don't support it since we are not sure what it is.
     logger.info("Successfully wrapped package `%s` version (%s)", pkg_name, pkg_version)
     attempts = 0
+    version_support = is_version_supported(pkg_version, required_version)
     while attempts < MAX_REPORT_TRIES:
         if not comms.get_comms():
             break  # Communications have not been set up.
@@ -27,7 +30,7 @@ def add_wrapped_package(pkg_name):
                 "name": pkg_name,
                 "details": {
                     "version": pkg_version,
-                    "supported": True,  #  We can change this later if we validate package versions?
+                    "supported": version_support,
                 },
             },
             True,
@@ -35,3 +38,9 @@ def add_wrapped_package(pkg_name):
         if res["success"] and res["data"] is True:
             break
         attempts += 1
+    return version_support
+
+
+def is_version_supported(pkg_verion, required_version):
+    """Checks if the package version is supported"""
+    return Version(pkg_verion) >= Version(required_version)
