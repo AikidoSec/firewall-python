98a24f8ddd0e7cd937722383dfb1800de452b1db
Wout Feys <wout@aikido.dev>
2024-09-16 17:29:00 +0200

    Create a token_is_possibly dangerous function with empty test file

---
+++ aikido_zen/vulnerabilities/code_injection/token_is_possibly_dangerous.py
@@ -0,0 +1,45 @@
+"""Exports token_is_possibly_dangerous"""
+
+import token
+
+SAFE_OPERATIONS = [
+    # Mathematical operations :
+    "+",
+    "-",
+    "*",
+    "%",
+    "**",
+    "//",
+    # Comparison :
+    "==",
+    "!=",
+    "<=",
+    ">=",
+    "<",
+    ">",
+    # Binary mathematical operations :
+    "&",
+    "~",
+    "^",
+    "<<",
+    ">>",
+    # Regarded as safe language tokens :
+    ",",
+]
+
+
+def token_is_possibly_dangerous(tok):
+    """
+    Returns boolean indicating if the token is possibly dangerous,
+    only marks "OP" and "NAME" types as dangerous and excludes list of SAFE_OPERATIONS.
+    """
+
+    if not tok.type == token.OP and not tok.type == token.NAME:
+        # The token is neither an "OP" nor a "NAME" token, assume safe
+        return False
+
+    if tok.string in SAFE_OPERATIONS:
+        # Check if the token is a safe operation (e.g. +, -, /, ==, ...)
+        return False
+    # If it can result in injection and it's not a safe operation, return True.
+    return True
---
+++ aikido_zen/vulnerabilities/code_injection/token_is_possibly_dangerous_test.py
@@ -0,0 +1,2 @@
+import pytest
+from .token_is_possibly_dangerous import token_is_possibly_dangerous
