fbd60dd2690d1844e572a86cf8333ce0a9fa7056
Wout Feys <wout@aikido.dev>
2024-09-17 15:49:03 +0200

    Ratelimiting switch to monotonic

--- aikido_zen/helpers/get_current_unixtime_ms.py
+++ aikido_zen/helpers/get_current_unixtime_ms.py
@@ -5,6 +5,9 @@ Helper function file, see function docstring
 import time
 
 
-def get_unixtime_ms():
+def get_unixtime_ms(monotonic=False):
     """Get the current unix time but in ms"""
+    if monotonic:
+        # Return monotonic time, used for intervals.
+        return int(time.monotonic() * 1000)
     return int(time.time() * 1000)
