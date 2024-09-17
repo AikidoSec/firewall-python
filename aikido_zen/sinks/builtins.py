c2902f891345c3f14bc3b632f632409f5102805a
Wout Feys <wout@aikido.dev>
2024-09-17 10:29:01 +0200

    Wrap eval() and exec() function in builtins file

--- aikido_zen/sinks/builtins.py
+++ aikido_zen/sinks/builtins.py
@@ -18,7 +18,10 @@ def on_builtins_import(builtins):
     modified_builtins = importhook.copy_module(builtins)
 
     former_open = copy.deepcopy(builtins.open)
+    former_eval = copy.deepcopy(builtins.eval)
+    former_exec = copy.deepcopy(builtins.exec)
 
+    # Path Traversal :
     def aikido_new_open(*args, **kwargs):
         #  args[0] is the filename
         if len(args) > 0 and isinstance(args[0], (str, bytes, PurePath)):
@@ -27,7 +30,26 @@ def on_builtins_import(builtins):
             )
         return former_open(*args, **kwargs)
 
+    # Code injection :
+    def aikido_new_eval(expression, *args, **kwargs):
+        if expression and isinstance(expression, str):
+            vulns.run_vulnerability_scan(
+                kind="code_injection", op="builtins.eval", args=expression
+            )
+        return former_eval(expression, *args, **kwargs)
+
+    def aikido_new_exec(object, *args, **kwargs):
+        if object and isinstance(object, str):
+            vulns.run_vulnerability_scan(
+                kind="code_injection", op="builtins.eval", args=object
+            )
+        return former_exec(object, *args, **kwargs)
+
     # pylint: disable=no-member
     setattr(builtins, "open", aikido_new_open)
     setattr(modified_builtins, "open", aikido_new_open)
+    setattr(builtins, "eval", aikido_new_eval)
+    setattr(modified_builtins, "eval", aikido_new_eval)
+    setattr(builtins, "exec", aikido_new_exec)
+    setattr(modified_builtins, "exec", aikido_new_exec)
     return modified_builtins
