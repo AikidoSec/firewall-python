71ff396377c595615215448f609360f58865edcb
Wout Feys <wout@aikido.dev>
2024-09-18 12:02:20 +0200

    Update update_route_info to now merge two apispecs

--- aikido_zen/api_discovery/update_route_info.py
+++ aikido_zen/api_discovery/update_route_info.py
@@ -13,26 +13,27 @@ def update_route_info(context, route):
     """
     if route["hits"] <= ANALYSIS_ON_FIRST_X_ROUTES:
         # Only analyze the first x routes for api discovery
-        route["body"] = update_api_info(context, route["body"])
+        route["apispec"] = update_api_info(context, route["apispec"])
 
 
-def update_api_info(context, existing_api_info=None):
+def update_api_info(context, existing_apispec=None):
     """
     Merges two apispec objects into one, getting all properties from both schemas to capture optional properties.
     If the body info is not defined, the existing body info is returned (if any).
     If there is no existing body info, but the new body info is defined, the new body info is returned without merging.
     """
-    new_api_info = get_api_info(context)
+    new_apispec = get_api_info(context)
 
-    if new_api_info is None:
-        return existing_api_info
+    if new_apispec is None:
+        return existing_apispec
 
-    if existing_api_info is None:
-        return new_api_info
-
-    return {
-        "type": new_api_info["type"],
-        "schema": merge_data_schemas(
-            existing_api_info["schema"], new_api_info["schema"]
-        ),
-    }
+    if existing_apispec is None:
+        return new_apispec
+    if existing_apispec["body"] and new_apispec["body"]:
+        existing_schema = existing_apispec["body"].get("schema", None)
+        new_schema = new_apispec["body"].get("schema", None)
+        existing_apispec["body"] = {
+            "type": new_apispec["body"]["type"],
+            "schema": merge_data_schemas(existing_schema, new_schema),
+        }
+    return existing_apispec
