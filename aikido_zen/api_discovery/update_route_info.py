21fdd465146af130324b490d5f56b103cc161649
Wout Feys <wout@aikido.dev>
2024-09-18 14:12:01 +0200

    Update update_api_info to be clearer, try-excepted and query/auth support

--- aikido_zen/api_discovery/update_route_info.py
+++ aikido_zen/api_discovery/update_route_info.py
@@ -1,7 +1,9 @@
 """Exports update_route_info function"""
 
+from aikido_zen.helpers.logging import logger
 from .get_api_info import get_api_info
 from .merge_data_schemas import merge_data_schemas
+from .merge_auth_types import merge_auth_types
 
 ANALYSIS_ON_FIRST_X_ROUTES = 20
 
@@ -22,21 +24,38 @@ def update_api_info(context, existing_apispec=None):
     If the body info is not defined, the existing body info is returned (if any).
     If there is no existing body info, but the new body info is defined, the new body info is returned without merging.
     """
-    new_apispec = get_api_info(context)
+    try:
+        new_apispec = get_api_info(context)
 
-    if new_apispec == {}:
+        if new_apispec == {}:
+            return existing_apispec
+        if existing_apispec == {}:
+            return new_apispec
+
+        # Body :
+        existing_body = existing_apispec.get("body")
+        if existing_body and new_apispec.get("body"):
+            new_schema = new_apispec["body"].get("schema")
+            existing_apispec["body"] = {
+                "type": new_apispec["body"]["type"],
+                "schema": merge_data_schemas(existing_body["schema"], new_schema),
+            }
+        elif new_apispec.get("body"):
+            existing_apispec["body"] = new_apispec["body"]
+
+        # Query :
+        existing_query = existing_apispec.get("query")
+        if existing_query and new_apispec.get("query"):
+            existing_apispec["query"] = merge_data_schemas(
+                existing_query, new_apispec["query"]
+            )
+        elif new_apispec.get("query"):
+            existing_apispec["query"] = new_apispec["query"]
+
+        # Auth :
+        existing_apispec["auth"] = merge_auth_types(
+            existing_apispec.get("auth"), new_apispec.get("auth")
+        )
         return existing_apispec
-
-    if existing_apispec == {}:
-        return new_apispec
-
-    if existing_apispec.get("body", None) and new_apispec.get("body", None):
-        existing_schema = existing_apispec["body"].get("schema", None)
-        new_schema = new_apispec["body"].get("schema", None)
-        existing_apispec["body"] = {
-            "type": new_apispec["body"]["type"],
-            "schema": merge_data_schemas(existing_schema, new_schema),
-        }
-    elif new_apispec.get("body", None):
-        existing_apispec["body"] = new_apispec["body"]
-    return existing_apispec
+    except Exception as e:
+        logger.debug("Exception occured in update_api_info : %s", e)
