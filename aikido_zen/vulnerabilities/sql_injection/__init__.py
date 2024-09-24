a4bbddcadaee1d07d5fcc4c7133dc9bbfaeb5e1b
Wout Feys <wout@aikido.dev>
2024-09-24 15:44:21 +0200

    Update __init__.py and remove old sql injection algo

--- aikido_zen/vulnerabilities/sql_injection/__init__.py
+++ aikido_zen/vulnerabilities/sql_injection/__init__.py
@@ -2,41 +2,55 @@
 SQL Injection algorithm
 """
 
-from aikido_zen.vulnerabilities.sql_injection.dialects import MySQL
-from aikido_zen.vulnerabilities.sql_injection.query_contains_user_input import (
-    query_contains_user_input,
-)
-from aikido_zen.vulnerabilities.sql_injection.userinput_contains_sql_syntax import (
-    userinput_contains_sqlsyntax,
-)
-from aikido_zen.vulnerabilities.sql_injection.uinput_occ_safely_encapsulated import (
-    uinput_occ_safely_encapsulated,
-)
+import ctypes
 from aikido_zen.helpers.logging import logger
+
+zen_vulns_lib = ctypes.CDLL("zen_vulns_lib/libzen_rustlib.so")
 
 
 def detect_sql_injection(query, user_input, dialect):
     """
     Execute this to check if the query is actually a SQL injection
     """
-    if len(user_input) <= 1:
-        # We ignore single characters since they are only able to crash the SQL Server,
-        # And don't pose a big threat.
+    if should_return_early(query, user_input):
         return False
 
-    if len(user_input) > len(query):
-        # We ignore cases where the user input is longer than the query.
-        # Because the user input can't be part of the query.
-        return False
-
-    if not query_contains_user_input(query, user_input):
-        # If the user input is not part of the query, return false (No need to check)
-        return False
-
-    if uinput_occ_safely_encapsulated(query, user_input):
-        return False
-    # Executing our final check with the massive RegEx
-    logger.debug(
-        "detect_sql_injection : No reason to return false with : %s", user_input
+    query_bytes = query.encode("utf-8")
+    userinput_bytes = user_input.encode("utf-8")
+    dialect_int = 0
+    c_int_res = zen_vulns_lib.detect_sql_injection(
+        query_bytes, userinput_bytes, dialect_int
     )
-    return userinput_contains_sqlsyntax(user_input, dialect)
+    return bool(c_int_res)
+
+
+def should_return_early(query, user_input):
+    """
+    Returns true if the detect_sql_injection algo should return early :
+    - user_input smaller than 3 chars or user input larger than query
+    - user_input not in query
+    - user_input is alphanumerical
+    - user_input is an array of integers
+    """
+    if len(user_input) <= 3 or len(query) < len(user_input):
+        # User input too small or larger than query, returning
+        return True
+
+    # Lowercase :
+    query_l = query.lower()
+    user_input_l = user_input.lower()
+    if not query_l.contains(user_input_l):
+        # User input not in query, returning
+        return True
+
+    if user_input_l.isalnum():
+        # User input is alphanumerical, returning
+        return True
+
+    cleaned_input_for_list = user_input_l.replace(" ", "").replace(",", "")
+    try:
+        int(cleaned_input_for_list)
+        # Should return early since this is a comma seperated list
+        return True
+    except ValueError:
+        return False
