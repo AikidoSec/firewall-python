9781c78c71a76f1c1e1a06914132b4570cfd4b60
Wout Feys <wout@aikido.dev>
2024-09-24 10:49:09 +0200

    Use new helper function everywhere in vulns

--- aikido_zen/vulnerabilities/path_traversal/check_context_for_path_traversal.py
+++ aikido_zen/vulnerabilities/path_traversal/check_context_for_path_traversal.py
@@ -1,10 +1,6 @@
 """Exports `check_context_for_path_traversal`"""
 
-from aikido_zen.helpers.extract_strings_from_user_input import (
-    extract_strings_from_user_input_cached,
-)
-from aikido_zen.helpers.logging import logger
-from aikido_zen.context import UINPUT_SOURCES as SOURCES
+from aikido_zen.helpers.extract_strings_from_context import extract_strings_from_context
 from aikido_zen.helpers.try_parse_url import try_parse_url
 from aikido_zen.helpers.path_to_string import path_to_string
 from .detect_path_traversal import detect_path_traversal
@@ -21,21 +17,14 @@ def check_context_for_path_traversal(
     if not path_string:
         return {}
 
-    for source in SOURCES:
-        if hasattr(context, source):
-            user_inputs = extract_strings_from_user_input_cached(
-                getattr(context, source), source
-            )
-            for user_input, path in user_inputs.items():
-                if detect_path_traversal(
-                    path_string, user_input, check_path_start, is_url
-                ):
-                    return {
-                        "operation": operation,
-                        "kind": "path_traversal",
-                        "source": source,
-                        "pathToPayload": path,
-                        "metadata": {"filename": path_string},
-                        "payload": user_input,
-                    }
+    for user_input, path, source in extract_strings_from_context(context):
+        if detect_path_traversal(path_string, user_input, check_path_start, is_url):
+            return {
+                "operation": operation,
+                "kind": "path_traversal",
+                "source": source,
+                "pathToPayload": path,
+                "metadata": {"filename": path_string},
+                "payload": user_input,
+            }
     return {}
--- aikido_zen/vulnerabilities/sql_injection/context_contains_sql_injection.py
+++ aikido_zen/vulnerabilities/sql_injection/context_contains_sql_injection.py
@@ -2,13 +2,8 @@
 This will check the context of the request for SQL Injections
 """
 
-import json
-from aikido_zen.helpers.extract_strings_from_user_input import (
-    extract_strings_from_user_input_cached,
-)
+from aikido_zen.helpers.extract_strings_from_context import extract_strings_from_context
 from aikido_zen.vulnerabilities.sql_injection import detect_sql_injection
-from aikido_zen.helpers.logging import logger
-from aikido_zen.context import UINPUT_SOURCES as SOURCES
 
 
 def context_contains_sql_injection(sql, operation, context, dialect):
@@ -18,19 +13,14 @@ def context_contains_sql_injection(sql, operation, context, dialect):
     if not isinstance(sql, str):
         # Only supports SQL queries that are strings, return otherwise.
         return {}
-    for source in SOURCES:
-        if hasattr(context, source):
-            user_inputs = extract_strings_from_user_input_cached(
-                getattr(context, source), source
-            )
-            for user_input, path in user_inputs.items():
-                if detect_sql_injection(sql, user_input, dialect):
-                    return {
-                        "operation": operation,
-                        "kind": "sql_injection",
-                        "source": source,
-                        "pathToPayload": path,
-                        "metadata": {"sql": sql},
-                        "payload": user_input,
-                    }
+    for user_input, path, source in extract_strings_from_context(context):
+        if detect_sql_injection(sql, user_input, dialect):
+            return {
+                "operation": operation,
+                "kind": "sql_injection",
+                "source": source,
+                "pathToPayload": path,
+                "metadata": {"sql": sql},
+                "payload": user_input,
+            }
     return {}
