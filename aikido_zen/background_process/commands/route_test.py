dacd7f6771f92b3c8d7bcf73b890232f7b850578
Wout Feys <wout@aikido.dev>
2024-10-03 09:54:19 +0200

    Update testing of the route command

--- aikido_zen/background_process/commands/route_test.py
+++ aikido_zen/background_process/commands/route_test.py
@@ -13,19 +13,22 @@ def mock_connection_manager():
 
 def test_process_route_adds_route(mock_connection_manager):
     """Test that process_route adds a route when connection_manager is present."""
-    data = 123456789
+    data = {"route_metadata": 123456, "apispec": 2345}
 
     process_route(
         mock_connection_manager, data, None
     )  # conn is not used in this function
 
     # Check that the add_route method was called with the correct arguments
-    mock_connection_manager.routes.add_route.assert_called_once_with(context=123456789)
+    mock_connection_manager.routes.add_route.assert_called_once_with(123456)
+    mock_connection_manager.routes.update_route_with_apispec.assert_called_once_with(
+        123456, 2345
+    )
 
 
 def test_process_route_no_connection_manager():
     """Test that process_route does nothing when connection_manager is not present."""
-    data = 123456789
+    data = {"route_metadata": 123456, "apispec": 2345}
 
     process_route(None, data, None)  # conn is not used in this function
 
