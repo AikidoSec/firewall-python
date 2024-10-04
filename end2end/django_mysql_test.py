dec88b7b443344661a3dd4e2c1b2d883238c6e3a
Wout Feys <wout@aikido.dev>
2024-10-04 14:39:27 +0200

    Add end2end tests to django-mysql

--- end2end/django_mysql_test.py
+++ end2end/django_mysql_test.py
@@ -1,14 +1,22 @@
+import time
 import pytest
 import requests
+from .server.check_events_from_mock import fetch_events_from_mock, validate_started_event, filter_on_event_type
+
 # e2e tests for django_mysql sample app
 post_url_fw = "http://localhost:8080/app/create"
 post_url_nofw = "http://localhost:8081/app/create"
+
+def test_firewall_started_okay():
+    events = fetch_events_from_mock("http://localhost:5000")
+    started_events = filter_on_event_type(events, "started")
+    assert len(started_events) == 1
+    validate_started_event(started_events[0], ["django", "mysqlclient"])
 
 def test_safe_response_with_firewall():
     dog_name = "Bobby Tables"
     res = requests.post(post_url_fw, data={'dog_name': dog_name})
     assert res.status_code == 200
-
 
 def test_safe_response_without_firewall():
     dog_name = "Bobby Tables"
@@ -20,6 +28,22 @@ def test_dangerous_response_with_firewall():
     dog_name = 'Dangerous bobby", 1); -- '
     res = requests.post(post_url_fw, data={'dog_name': dog_name})
     assert res.status_code == 500
+    time.sleep(5) # Wait for attack to be reported
+    events = fetch_events_from_mock("http://localhost:5000")
+    attacks = filter_on_event_type(events, "detected_attack")
+    
+    assert len(attacks) == 1
+    del attacks[0]["attack"]["stack"]
+    assert attacks[0]["attack"] == {
+        "blocked": True,
+        "kind": "sql_injection",
+        'metadata': {'sql': 'INSERT INTO sample_app_dogs (dog_name, dog_boss) VALUES ("Dangerous bobby", 1); -- ", "N/A")'},
+        'operation': 'MySQLdb.Cursor.execute',
+        'pathToPayload': '.dog_name',
+        'payload': '"Dangerous bobby\\", 1); -- "',
+        'source': "body",
+        'user': None
+    }
 
 def test_dangerous_response_without_firewall():
     dog_name = 'Dangerous bobby", 1); -- '
