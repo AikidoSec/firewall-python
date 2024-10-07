9f7b16ba5c6ceb3ae4229ba0ebc481cfb42af741
Wout Feys <wout@aikido.dev>
2024-10-07 16:53:02 +0200

    Cleanup and minor bugfixes on thread_cache.py file

--- aikido_zen/thread/thread_cache.py
+++ aikido_zen/thread/thread_cache.py
@@ -2,11 +2,10 @@
 
 from threading import local
 import aikido_zen.background_process.comms as comms
-from aikido_zen.helpers.logging import logger
 from aikido_zen.helpers.get_current_unixtime_ms import get_unixtime_ms
 from aikido_zen.background_process.routes import Routes
 
-THREAD_CONFIG_TLL_MS = 60 * 1000  # Time-To-Live is 60 seconds for the thread cache
+THREAD_CONFIG_TTL_MS = 60 * 1000  # Time-To-Live is 60 seconds for the thread cache
 
 threadlocal_storage = local()
 
@@ -14,12 +13,17 @@ threadlocal_storage = local()
 def get_cache():
     """Returns the current ThreadCache"""
     cache = getattr(threadlocal_storage, "cache", None)
-    if cache:
+    if cache and isinstance(cache, ThreadCache):
         cache.renew_if_ttl_expired()
     return cache
 
 
 class ThreadCache:
+    """
+    A thread-local cache object that holds routes, bypassed ips, endpoints amount of requests
+    With a Time-To-Live given by THREAD_CONFIG_TTL_MS
+    """
+
     def __init__(self):
         # Load initial data :
         self.reset()
@@ -35,7 +39,7 @@ class ThreadCache:
     def renew_if_ttl_expired(self):
         """Renews the data only if TTL has expired"""
         ttl_has_expired = (
-            get_unixtime_ms(monotonic=True) - self.last_renewal > THREAD_CONFIG_TLL_MS
+            get_unixtime_ms(monotonic=True) - self.last_renewal > THREAD_CONFIG_TTL_MS
         )
         if ttl_has_expired:
             self.renew()
@@ -46,11 +50,14 @@ class ThreadCache:
         self.bypassed_ips = set()
         self.endpoints = []
         self.reqs = 0
+        self.last_renewal = 0
 
     def renew(self):
         """
         Makes an IPC call to store the amount of hits and requests and renew the config
         """
+        if not comms.get_comms():
+            return
         res = comms.get_comms().send_data_to_bg_process(
             action="RENEW_CONFIG",
             obj={"current_routes": dict(self.routes.routes), "reqs": self.reqs},
@@ -66,8 +73,6 @@ class ThreadCache:
             if isinstance(res["data"]["routes"], dict):
                 self.routes.routes = res["data"]["routes"]
             self.last_renewal = get_unixtime_ms(monotonic=True)
-        else:
-            self.last_renewal = 0
 
     def increment_stats(self):
         """Increments the requests"""
