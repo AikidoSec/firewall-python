7b07fffcf205684bfdf21bd5a87a9aaa0e57f677
Wout Feys <wout@aikido.dev>
2024-10-07 16:51:31 +0200

    Update vulnerabilities init_test.py file

--- aikido_zen/vulnerabilities/init_test.py
+++ aikido_zen/vulnerabilities/init_test.py
@@ -2,11 +2,8 @@ import pytest
 from unittest.mock import MagicMock, patch
 from . import run_vulnerability_scan
 from aikido_zen.context import current_context, Context
-from aikido_zen.background_process.ipc_lifecycle_cache import (
-    ipc_lifecycle_cache,
-    IPCLifecycleCache,
-)
 from aikido_zen.errors import AikidoSQLInjection
+from aikido_zen.thread.thread_cache import ThreadCache, threadlocal_storage
 
 
 @pytest.fixture
@@ -41,52 +38,40 @@ def get_context():
 
 def test_run_vulnerability_scan_no_context(caplog):
     current_context.set(None)
-    ipc_lifecycle_cache.set(1)
+    threadlocal_storage.cache = 1
     run_vulnerability_scan(kind="test", op="test", args=tuple())
     assert len(caplog.text) == 0
 
 
 def test_run_vulnerability_scan_no_context_no_lifecycle(caplog):
     current_context.set(None)
-    ipc_lifecycle_cache.set(None)
+    threadlocal_storage.cache = None
     run_vulnerability_scan(kind="test", op="test", args=tuple())
     assert len(caplog.text) == 0
 
 
 def test_run_vulnerability_scan_context_no_lifecycle(caplog):
     current_context.set(1)
-    ipc_lifecycle_cache.set(None)
-    run_vulnerability_scan(kind="test", op="test", args=tuple())
-    assert len(caplog.text) == 0
-
-
-def test_lifecycle_cache_parts(caplog, get_context):
-    get_context.set_as_current_context()
-    cache = IPCLifecycleCache(get_context)
-    cache.matched_endpoints = [{"forceProtectionOff": True}]
-    assert cache.protection_forced_off()
+    threadlocal_storage.cache = None
     run_vulnerability_scan(kind="test", op="test", args=tuple())
     assert len(caplog.text) == 0
 
 
 def test_lifecycle_cache_ok(caplog, get_context):
     get_context.set_as_current_context()
-    cache = IPCLifecycleCache(get_context)
-    assert not cache.protection_forced_off()
+    cache = ThreadCache()
     run_vulnerability_scan(kind="test", op="test", args=tuple())
     assert "Vulnerability type test currently has no scans implemented" in caplog.text
 
 
 def test_ssrf(caplog, get_context):
     current_context.set(None)
-    cache = IPCLifecycleCache(get_context)
-    assert not cache.protection_forced_off()
     run_vulnerability_scan(kind="ssrf", op="test", args=tuple())
 
 
 def test_lifecycle_cache_bypassed_ip(caplog, get_context):
     get_context.set_as_current_context()
-    cache = IPCLifecycleCache(get_context)
+    cache = ThreadCache()
     cache.bypassed_ips = {"198.51.100.23"}
     assert cache.is_bypassed_ip("198.51.100.23")
     run_vulnerability_scan(kind="test", op="test", args=tuple())
@@ -97,8 +82,7 @@ def test_sql_injection(caplog, get_context, monkeypatch):
     from aikido_zen.vulnerabilities.sql_injection.dialects import MySQL
 
     get_context.set_as_current_context()
-    cache = IPCLifecycleCache(get_context)
-    assert not cache.protection_forced_off()
+    cache = ThreadCache()
     monkeypatch.setenv("AIKIDO_BLOCKING", "1")
     with pytest.raises(AikidoSQLInjection):
         run_vulnerability_scan(
@@ -112,8 +96,8 @@ def test_sql_injection_with_comms(caplog, get_context, monkeypatch):
     from aikido_zen.vulnerabilities.sql_injection.dialects import MySQL
 
     get_context.set_as_current_context()
-    cache = IPCLifecycleCache(get_context)
-    assert not cache.protection_forced_off()
+    cache = ThreadCache()
+    cache.last_renewal = 9999999999999999999999
     monkeypatch.setenv("AIKIDO_BLOCKING", "1")
     with patch("aikido_zen.background_process.comms.get_comms") as mock_get_comms:
         # Create a mock comms object
@@ -139,8 +123,6 @@ def test_ssrf_with_comms_hostnames_add(caplog, get_context, monkeypatch):
     from aikido_zen.vulnerabilities.sql_injection.dialects import MySQL
 
     get_context.set_as_current_context()
-    cache = IPCLifecycleCache(get_context)
-    assert not cache.protection_forced_off()
     monkeypatch.setenv("AIKIDO_BLOCKING", "1")
     with patch("aikido_zen.background_process.comms.get_comms") as mock_get_comms:
         # Create a mock comms object
