4ce8b80f4444cbb8957ef3fdc47b8b9583bef0f6
Wout Feys <wout@aikido.dev>
2024-10-08 09:54:35 +0200

    In renew_config merge/update existing route data

--- aikido_zen/background_process/commands/renew_config.py
+++ aikido_zen/background_process/commands/renew_config.py
@@ -1,10 +1,24 @@
 """Exports process_renew_config"""
 
+from aikido_zen.api_discovery.update_route_info import update_route_info
+
 
 def process_renew_config(connection_manager, data, conn, queue=None):
     """Fetches all config data needed for thread-local cache"""
 
     # Process data here.
+    routes = connection_manager.routes
+    for route_key, route in data.items():
+        if not routes.get(key=route_key):
+            routes.initialize_route(key=route_key)
+        existing_route = routes.get(key=route_key)
+
+        # Update hit count :
+        thread_hits = int(route["thread_hits"])
+        existing_route["hits"] += thread_hits
+
+        # Update API Spec :
+        update_route_info(route["apispec"], existing_route)
 
     return {
         "routes": list(connection_manager.routes),
