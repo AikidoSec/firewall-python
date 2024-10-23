00fbb5609867b6e87e203afe3b0c99f418d06dbc
Hans Ott <hansott@hotmail.be>
2024-10-23 12:07:43 +0200

    Use `logger.debug`

--- aikido_zen/vulnerabilities/sql_injection/__init__.py
+++ aikido_zen/vulnerabilities/sql_injection/__init__.py
@@ -31,7 +31,7 @@ def detect_sql_injection(query, user_input, dialect):
 
     # This means that an error occurred in the library
     if (c_int_res == 2):
-        logger.error("Unable to check for SQL Injection, an error occurred in the library")
+        logger.debug("Unable to check for SQL Injection, an error occurred in the library")
         return False
 
     return bool(c_int_res)
