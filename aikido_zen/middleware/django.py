a14a567f8629ea5c5bc37544f80a640c0a35ede9
Wout Feys <wout@aikido.dev>
2024-10-28 10:34:08 +0100

    Linting for django.py file

--- aikido_zen/middleware/django.py
+++ aikido_zen/middleware/django.py
@@ -1,18 +1,23 @@
 """Exports AikidoDjangoMiddleware"""
+
 from aikido_zen.helpers.logging import logger
 from .should_block_request import should_block_request
 
 
 class AikidoDjangoMiddleware:
     """Middleware for rate-limiting and user blocking for django"""
+
     def __init__(self, get_response):
         logger.critical("Django middleware ised")
         self.get_response = get_response
         try:
             from django.http import HttpResponse
+
             self.HttpResponse = HttpResponse
         except ImportError:
-            logger.warning("django.http import not working, aikido rate-limiting middleware not running.")
+            logger.warning(
+                "django.http import not working, aikido rate-limiting middleware not running."
+            )
 
     def __call__(self, request):
         result = should_block_request()
@@ -25,6 +30,8 @@ class AikidoDjangoMiddleware:
                 message += " (Your IP: " + result["ip"] + ")"
             return self.HttpResponse(message, content_type="text/plain", status=429)
         elif result["type"] == "blocked":
-            return self.HttpResponse("You are blocked by Zen.", content_type="text/plain", status=403)
+            return self.HttpResponse(
+                "You are blocked by Zen.", content_type="text/plain", status=403
+            )
         logger.debug("Unknown type for blocking request: %s", result["type"])
         return self.get_response(request)
