8723847a5d5efef563f1db9c29ca27665ce1c48f
Wout Feys <wout@aikido.dev>
2024-10-28 09:06:00 +0100

    Also still export should_block_request

--- aikido_zen/middleware/__init__.py
+++ aikido_zen/middleware/__init__.py
@@ -3,3 +3,4 @@
 from .asgi import AikidoASGIMiddleware as AikidoQuartMiddleware
 from .asgi import AikidoASGIMiddleware as AikidoStarletteMiddleware
 from .flask import AikidoFlaskMiddleware
+from .should_block_request import should_block_request
