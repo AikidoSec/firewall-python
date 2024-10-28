97b020e4cd2078160419c8ff0060734401be74c9
Wout Feys <wout@aikido.dev>
2024-10-28 09:03:20 +0100

    Update flask middleware with rename and dynamic import to avoid crashes

--- aikido_zen/middleware/flask.py
+++ aikido_zen/middleware/flask.py
@@ -1,29 +1,33 @@
 """Exports ratelimiting and user blocking middleware for Flask"""
 
 from aikido_zen.helpers.logging import logger
-from werkzeug.wrappers import Response
-from . import should_block_request
+from .should_block_request import should_block_request
 
 
-class AikidoMiddleware:
+class AikidoFlaskMiddleware:
     """Ratelimiting and user blocking middleware for Flask"""
 
     def __init__(self, app):
         self.app = app
+        try:
+            from werkzeug.wrappers import Response
+            self.Response = Response
+        except ImportError:
+            logger.warning("Something went wrong whilst importing werkzeug.wrappers, middleware does not work")
 
     def __call__(self, environ, start_response):
         result = should_block_request()
-        if result["block"] is not True:
+        if result["block"] is not True or self.Response is None:
             return self.app(environ, start_response)
 
         if result["type"] == "ratelimited":
             message = "You are rate limited by Zen."
             if result["trigger"] == "ip" and result["ip"]:
                 message += " (Your IP: " + result["ip"] + ")"
-            res = Response(message, mimetype="text/plain", status=429)
+            res = self.Response(message, mimetype="text/plain", status=429)
             return res(environ, start_response)
         elif result["type"] == "blocked":
-            res = Response("You are blocked by Zen.", mimetype="text/plain", status=403)
+            res = self.Response("You are blocked by Zen.", mimetype="text/plain", status=403)
             return res(environ, start_response)
         logger.debug("Unknown type for blocking request: %s", result["type"])
         return self.app(environ, start_response)
