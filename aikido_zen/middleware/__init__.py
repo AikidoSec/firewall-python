63c29bbfcadf061a3ae9f5cab0b9a0b7a81d2731
Wout Feys <wout@aikido.dev>
2024-10-28 10:23:29 +0100

    Update django docs and create django middleware

--- aikido_zen/middleware/__init__.py
+++ aikido_zen/middleware/__init__.py
@@ -3,4 +3,5 @@
 from .asgi import AikidoASGIMiddleware as AikidoQuartMiddleware
 from .asgi import AikidoASGIMiddleware as AikidoStarletteMiddleware
 from .flask import AikidoFlaskMiddleware
+from .django import AikidoDjangoMiddleware
 from .should_block_request import should_block_request
