9bfc63888fa8b77794740512f99650e389690a97
Wout Feys <wout@aikido.dev>
2024-11-06 12:05:55 +0100

    Improve extract_route_params with unqoute/quote and by using build_route

--- aikido_zen/context/extract_route_params.py
+++ aikido_zen/context/extract_route_params.py
@@ -1,19 +1,28 @@
 """Exports extract_route_params function"""
 
-from urllib.parse import unquote
+from urllib.parse import quote, unquote
 from aikido_zen.helpers.try_parse_url_path import try_parse_url_path
+from aikido_zen.helpers.build_route_from_url import replace_url_segment_with_param
 
 
 def extract_route_params(url):
     """Will try and build an array of user input based on the url"""
     results = []
+
     path = try_parse_url_path(url)
     segments = path.split("/")
     for segment in segments:
+        segment = unquote(segment)
+        print(segment)
+        print(quote(segment))
         if segment.isalnum():
             continue  # Ignore alphanumerical parts of the url
-        unqouted_segment = unquote(segment)
-        if segment is not unqouted_segment:
-            # URL Encoded part, see this as user input
-            results.append(unqouted_segment)
+
+        if segment is not quote(segment):
+            results.append(segment)  # This is not a standard piece of the URL
+        elif replace_url_segment_with_param(segment) is not segment:
+            results.append(segment)  # Might be a secret, a hash, ...
+
+    if len(path) > 1:
+        results.append(path[1:])  # Add path after slash as user input
     return results
--- aikido_zen/context/extract_route_params_test.py
+++ aikido_zen/context/extract_route_params_test.py
@@ -4,4 +4,24 @@ from .extract_route_params import extract_route_params
 
 def test_with_urlencoded_urls():
     url1 = "http://localhost:8080/app/shell/ls%20-la"
-    assert extract_route_params(url1) == ["ls -la"]
+    assert extract_route_params(url1) == ["ls -la", "app/shell/ls%20-la"]
+
+    url2 = "http://localhost:8080/app/shell/ls -la"
+    assert extract_route_params(url2) == ["ls -la", "app/shell/ls -la"]
+
+
+def test_uses_keys():
+    url = "http://localhost:8080/app/shell/me@woutfeys.be/017shell/127.0.0.1/"
+    assert extract_route_params(url) == [
+        "me@woutfeys.be",
+        "127.0.0.1",
+        "app/shell/me@woutfeys.be/017shell/127.0.0.1/",
+    ]
+
+
+def test_with_empty_route():
+    url1 = "http://localhost:8080"
+    assert extract_route_params(url1) == []
+
+    url2 = "http://localhost:8080"
+    assert extract_route_params(url2) == []
