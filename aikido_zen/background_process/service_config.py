34986fc8ae9600c9d3864b952048bc0c83d7c335
Wout Feys <wout@aikido.dev>
2024-11-14 17:22:27 +0100

    Update the service config to store blocked_ips

--- aikido_zen/background_process/service_config.py
+++ aikido_zen/background_process/service_config.py
@@ -2,14 +2,22 @@
 Exports ServiceConfig class
 """
 
+from aikido_zen.helpers.add_ip_address_to_blocklist import add_ip_address_to_blocklist
 from aikido_zen.helpers.match_endpoints import match_endpoints
+from aikido_zen.helpers.blocklist import BlockList
 
 
 class ServiceConfig:
     """Class holding the config of the connection_manager"""
 
     def __init__(
-        self, endpoints, last_updated_at, blocked_uids, bypassed_ips, received_any_stats
+        self,
+        endpoints,
+        last_updated_at,
+        blocked_uids,
+        bypassed_ips,
+        received_any_stats,
+        blocked_ips,
     ):
         self.endpoints = [
             endpoint for endpoint in endpoints if not endpoint.get("graphql")
@@ -18,6 +26,9 @@ class ServiceConfig:
         self.bypassed_ips = set(bypassed_ips)
         self.blocked_uids = set(blocked_uids)
         self.received_any_stats = bool(received_any_stats)
+        self.blocked_ips = BlockList()
+        for blocked_ip in blocked_ips:
+            add_ip_address_to_blocklist(blocked_ip, self.blocked_ips)
 
     def get_endpoints(self, route_metadata):
         """
@@ -29,3 +40,6 @@ class ServiceConfig:
     def is_bypassed_ip(self, ip):
         """Checks if the IP is on the bypass list"""
         return ip in self.bypassed_ips
+
+    def is_blocked_ip(self, ip):
+        return self.blocked_ips.is_blocked(ip)
--- aikido_zen/background_process/service_config_test.py
+++ aikido_zen/background_process/service_config_test.py
@@ -45,6 +45,7 @@ def test_service_config_initialization():
         ["0", "0", "1", "5"],
         ["5", "1", "2", "1", "5"],
         True,
+        [],
     )
 
     # Check that non-GraphQL endpoints are correctly filtered
@@ -72,6 +73,7 @@ def service_config():
         blocked_uids=["user1", "user2"],
         bypassed_ips=["192.168.1.1", "10.0.0.1"],
         received_any_stats=True,
+        blocked_ips=[],
     )
 
 
@@ -80,3 +82,30 @@ def test_initialization(service_config):
     assert service_config.last_updated_at == "2023-10-01T00:00:00Z"
     assert service_config.bypassed_ips == {"192.168.1.1", "10.0.0.1"}
     assert service_config.blocked_uids == {"user1", "user2"}
+
+
+def test_ip_blocking():
+    config = ServiceConfig(
+        endpoints=sample_endpoints,
+        last_updated_at="2023-10-01T00:00:00Z",
+        blocked_uids=["user1", "user2"],
+        bypassed_ips=["192.168.1.1", "10.0.0.1"],
+        received_any_stats=True,
+        blocked_ips=[
+            "1.2.3.4",
+            "192.168.2.1/24",
+            "fd00:1234:5678:9abc::1",
+            "fd00:3234:5678:9abc::1/64",
+            "5.6.7.8/32",
+        ],
+    )
+
+    assert config.is_blocked_ip("1.2.3.4") is True
+    assert config.is_blocked_ip("2.3.4.5") is False
+    assert config.is_blocked_ip("192.168.2.2") is True
+    assert config.is_blocked_ip("fd00:1234:5678:9abc::1") is True
+    assert config.is_blocked_ip("fd00:1234:5678:9abc::2") is False
+    assert config.is_blocked_ip("fd00:3234:5678:9abc::1") is True
+    assert config.is_blocked_ip("fd00:3234:5678:9abc::2") is True
+    assert config.is_blocked_ip("5.6.7.8") is True
+    assert config.is_blocked_ip("1.2") is False
