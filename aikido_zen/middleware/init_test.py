87782c1f508db8da033c0b606443726e8339381b
Wout Feys <wout@aikido.dev>
2024-11-28 16:08:37 +0100

    Fix middleware tests

--- aikido_zen/middleware/init_test.py
+++ aikido_zen/middleware/init_test.py
@@ -52,7 +52,7 @@ def test_with_context_without_cache():
 
 def test_with_context_with_cache():
     set_context(user={"id": "123"})
-    threadCache = MyThreadCache()
+    thread_cache = MyThreadCache()
 
     thread_cache.config.blocked_uids = ["123"]
     assert get_current_context().executed_middleware == False
@@ -73,9 +73,9 @@ def test_with_context_with_cache():
 
 def test_cache_comms_with_endpoints():
     set_context(user={"id": "456"})
-    threadCache = MyThreadCache()
-    threadCache.blocked_uids = ["123"]
-    threadCache.endpoints = [
+    thread_cache = MyThreadCache()
+    thread_cache.config.blocked_uids = ["123"]
+    thread_cache.config.endpoints = [
         {
             "method": "POST",
             "route": "/login",
@@ -102,7 +102,7 @@ def test_cache_comms_with_endpoints():
         assert should_block_request() == {"block": False}
         mock_comms.send_data_to_bg_process.assert_not_called()
 
-    threadCache.endpoints.append(
+    thread_cache.config.endpoints.append(
         {
             "method": "POST",
             "route": "/posts/:id",
@@ -124,7 +124,7 @@ def test_cache_comms_with_endpoints():
         mock_comms.send_data_to_bg_process.assert_not_called()
 
     # Enable ratelimiting
-    threadCache.endpoints[1]["rateLimiting"]["enabled"] = True
+    thread_cache.config.endpoints[1]["rateLimiting"]["enabled"] = True
 
     with patch("aikido_zen.background_process.comms.get_comms") as mock_get_comms:
         mock_comms = MagicMock()
