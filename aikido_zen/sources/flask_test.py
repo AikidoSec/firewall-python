a79a967653c1de82652d7ecb6dab3824be08c8c1
nadiraikido <166383531+nadiraikido@users.noreply.github.com>
2024-12-12 17:37:11 +0100

    Code coverage

--- aikido_zen/sources/flask_test.py
+++ aikido_zen/sources/flask_test.py
@@ -10,11 +10,12 @@ sample_environ = {
     "HTTP_HEADER_1": "header 1 value",
     "HTTP_HEADER_2": "Header 2 value",
     "RANDOM_VALUE": "Random value",
-    "HTTP_COOKIE": "sessionId=abc123xyz456;",
+    "HTTP_COOKIE": "sessionklalkdlkjasdlkj@@as2@@d;a@@2sd=asd@@2;as@2d;'asd'aksksId=abc123xyz456;",
     "wsgi.url_scheme": "https",
     "HTTP_HOST": "example.com",
     "PATH_INFO": "/hello",
     "QUERY_STRING": "user=JohnDoe&age=30&age=35",
+    "body": "username=JohnDoe&age=30&age=35",
     "CONTENT_TYPE": "application/x-www-form-urlencoded",
     "REMOTE_ADDR": "198.51.100.23",
 }
@@ -45,6 +46,43 @@ def timeout_handler(signum, frame):
 
 signal.signal(signal.SIGALRM, timeout_handler)
 
+def test_flask_all_3_func_with_invalid_body():
+    """When the flask body can not be parsed (because it contains invalid json for example), we should still parse the cookies of the endpoint"""
+    with patch(
+        "aikido_zen.sources.functions.request_handler.request_handler"
+    ) as mock_request_handler:
+        reset_comms()
+        current_context.set(None)
+        mock_request_handler.return_value = None
+
+        from flask import Flask
+
+        app = Flask(__name__)
+        try:
+            signal.alarm(1)
+            app(sample_environ_malformed_json, lambda x, y: x)
+            app.run()
+        except TimeoutException:
+            pass
+
+        assert get_current_context().method == "POST"
+        assert (
+            get_current_context().body == None
+        )  # body is None since it's invalid json
+        assert get_current_context().headers == {
+            "COOKIE": "sessionId=abc123xyz456;",
+            "HEADER_1": "header 1 value",
+            "HEADER_2": "Header 2 value",
+            "HOST": "example.com",
+            "CONTENT_TYPE": "application/json",
+        }
+        calls = mock_request_handler.call_args_list
+        assert len(calls) == 3
+        assert calls[0][1]["stage"] == "init"
+        assert calls[1][1]["stage"] == "pre_response"
+        assert calls[2][1]["stage"] == "post_response"
+        assert calls[2][1]["status_code"] == 404
+        
 
 def test_flask_all_3_func_with_invalid_body():
     """When the flask body can not be parsed (because it contains invalid json for example), we should still parse the cookies of the endpoint"""
@@ -104,7 +142,7 @@ def test_flask_all_3_func():
         assert get_current_context().method == "POST"
         assert get_current_context().body == None
         assert get_current_context().headers == {
-            "COOKIE": "sessionId=abc123xyz456;",
+            "COOKIE": "sessionklalkdlkjasdlkj@@as2@@d;a@@2sd=asd@@2;as@2d;'asd'aksksId=abc123xyz456;",
             "HEADER_1": "header 1 value",
             "HEADER_2": "Header 2 value",
             "HOST": "example.com",
