7273413e800752b1343c3d1be7c9ca3e5de555cc
BitterPanda63 <bitterpanda@proton.me>
2025-01-22 11:31:00 +0100

    AIKIDO_DISABLED -> AIKIDO_DISABLE

--- aikido_zen/helpers/aikido_disabled_flag_active.py
+++ aikido_zen/helpers/aikido_disabled_flag_active.py
@@ -7,9 +7,9 @@ import os
 
 def aikido_disabled_flag_active():
     """
-    Checks the environment variable "AIKIDO_DISABLED"
+    Checks the environment variable "AIKIDO_DISABLE"
     """
-    aikido_disabled_env = os.getenv("AIKIDO_DISABLED")
+    aikido_disabled_env = os.getenv("AIKIDO_DISABLE")
     if aikido_disabled_env is not None:
         return aikido_disabled_env.lower() in ["true", "1"]
     return False
