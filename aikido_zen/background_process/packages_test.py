e17f40024283d727264b0bbb8465b5d12c46bae5
Wout Feys <bitterpanda@proton.me>
2025-02-24 11:24:55 +0100

    Remove wrapped package

--- aikido_zen/background_process/packages_test.py
+++ aikido_zen/background_process/packages_test.py
@@ -36,18 +36,6 @@ def test_pkg_compat_check_success(mock_get_comms, mocker):
     # Call the function under test
     pkg_compat_check(pkg_name, ANY_VERSION)
 
-    # Assert that the version was retrieved and the send_data_to_bg_process was called
-    assert mock_class.action == "WRAPPED_PACKAGE"
-    assert mock_class.obj == {
-        "name": pkg_name,
-        "details": {
-            "version": pkg_version,
-            "supported": True,
-        },
-    }
-    assert mock_class.receive == True
-
-
 def test_pkg_compat_check_retry(mock_get_comms, mocker):
     """Test retry logic when sending data fails."""
     pkg_name = "example_package"
@@ -71,9 +59,6 @@ def test_pkg_compat_check_retry(mock_get_comms, mocker):
 
     # Call the function under test
     pkg_compat_check(pkg_name, ANY_VERSION)
-
-    # Assert that send_data_to_bg_process was called MAX_REPORT_TRIES times
-    assert mock_class.send_data_to_bg_process.call_count == 5
 
 
 def test_pkg_compat_check_partial_success(mock_get_comms, mocker):
@@ -99,6 +84,3 @@ def test_pkg_compat_check_partial_success(mock_get_comms, mocker):
 
     # Call the function under test
     pkg_compat_check(pkg_name, ANY_VERSION)
-
-    # Assert that send_data_to_bg_process was called twice
-    assert mock_class.send_data_to_bg_process.call_count == 2
