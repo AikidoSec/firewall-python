0e94c489a9517bc7bd8d4d04f129bef01198fcf7
Wout Feys <bitterpanda@proton.me>
2025-02-27 09:11:00 +0100

    Python fetch_firewall_lists

--- aikido_zen/background_process/cloud_connection_manager/update_firewall_lists.py
+++ aikido_zen/background_process/cloud_connection_manager/update_firewall_lists.py
@@ -1,17 +1,17 @@
 """
 Exports update_firewall_lists
 """
-
+from aikido_zen.background_process.cloud_connection_manager import CloudConnectionManager
 from aikido_zen.helpers.logging import logger
 
 
-def update_firewall_lists(connection_manager):
+def update_firewall_lists(connection_manager: CloudConnectionManager):
     """Will update service config with blocklist of IP addresses"""
     if not connection_manager.token or connection_manager.serverless:
         # Need token, and support in serverless mode is off for this feature
         return
     try:
-        res = connection_manager.api.fetch_blocked_ips(connection_manager.token)
+        res = connection_manager.api.fetch_firewall_lists(connection_manager.token)
         if res.get("success") and res.get("blockedIPAddresses"):
             connection_manager.conf.set_blocked_ips(res.get("blockedIPAddresses"))
     except Exception as e:
