7184ed803fd585d406c049679001dc5cd840602e
Wout Feys <bitterpanda@proton.me>
2025-02-27 09:48:13 +0100

    use .update

--- aikido_zen/background_process/cloud_connection_manager/update_service_config.py
+++ aikido_zen/background_process/cloud_connection_manager/update_service_config.py
@@ -2,10 +2,11 @@
 
 from aikido_zen.helpers.logging import logger
 from aikido_zen.helpers.get_current_unixtime_ms import get_unixtime_ms
+from . import CloudConnectionManager
 from ..service_config import ServiceConfig
 
 
-def update_service_config(connection_manager, res):
+def update_service_config(connection_manager: CloudConnectionManager, res):
     """
     Update configuration based on the server's response
     """
@@ -16,11 +17,10 @@ def update_service_config(connection_manager, res):
         logger.debug("Updating blocking, setting blocking to : %s", res["block"])
         connection_manager.block = bool(res["block"])
 
-    connection_manager.conf = ServiceConfig(
+    connection_manager.conf.update(
         endpoints=res.get("endpoints", []),
         last_updated_at=res.get("configUpdatedAt", get_unixtime_ms()),
         blocked_uids=res.get("blockedUserIds", []),
         bypassed_ips=res.get("allowedIPAddresses", []),
         received_any_stats=res.get("receivedAnyStats", True),
-        blocked_ips=res.get("blockedIPAddresses", []),
     )
