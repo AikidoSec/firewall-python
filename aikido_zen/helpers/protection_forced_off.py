31688c80e48a9c526defbc1b8e7b8a661bbe761d
BitterPanda63 <bitterpanda@proton.me>
2025-03-06 10:42:21 +0100

    Check all endpoints for forced protection off

--- aikido_zen/helpers/protection_forced_off.py
+++ aikido_zen/helpers/protection_forced_off.py
@@ -4,8 +4,10 @@ from .match_endpoints import match_endpoints
 
 
 def protection_forced_off(route_metadata, endpoints):
-    """Matches the route from given endpoints and returns boolean wether protection is off."""
+    """Matches the route from given endpoints and returns boolean whether protection is off."""
     matches = match_endpoints(route_metadata, endpoints)
     if matches and len(matches) > 0:
-        return matches[0]["forceProtectionOff"]
+        for match in matches:
+            if match["forceProtectionOff"]:
+                return True
     return False
