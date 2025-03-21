cbf5d0431ce2754fa73ccc38bafc2735622b96c6
BitterPanda63 <bitterpanda@proton.me>
2025-03-21 10:07:11 +0100

    make thread_cache process-llocal

--- aikido_zen/thread/thread_cache.py
+++ aikido_zen/thread/thread_cache.py
@@ -1,39 +1,19 @@
 """Exports class ThreadConfig"""
 
-from threading import local
 import aikido_zen.background_process.comms as comms
-import aikido_zen.helpers.get_current_unixtime_ms as t
-from aikido_zen.context import get_current_context
 from aikido_zen.background_process.routes import Routes
 from aikido_zen.background_process.service_config import ServiceConfig
 from aikido_zen.helpers.logging import logger
-
-THREAD_CONFIG_TTL_MS = 60 * 1000  # Time-To-Live is 60 seconds for the thread cache
-
-threadlocal_storage = local()
-
-
-def get_cache():
-    """Returns the current ThreadCache"""
-    cache = getattr(threadlocal_storage, "cache", None)
-    if not cache:
-        return ThreadCache()
-    return cache
+from aikido_zen.thread import process_worker_loader
 
 
 class ThreadCache:
     """
-    A thread-local cache object that holds routes, bypassed ips, endpoints amount of requests
-    With a Time-To-Live given by THREAD_CONFIG_TTL_MS
+    A process-local cache object that holds routes, bypassed ips, endpoints amount of requests
     """
 
     def __init__(self):
-        # Load initial data :
-        self.reset()
-        self.renew()
-
-        # Save as a thread-local object :
-        threadlocal_storage.cache = self
+        self.reset()  # Initialize values
 
     def is_bypassed_ip(self, ip):
         """Checks the given IP against the list of bypassed ips"""
@@ -42,14 +22,6 @@ class ThreadCache:
     def is_user_blocked(self, user_id):
         """Checks if the user id is blocked"""
         return user_id in self.config.blocked_uids
-
-    def renew_if_ttl_expired(self):
-        """Renews the data only if TTL has expired"""
-        ttl_has_expired = (
-            t.get_unixtime_ms(monotonic=True) - self.last_renewal > THREAD_CONFIG_TTL_MS
-        )
-        if ttl_has_expired:
-            self.renew()
 
     def get_endpoints(self):
         return self.config.endpoints
@@ -65,16 +37,14 @@ class ThreadCache:
             received_any_stats=False,
         )
         self.reqs = 0
-        self.last_renewal = 0
         self.middleware_installed = False
 
     def renew(self):
         """
         Makes an IPC call to store the amount of hits and requests and renew the config
         """
-        # Don't try to fetch a thread cache if communications don't work or
-        # if we are not inside the context of a web request
-        if not comms.get_comms() or not get_current_context():
+
+        if not comms.get_comms():
             return
 
         res = comms.get_comms().send_data_to_bg_process(
@@ -95,9 +65,28 @@ class ThreadCache:
                 self.routes.routes = res["data"]["routes"]
                 for route in self.routes.routes.values():
                     route["hits_delta_since_sync"] = 0
-            self.last_renewal = t.get_unixtime_ms(monotonic=True)
             logger.debug("Renewed thread cache")
 
     def increment_stats(self):
         """Increments the requests"""
         self.reqs += 1
+
+
+# For these 2 functions and the data they process, we rely on Python's GIL
+# See here: https://wiki.python.org/moin/GlobalInterpreterLock
+global_thread_cache = ThreadCache()
+
+
+def get_cache():
+    """
+    Returns the cache, protected by Python's GIL (so not our own mutex),
+    and starts the process worker (which syncs info between the cache and agent), if it doesn't already exist.
+    """
+    global global_thread_cache
+    process_worker_loader.load_worker()
+    return global_thread_cache
+
+
+def renew():
+    global global_thread_cache
+    global_thread_cache.renew()
