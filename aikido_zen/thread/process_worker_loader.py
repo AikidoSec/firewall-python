3d208a58fe17c34e8bbdd5cf776f9e0f439f8e44
BitterPanda63 <bitterpanda@proton.me>
2025-03-21 10:07:01 +0100

    Create a process_worker and process_worker_loader in thread

---
+++ aikido_zen/thread/process_worker_loader.py
@@ -0,0 +1,20 @@
+import multiprocessing
+import threading
+
+from aikido_zen.thread.process_worker import aikido_process_worker_thread
+
+
+def load_worker():
+    """
+    Loads in a new process worker if one does not already exist for the current process
+    """
+
+    # The name is aikido-process-worker- + the current PID
+    thread_name = "aikido-process-worker-" + str(multiprocessing.current_process().pid)
+    if any([thread.name == thread_name for thread in threading.enumerate()]):
+        return  # The thread already exists, returning.
+
+    # Create a new daemon thread tht will handle communication to and from background agent
+    thread = threading.Thread(target=aikido_process_worker_thread, name=thread_name)
+    thread.daemon = True
+    thread.start()
