a2fd5f956670383cb99cf95604f731695c7d3b4e
nadiraikido <166383531+nadiraikido@users.noreply.github.com>
2025-04-08 13:35:33 +0200

    Revert linting, add JSON endpoint in Django sample app

--- end2end/django_mysql_test.py
+++ end2end/django_mysql_test.py
@@ -1,17 +1,12 @@
 import time
 import pytest
 import requests
-from .server.check_events_from_mock import (
-    fetch_events_from_mock,
-    validate_started_event,
-    filter_on_event_type,
-    validate_heartbeat,
-)
+import json
+from .server.check_events_from_mock import fetch_events_from_mock, validate_started_event, filter_on_event_type, validate_heartbeat
 
 # e2e tests for django_mysql sample app
 base_url_fw = "http://localhost:8080/app"
 base_url_nofw = "http://localhost:8081/app"
-
 
 def test_firewall_started_okay():
     events = fetch_events_from_mock("http://localhost:5000")
@@ -19,48 +14,40 @@ def test_firewall_started_okay():
     assert len(started_events) == 1
     validate_started_event(started_events[0], ["django", "mysqlclient"])
 
-
 def test_safe_response_with_firewall():
     dog_name = "Bobby Tables"
-    res = requests.post(base_url_fw + "/create", data={"dog_name": dog_name})
+    res = requests.post(base_url_fw + "/create", data={'dog_name': dog_name})
     assert res.status_code == 200
-
 
 def test_safe_response_without_firewall():
     dog_name = "Bobby Tables"
-    res = requests.post(base_url_nofw + "/create", data={"dog_name": dog_name})
+    res = requests.post(base_url_nofw + "/create", data={'dog_name': dog_name})
     assert res.status_code == 200
 
 
 def test_dangerous_response_with_firewall():
     dog_name = 'Dangerous bobby", 1); -- '
-    res = requests.post(base_url_fw + "/create", data={"dog_name": dog_name})
+    res = requests.post(base_url_fw + "/create", data={'dog_name': dog_name})
     assert res.status_code == 500
-    time.sleep(5)  # Wait for attack to be reported
+    time.sleep(5) # Wait for attack to be reported
     events = fetch_events_from_mock("http://localhost:5000")
     attacks = filter_on_event_type(events, "detected_attack")
-
+    
     assert len(attacks) == 1
     del attacks[0]["attack"]["stack"]
     assert attacks[0]["attack"] == {
         "blocked": True,
         "kind": "sql_injection",
-        "metadata": {
-            "sql": 'INSERT INTO sample_app_dogs (dog_name, dog_boss) VALUES ("Dangerous bobby", 1); -- ", "N/A")'
-        },
-        "operation": "MySQLdb.Cursor.execute",
-        "pathToPayload": ".dog_name",
-        "payload": '"Dangerous bobby\\", 1); -- "',
-        "source": "body",
-        "user": None,
+        'metadata': {'sql': 'INSERT INTO sample_app_dogs (dog_name, dog_boss) VALUES ("Dangerous bobby", 1); -- ", "N/A")'},
+        'operation': 'MySQLdb.Cursor.execute',
+        'pathToPayload': '.dog_name',
+        'payload': '"Dangerous bobby\\", 1); -- "',
+        'source': "body",
+        'user': None
     }
 
-
 def test_dangerous_response_with_form_header_but_json_body():
-    import json
-
     headers = {"Content-Type": "application/x-www-form-urlencoded"}
-
     json_body = json.dumps({"dog_name": 'Dangerous bobby", 1); -- '})
 
     res = requests.post(base_url_fw + "/create", headers=headers, data=json_body)
@@ -85,51 +72,45 @@ def test_dangerous_response_with_form_header_but_json_body():
         "user": None,
     }
 
-
 def test_dangerous_response_with_firewall_shell():
     dog_name = 'Dangerous bobby", 1); -- '
     res = requests.get(base_url_fw + "/shell/ls -la")
     assert res.status_code == 500
-    time.sleep(5)  # Wait for attack to be reported
+    time.sleep(5) # Wait for attack to be reported
     events = fetch_events_from_mock("http://localhost:5000")
     attacks = filter_on_event_type(events, "detected_attack")
-
+    
     assert len(attacks) == 2
-    del attacks[0]  # Previous attack
+    del attacks[0] # Previous attack
     del attacks[0]["attack"]["stack"]
     assert attacks[0]["attack"] == {
         "blocked": True,
         "kind": "shell_injection",
-        "metadata": {"command": "ls -la"},
-        "operation": "subprocess.Popen",
-        "pathToPayload": ".[0]",
-        "payload": '"ls -la"',
-        "source": "route_params",
-        "user": None,
+        'metadata': {'command': 'ls -la'},
+        'operation': 'subprocess.Popen',
+        'pathToPayload': '.[0]',
+        'payload': '"ls -la"',
+        'source': "route_params",
+        'user': None
     }
-
 
 def test_dangerous_response_without_firewall():
     dog_name = 'Dangerous bobby", 1); -- '
-    res = requests.post(base_url_nofw + "/create", data={"dog_name": dog_name})
+    res = requests.post(base_url_nofw + "/create", data={'dog_name': dog_name})
     assert res.status_code == 200
 
-
 def test_initial_heartbeat():
-    time.sleep(55)  # Sleep 5 + 55 seconds for heartbeat
+    time.sleep(55) # Sleep 5 + 55 seconds for heartbeat
     events = fetch_events_from_mock("http://localhost:5000")
     heartbeat_events = filter_on_event_type(events, "heartbeat")
     assert len(heartbeat_events) == 1
-    validate_heartbeat(
-        heartbeat_events[0],
-        [
-            {
-                "apispec": {},
-                "hits": 1,
-                "hits_delta_since_sync": 1,
-                "method": "POST",
-                "path": "/app/create",
-            }
-        ],
-        {"aborted": 0, "attacksDetected": {"blocked": 2, "total": 2}, "total": 0},
+    validate_heartbeat(heartbeat_events[0], 
+        [{
+            "apispec": {},
+            "hits": 1,
+            "hits_delta_since_sync": 1,
+            "method": "POST",
+            "path": "/app/create"
+        }], 
+        {"aborted":0,"attacksDetected":{"blocked":2,"total":2},"total":0}
     )
--- sample-apps/django-mysql/sample_app/urls.py
+++ sample-apps/django-mysql/sample_app/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("dogpage/<int:dog_id>", views.dog_page, name="dog_page"),
+    path("json/create", views.json_create_dog, name="json_create"),
     path("shell/<str:user_command>", views.shell_url, name="shell"),
     path("create", views.create_dogpage, name="create")
 ]
