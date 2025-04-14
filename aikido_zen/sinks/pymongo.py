3f0017812d3005d552cfa6529478babd8c2fee9e
BitterPanda63 <bitterpanda@proton.me>
2025-04-14 16:35:31 +0200

    pymongo convert code to wrapt code

--- aikido_zen/sinks/pymongo.py
+++ aikido_zen/sinks/pymongo.py
@@ -2,92 +2,114 @@
 Sink module for `pymongo`
 """
 
-from copy import deepcopy
-import aikido_zen.importhook as importhook
-from aikido_zen.helpers.logging import logger
+from wrapt import when_imported
+
+from aikido_zen.helpers.get_argument import get_argument
 import aikido_zen.background_process.packages as pkgs
 import aikido_zen.vulnerabilities as vulns
-
-# find_one not present in list since find_one calls find function.
-
-OPERATIONS_WITH_FILTER = [
-    ("replace_one", [0, "filter"]),
-    ("update_one", [0, "filter"]),
-    ("update_many", [0, "filter"]),
-    ("delete_one", [0, "filter"]),
-    ("delete_many", [0, "filter"]),
-    ("count_documents", [0, "filter"]),
-    ("find_one_and_delete", [0, "filter"]),
-    ("find_one_and_replace", [0, "filter"]),
-    ("find_one_and_update", [0, "filter"]),
-    ("find", [0, "filter"]),
-    ("find_raw_batches", [0, "filter"]),
-    ("distinct", [1, "filter"]),
-    ("watch", [0, "pipeline"]),
-    ("aggregate", [0, "pipeline"]),
-    ("aggregate_raw_batches", [0, "pipeline"]),
-]
+from . import try_wrap_function_wrapper
+from ..errors import AikidoException
 
 REQUIRED_PYMONGO_VERSION = "3.10.0"
 
 
-# Synchronous :
-@importhook.on_import("pymongo.collection")
-def on_pymongo_import(pymongo):
+@when_imported("pymongo.collection")
+def patch(m):
     """
-    Hook 'n wrap on `pymongo.collection`
-    Our goal is to wrap the following functions in the Collection class :
-    https://github.com/mongodb/mongo-python-driver/blob/98658cfd1fea42680a178373333bf27f41153759/pymongo/synchronous/collection.py#L136
-    Returns : Modified pymongo.collection.Collection object
+    patching pymongo.collection
+    - patches Collection.*(filter, ...)
+    - patches Collection.*(..., filter, ...)
+    - patches Collection.*(pipeline, ...)
+    - patches Collection.bulk_write
+    src: https://github.com/mongodb/mongo-python-driver/blob/98658cfd1fea42680a178373333bf27f41153759/pymongo/synchronous/collection.py#L136
     """
     if not pkgs.is_package_compatible("pymongo", REQUIRED_PYMONGO_VERSION):
-        return pymongo
-    modified_pymongo = importhook.copy_module(pymongo)
-    for op_data in OPERATIONS_WITH_FILTER:
-        op = op_data[0]
-        if not hasattr(pymongo.Collection, op):
-            logger.warning("Operation `%s` not found on Collection object.", op)
+        return
 
-        prev_func = deepcopy(getattr(pymongo.Collection, op))
+    # func(filter, ...)
+    try_wrap_function_wrapper(m, "Collection.replace_one", _func_filter_first)
+    try_wrap_function_wrapper(m, "Collection.update_one", _func_filter_first)
+    try_wrap_function_wrapper(m, "Collection.update_many", _func_filter_first)
+    try_wrap_function_wrapper(m, "Collection.delete_one", _func_filter_first)
+    try_wrap_function_wrapper(m, "Collection.delete_many", _func_filter_first)
+    try_wrap_function_wrapper(m, "Collection.count_documents", _func_filter_first)
+    try_wrap_function_wrapper(m, "Collection.find_one_and_delete", _func_filter_first)
+    try_wrap_function_wrapper(m, "Collection.find_one_and_replace", _func_filter_first)
+    try_wrap_function_wrapper(m, "Collection.find_one_and_update", _func_filter_first)
+    try_wrap_function_wrapper(m, "Collection.find", _func_filter_first)
+    try_wrap_function_wrapper(m, "Collection.find_raw_batches", _func_filter_first)
+    # find_one not present in list since find_one calls find function.
 
-        def wrapped_op_func(
-            self,
-            *args,
-            prev_func=prev_func,
-            op_data=op_data,
-            **kwargs,
-        ):
-            op, spot, key = op_data[0], op_data[1][0], op_data[1][1]
-            data = None
-            if kwargs.get(key, None):
-                # Keyword found, setting data
-                data = kwargs.get(key)
-            elif len(args) > spot and args[spot]:
-                data = args[spot]
-            if data:
-                vulns.run_vulnerability_scan(
-                    kind="nosql_injection",
-                    op=f"pymongo.collection.Collection.{op}",
-                    args=(data,),
-                )
+    # func(..., filter, ...)
+    try_wrap_function_wrapper(m, "Collection.distinct", _func_filter_second)
 
-            return prev_func(self, *args, **kwargs)
+    # func(pipeline, ...)
+    try_wrap_function_wrapper(m, "Collection.watch", _func_pipeline)
+    try_wrap_function_wrapper(m, "Collection.aggregate", _func_pipeline)
+    try_wrap_function_wrapper(m, "Collection.aggregate_raw_batches", _func_pipeline)
 
-        setattr(modified_pymongo.Collection, op, wrapped_op_func)
+    # bulk_write
+    try_wrap_function_wrapper(m, "Collection.bulk_write", _bulk_write)
 
-    # Add bulk_write support :
-    former_bulk_write = deepcopy(pymongo.Collection.bulk_write)
 
-    def aikido_bulk_write(self, requests, *args, **kwargs):
-        for request in requests:
-            if hasattr(request, "_filter"):
-                # Requested operation has a filter
-                vulns.run_vulnerability_scan(
-                    kind="nosql_injection",
-                    op="pymongo.collection.Collection.bulk_write",
-                    args=(request._filter,),
-                )
-        return former_bulk_write(self, requests, *args, **kwargs)
+def _func_filter_first(func, instance, args, kwargs):
+    """Collection.func(filter, ...)"""
+    nosql_filter = get_argument(args, kwargs, 0, "filter")
+    if not nosql_filter:
+        return func(*args, **kwargs)
 
-    setattr(modified_pymongo.Collection, "bulk_write", aikido_bulk_write)
-    return modified_pymongo
+    vulns.run_vulnerability_scan(
+        kind="nosql_injection",
+        op=f"pymongo.collection.Collection.{func.__name__}",
+        args=(nosql_filter,),
+    )
+    return func(*args, **kwargs)
+
+
+def _func_filter_second(func, instance, args, kwargs):
+    """Collection.func(..., filter, ...)"""
+    nosql_filter = get_argument(args, kwargs, 1, "filter")
+    if not nosql_filter:
+        return func(*args, **kwargs)
+
+    vulns.run_vulnerability_scan(
+        kind="nosql_injection",
+        op=f"pymongo.collection.Collection.{func.__name__}",
+        args=(nosql_filter,),
+    )
+    return func(*args, **kwargs)
+
+
+def _func_pipeline(func, instance, args, kwargs):
+    """Collection.func(pipeline, ...)"""
+    nosql_pipeline = get_argument(args, kwargs, 0, "pipeline")
+    if not nosql_pipeline:
+        return func(*args, **kwargs)
+
+    vulns.run_vulnerability_scan(
+        kind="nosql_injection",
+        op=f"pymongo.collection.Collection.{func.__name__}",
+        args=(nosql_pipeline,),
+    )
+    return func(*args, **kwargs)
+
+
+def _bulk_write(func, instance, args, kwargs):
+    requests = get_argument(args, kwargs, 0, "requests")
+
+    # Filter requests that contain "_filter"
+    requests_with_filter = [req for req in requests if hasattr(req, "_filter")]
+    # Run vulnerability scans for each filtered request
+    for request in requests_with_filter:
+        try:
+            vulns.run_vulnerability_scan(
+                kind="nosql_injection",
+                op="pymongo.collection.Collection.bulk_write",
+                args=(request._filter,),
+            )
+        except AikidoException as e:
+            raise e
+        except Exception:
+            pass  # Log or handle the exception if needed
+
+    return func(*args, **kwargs)
