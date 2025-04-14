76894deae8b4c61d19098ef3bbf1bf571d777700
BitterPanda63 <bitterpanda@proton.me>
2025-04-14 21:21:02 +0200

    convert io module

--- aikido_zen/sinks/io.py
+++ aikido_zen/sinks/io.py
@@ -2,34 +2,38 @@
 Sink module for python's `io`
 """
 
-import copy
-import aikido_zen.importhook as importhook
+from wrapt import when_imported
 import aikido_zen.vulnerabilities as vulns
+from aikido_zen.helpers.get_argument import get_argument
+from aikido_zen.sinks import try_wrap_function_wrapper
 
-KIND = "path_traversal"
+
+def _open(func, instance, args, kwargs):
+    file = get_argument(args, kwargs, 0, "file")
+    if not file:
+        return func(*args, **kwargs)
+
+    vulns.run_vulnerability_scan(kind="path_traversal", op="io.open", args=(file,))
+
+    return func(*args, **kwargs)
 
 
-@importhook.on_import("io")
-def on_io_import(io):
+def _open_code(func, instance, args, kwargs):
+    path = get_argument(args, kwargs, 0, "path")
+    if not path:
+        return func(*args, **kwargs)
+
+    vulns.run_vulnerability_scan(kind="path_traversal", op="io.open_code", args=(path,))
+
+    return func(*args, **kwargs)
+
+
+@when_imported("io")
+def patch(m):
     """
-    Hook 'n wrap on `io`, wrapping io.open(...) and io.open_code(...)
-    Returns : Modified io object
+    patching module io
+    - patches io.open(file, ...)
+    - patches io.open_code(path)
     """
-    modified_io = importhook.copy_module(io)
-    former_open_func = copy.deepcopy(io.open)
-    former_open_code_func = copy.deepcopy(io.open_code)
-
-    def aikido_open_func(file, *args, **kwargs):
-        if file:
-            vulns.run_vulnerability_scan(kind=KIND, op="io.open", args=(file,))
-        return former_open_func(file, *args, **kwargs)
-
-    def aikido_open_code_func(path):
-        if path:
-            vulns.run_vulnerability_scan(kind=KIND, op="io.open_code", args=(path,))
-        return former_open_code_func(path)
-
-    setattr(modified_io, "open", aikido_open_func)
-    setattr(modified_io, "open_code", aikido_open_code_func)
-
-    return modified_io
+    try_wrap_function_wrapper(m, "open", _open)
+    try_wrap_function_wrapper(m, "open_code", _open_code)
