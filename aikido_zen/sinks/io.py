70c16a1f3deab5d17de56b307dd4b6f9e706f30e
BitterPanda63 <bitterpanda@proton.me>
2025-04-14 22:28:33 +0200

    Update io module to use new system

--- aikido_zen/sinks/io.py
+++ aikido_zen/sinks/io.py
@@ -2,38 +2,35 @@
 Sink module for python's `io`
 """
 
-from wrapt import when_imported
 import aikido_zen.vulnerabilities as vulns
 from aikido_zen.helpers.get_argument import get_argument
-from aikido_zen.sinks import try_wrap_function_wrapper
+from aikido_zen.sinks import patch_function, before, on_import
 
 
+@before
 def _open(func, instance, args, kwargs):
     file = get_argument(args, kwargs, 0, "file")
     if not file:
-        return func(*args, **kwargs)
+        return
 
     vulns.run_vulnerability_scan(kind="path_traversal", op="io.open", args=(file,))
 
-    return func(*args, **kwargs)
 
-
+@before
 def _open_code(func, instance, args, kwargs):
     path = get_argument(args, kwargs, 0, "path")
     if not path:
-        return func(*args, **kwargs)
+        return
 
     vulns.run_vulnerability_scan(kind="path_traversal", op="io.open_code", args=(path,))
 
-    return func(*args, **kwargs)
 
-
-@when_imported("io")
+@on_import("io")
 def patch(m):
     """
     patching module io
     - patches io.open(file, ...)
     - patches io.open_code(path)
     """
-    try_wrap_function_wrapper(m, "open", _open)
-    try_wrap_function_wrapper(m, "open_code", _open_code)
+    patch_function(m, "open", _open)
+    patch_function(m, "open_code", _open_code)
