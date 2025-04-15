807b9817789d70d23598ffa1d0ec8a3af8b2a0dd
BitterPanda63 <bitterpanda@proton.me>
2025-04-15 16:43:54 +0200

    Add extra tests for psycopg2 and fix issue for immutables

--- aikido_zen/sinks/psycopg2.py
+++ aikido_zen/sinks/psycopg2.py
@@ -2,7 +2,6 @@
 Sink module for `psycopg2`
 """
 
-from aikido_zen import logger
 from aikido_zen.background_process.packages import is_package_compatible
 import aikido_zen.vulnerabilities as vulns
 from aikido_zen.helpers.get_argument import get_argument
@@ -28,25 +27,31 @@ def patch(m):
 
 
 @after
-def _connect(func, instance, args, kwargs, rv):
+def _connect(func, instance, _args, _kwargs, rv):
     """
     the return value (rv) is the new connection object, we'll change the cursor_factory attribute here.
     """
-    # Create new default cursor factory if not exists
     if rv.cursor_factory is None:
+        # set default if not set
         import psycopg2.extensions
 
-        class AikidoDefaultCursor(psycopg2.extensions.cursor):
-            pass
+        rv.cursor_factory = psycopg2.extensions.cursor
 
-        rv.cursor_factory = AikidoDefaultCursor
-
-    patch_function(rv.cursor_factory, "execute", _execute)
-    patch_function(rv.cursor_factory, "executemany", _execute)
+    rv.cursor_factory = type(
+        "AikidoPsycopg2Cursor",
+        (rv.cursor_factory,),
+        {
+            # Allows us to modify these otherwise immutable functions
+            "execute": rv.cursor_factory.execute,
+            "executemany": rv.cursor_factory.executemany,
+        },
+    )
+    patch_function(rv.cursor_factory, "execute", psycopg2_patch)
+    patch_function(rv.cursor_factory, "executemany", psycopg2_patch)
 
 
 @before
-def _execute(func, instance, args, kwargs):
+def psycopg2_patch(func, instance, args, kwargs):
     query = get_argument(args, kwargs, 0, "query")
     op = f"psycopg2.Connection.Cursor.{func.__name__}"
     vulns.run_vulnerability_scan(kind="sql_injection", op=op, args=(query, "postgres"))
--- aikido_zen/sinks/tests/psycopg2_test.py
+++ aikido_zen/sinks/tests/psycopg2_test.py
@@ -27,6 +27,20 @@ def database_conn():
 
 
 @pytest.fixture
+def database_conn_immutable_cursor():
+    import psycopg2
+    from psycopg2.extensions import cursor
+
+    return psycopg2.connect(
+        cursor_factory=cursor,
+        host="127.0.0.1",
+        user="user",
+        password="password",
+        database="db",
+    )
+
+
+@pytest.fixture
 def database_conn_empty_cursor():
     import psycopg2
 
@@ -45,6 +59,7 @@ def test_cursor_execute(database_conn):
         "aikido_zen.vulnerabilities.run_vulnerability_scan"
     ) as mock_run_vulnerability_scan:
         cursor = database_conn.cursor()
+        print(cursor)
         query = "SELECT * FROM dogs"
         cursor.execute(query)
 
@@ -75,6 +90,26 @@ def test_cursor_execute2(database_conn_empty_cursor):
 
         cursor.fetchall()
         database_conn_empty_cursor.close()
+        mock_run_vulnerability_scan.assert_called_once()
+
+
+@pytest.mark.skipif(skip_python_3_13, reason="Skipping test on Python 3.13")
+def test_cursor_execute3(database_conn_immutable_cursor):
+    reset_comms()
+    with patch(
+        "aikido_zen.vulnerabilities.run_vulnerability_scan"
+    ) as mock_run_vulnerability_scan:
+        cursor = database_conn_immutable_cursor.cursor()
+        query = "SELECT * FROM dogs"
+        cursor.execute(query)
+
+        called_with_args = mock_run_vulnerability_scan.call_args[1]["args"]
+        assert called_with_args[0] == query
+        assert called_with_args[1] == "postgres"
+        mock_run_vulnerability_scan.assert_called_once()
+
+        cursor.fetchall()
+        database_conn_immutable_cursor.close()
         mock_run_vulnerability_scan.assert_called_once()
 
 
@@ -164,4 +199,31 @@ def test_cursor_executemany2(database_conn_empty_cursor):
         database_conn_empty_cursor.commit()
         cursor.close()
         database_conn_empty_cursor.close()
+        mock_run_vulnerability_scan.assert_called_once()
+
+
+@pytest.mark.skipif(skip_python_3_13, reason="Skipping test on Python 3.13")
+def test_cursor_executemany3(database_conn_immutable_cursor):
+    reset_comms()
+    with patch(
+        "aikido_zen.vulnerabilities.run_vulnerability_scan"
+    ) as mock_run_vulnerability_scan:
+        cursor = database_conn_immutable_cursor.cursor()
+        data = [
+            ("Doggy", False),
+            ("Doggy 2", True),
+            ("Dogski", True),
+        ]
+        cursor.executemany("INSERT INTO dogs (dog_name, isadmin) VALUES (%s, %s)", data)
+        called_with_args = mock_run_vulnerability_scan.call_args[1]["args"]
+        assert (
+            called_with_args[0]
+            == "INSERT INTO dogs (dog_name, isadmin) VALUES (%s, %s)"
+        )
+        assert called_with_args[1] == "postgres"
+        mock_run_vulnerability_scan.assert_called_once()
+
+        database_conn_immutable_cursor.commit()
+        cursor.close()
+        database_conn_immutable_cursor.close()
         mock_run_vulnerability_scan.assert_called_once()
