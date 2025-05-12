793a1fe8cde983cd91f27e16913f4d98e4d02198
BitterPanda <bitterpanda@proton.me>
2025-05-12 09:48:47 +0200

    Django cleanup and expand comments

--- aikido_zen/sources/django/__init__.py
+++ aikido_zen/sources/django/__init__.py
@@ -1,9 +1,5 @@
 """`Django` source module"""
 
-import copy
-import aikido_zen.importhook as importhook
-from aikido_zen.helpers.logging import logger
-from aikido_zen.background_process.packages import is_package_compatible, ANY_VERSION
 from ..functions.request_handler import request_handler
 from .run_init_stage import run_init_stage
 from .pre_response_middleware import pre_response_middleware
@@ -31,8 +27,9 @@ def _get_response_after(func, instance, args, kwargs, return_value):
 @on_import("django.core.handlers.base")
 def patch(m):
     """
-    Patch for _get_response
+    Patch for _get_response (Synchronous/WSGI)
     - before: Parse body, create context & add middleware to run before a response
+    - after: Check respone code to see if route should be analyzed
     # https://github.com/django/django/blob/5865ff5adcf64da03d306dc32b36e87ae6927c85/django/core/handlers/base.py#L174
     """
     patch_function(m, "BaseHandler._get_response", _get_response_before)
