5e81e4dfe0f7a345b75fb698777e8cf7c7e6443e
BitterPanda63 <bitterpanda@proton.me>
2025-05-15 16:57:42 +0200

    blocking_enabled go check the cache for blocking boolean

--- aikido_zen/helpers/blocking_enabled.py
+++ aikido_zen/helpers/blocking_enabled.py
@@ -1,18 +1,11 @@
 """Helper function file, see function docstring"""
 
-from aikido_zen.background_process import get_comms
 from aikido_zen.helpers.check_env_for_blocking import check_env_for_blocking
+from aikido_zen.thread.thread_cache import get_cache
 
 
 def is_blocking_enabled():
-    """
-    Checks with the background process if blocking is enabled
-    """
-    if get_comms():
-        # Only check if comms are defined
-        should_block_res = get_comms().send_data_to_bg_process(
-            action="READ_PROPERTY", obj="block", receive=True
-        )
-        if should_block_res["success"]:
-            return should_block_res["data"]
+    """Checks with the process cache for blocking, and if that is not defined, checks env"""
+    if get_cache() and get_cache().config and get_cache().config.blocking is not None:
+        return get_cache().config.blocking
     return check_env_for_blocking()
