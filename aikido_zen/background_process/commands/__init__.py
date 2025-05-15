4c300dbfaa4aecebbc5b9aacc5a7c98047318f18
BitterPanda63 <bitterpanda@proton.me>
2025-05-15 14:31:43 +0200

    Delete the USER command

--- aikido_zen/background_process/commands/__init__.py
+++ aikido_zen/background_process/commands/__init__.py
@@ -3,7 +3,6 @@
 from aikido_zen.helpers.logging import logger
 from .attack import process_attack
 from .read_property import process_read_property
-from .user import process_user
 from .should_ratelimit import process_should_ratelimit
 from .ping import process_ping
 from .sync_data import process_sync_data
@@ -12,7 +11,6 @@ commands_map = {
     # This maps to a tuple : (function, returns_data?)
     # Commands that don't return data :
     "ATTACK": (process_attack, False),
-    "USER": (process_user, False),
     # Commands that return data :
     "SYNC_DATA": (process_sync_data, True),
     "READ_PROPERTY": (process_read_property, True),
