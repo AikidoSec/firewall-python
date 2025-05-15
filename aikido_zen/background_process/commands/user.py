b03253212591157750b259ab781fd417655c85eb
BitterPanda63 <bitterpanda@proton.me>
2025-05-15 14:31:43 +0200

    Delete the USER command

--- aikido_zen/background_process/commands/user.py
+++
@@ -1,7 +0,0 @@
-"""Exports `process_user`"""
-
-
-def process_user(connection_manager, data, queue=None):
-    """Adds a user to the users object of the connection_manager"""
-    if connection_manager:
-        connection_manager.users.add_user(data)
