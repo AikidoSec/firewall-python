b3b4515e02a46f46fc520fc390a335971f19b55b
BitterPanda63 <bitterpanda@proton.me>
2025-05-21 15:43:10 +0200

    Remove @before wrapper in favour of parsing args,kwargs

--- aikido_zen/sinks/http_client.py
+++ aikido_zen/sinks/http_client.py
@@ -3,22 +3,16 @@ Sink module for `http`
 """
 
 from aikido_zen.helpers.get_argument import get_argument
-from aikido_zen.sinks import before, after, patch_function, on_import
+from aikido_zen.sinks import after, patch_function, on_import
 from aikido_zen.vulnerabilities.ssrf.handle_http_response import (
     handle_http_response,
 )
 from aikido_zen.helpers.try_parse_url import try_parse_url
 
 
-@before
-def _putrequest(func, instance, args, kwargs):
-    path = get_argument(args, kwargs, 1, "path")
-    setattr(instance, "_aikido_var_path", path)
-
-
 @after
 def _getresponse(func, instance, args, kwargs, return_value):
-    path = getattr(instance, "_aikido_var_path")
+    path = get_argument(args, kwargs, 1, "path")
     source_url = try_parse_url(f"http://{instance.host}:{instance.port}{path}")
     handle_http_response(http_response=return_value, source=source_url)
 
@@ -27,8 +21,6 @@ def _getresponse(func, instance, args, kwargs, return_value):
 def patch(m):
     """
     patching module http.client
-    - patches HTTPConnection.putrequest -> stores path
-    - patches HTTPConnection.getresponse -> handles response object
+    - patches HTTPConnection.getresponse
     """
-    patch_function(m, "HTTPConnection.putrequest", _putrequest)
     patch_function(m, "HTTPConnection.getresponse", _getresponse)
