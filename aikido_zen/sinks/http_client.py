0b752045bfca417f6452b827de4f98ffaa6a4201
BitterPanda63 <bitterpanda@proton.me>
2025-05-21 17:18:40 +0200

    Revert "Remove @before wrapper in favour of parsing args,kwargs"
    This reverts commit 5650510320e207e6fc7209e4c57e9c108f65cb4e.

--- aikido_zen/sinks/http_client.py
+++ aikido_zen/sinks/http_client.py
@@ -3,16 +3,22 @@ Sink module for `http`
 """
 
 from aikido_zen.helpers.get_argument import get_argument
-from aikido_zen.sinks import after, patch_function, on_import
+from aikido_zen.sinks import before, after, patch_function, on_import
 from aikido_zen.vulnerabilities.ssrf.handle_http_response import (
     handle_http_response,
 )
 from aikido_zen.helpers.try_parse_url import try_parse_url
 
 
+@before
+def _putrequest(func, instance, args, kwargs):
+    path = get_argument(args, kwargs, 1, "path")
+    setattr(instance, "_aikido_var_path", path)
+
+
 @after
 def _getresponse(func, instance, args, kwargs, return_value):
-    path = get_argument(args, kwargs, 1, "path")
+    path = getattr(instance, "_aikido_var_path")
     source_url = try_parse_url(f"http://{instance.host}:{instance.port}{path}")
     handle_http_response(http_response=return_value, source=source_url)
 
@@ -21,6 +27,8 @@ def _getresponse(func, instance, args, kwargs, return_value):
 def patch(m):
     """
     patching module http.client
-    - patches HTTPConnection.getresponse
+    - patches HTTPConnection.putrequest -> stores path
+    - patches HTTPConnection.getresponse -> handles response object
     """
+    patch_function(m, "HTTPConnection.putrequest", _putrequest)
     patch_function(m, "HTTPConnection.getresponse", _getresponse)
