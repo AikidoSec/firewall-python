65e21ae9916c56625de5d3efbb7dfe7b04af4f54
BitterPanda63 <bitterpanda@proton.me>
2025-05-30 14:21:06 +0200

    Create a new storage object for statistics

---
+++ aikido_zen/storage/statistics/__init__.py
@@ -0,0 +1,49 @@
+from aikido_zen.helpers.get_current_unixtime_ms import get_unixtime_ms
+
+
+class Statistics:
+    """
+    Keeps track of total and aborted requests
+    and total and blocked attacks
+    """
+
+    def __init__(self):
+        self.total_hits = 0
+        self.attacks_detected = 0
+        self.attacks_blocked = 0
+        self.started_at = get_unixtime_ms()
+
+    def clear(self):
+        self.total_hits = 0
+        self.attacks_detected = 0
+        self.attacks_blocked = 0
+        self.started_at = get_unixtime_ms()
+
+    def increment_total_hits(self):
+        self.total_hits += 1
+
+    def on_detected_attack(self, blocked):
+        self.attacks_detected += 1
+        if blocked:
+            self.attacks_blocked += 1
+
+    def get_record(self):
+        current_time = get_unixtime_ms()
+        return {
+            "startedAt": self.started_at,
+            "endedAt": current_time,
+            "requests": {
+                "total": self.total_hits,
+                "aborted": 0,  # statistic currently not in use
+                "attacksDetected": {
+                    "total": self.attacks_detected,
+                    "blocked": self.attacks_blocked,
+                },
+            },
+        }
+
+    def import_from_record(self, record):
+        attacks_detected = record.get("requests", {}).get("attacksDetected", {})
+        self.total_hits += record.get("requests", {}).get("total", 0)
+        self.attacks_detected += attacks_detected.get("total", 0)
+        self.attacks_blocked += attacks_detected.get("blocked", 0)
