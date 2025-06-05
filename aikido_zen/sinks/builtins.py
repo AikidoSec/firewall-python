0a68f44aa422579fbdc8035ca70a3c1aa5424f46
BitterPanda63 <bitterpanda@proton.me>
2025-06-05 11:11:00 +0200

    builtins add register_call

--- aikido_zen/sinks/builtins.py
+++ aikido_zen/sinks/builtins.py
@@ -5,6 +5,7 @@ Sink module for `builtins`, python's built-in function
 from pathlib import PurePath
 import aikido_zen.vulnerabilities as vulns
 from aikido_zen.helpers.get_argument import get_argument
+from aikido_zen.helpers.register_call import register_call
 from aikido_zen.sinks import patch_function, on_import, before
 
 
@@ -14,9 +15,10 @@ def _open(func, instance, args, kwargs):
     if not isinstance(filename, (str, bytes, PurePath)):
         return
 
-    vulns.run_vulnerability_scan(
-        kind="path_traversal", op="builtins.open", args=(filename,)
-    )
+    op = "builtins.open"
+    register_call(op, "fs_op")
+
+    vulns.run_vulnerability_scan(kind="path_traversal", op=op, args=(filename,))
 
 
 @on_import("builtins")
