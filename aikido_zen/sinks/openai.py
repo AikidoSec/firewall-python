1e2317edb8f86a123f3d2512a55f146f8216a9c3
BitterPanda63 <bitterpanda@proton.me>
2025-06-10 00:30:15 +0200

    Add openai sink code

--- aikido_zen/sinks/openai.py
+++ aikido_zen/sinks/openai.py
@@ -1,7 +1,25 @@
-from aikido_zen.sinks import on_import
+from aikido_zen.helpers.register_call import register_call
+from aikido_zen.sinks import on_import, patch_function, after
+from aikido_zen.storage.ai_statistics import on_ai_call
 
 
-@on_import("openai")
+@after
+def _create(func, instance, args, kwargs, return_value):
+    op = "openai.resources.responses.responses.Responses.create"
+    register_call(op, "ai_op")
+
+    on_ai_call(
+        provider="openai",
+        model=return_value.get("model", ""),
+        input_tokens=return_value.usage.input_tokens,
+        output_tokens=return_value.usage.output_tokens,
+    )
+
+
+@on_import("openai.resources.responses.responses")
 def patch(m):
-    """patching module openai"""
-    pass
+    """
+    patching module openai
+    - patches function create(...) on Responses class, to inspect response
+    """
+    patch_function(m, "Responses.create", _create)
