8eac18168190f906c458f8ce672afaf50e0634f8
BitterPanda63 <bitterpanda@proton.me>
2025-06-10 18:30:38 +0200

    Cleanup flask-openai app

--- sample-apps/flask-openai/app.py
+++ sample-apps/flask-openai/app.py
@@ -1,55 +1,16 @@
 import aikido_zen # Aikido package import
 aikido_zen.protect()
 
-import os
 from flask import Flask, render_template, request
-import psycopg2
 import openai
 from pydantic import BaseModel
 
 app = Flask(__name__)
 client = openai.OpenAI()
 
-def get_db_connection():
-    return psycopg2.connect(
-        host="localhost",
-        database="db",
-        user="user",
-        password="password")
-
 @app.route("/")
 def homepage():
-    cursor = get_db_connection().cursor()
-    cursor.execute("SELECT * FROM dogs")
-    dogs = cursor.fetchall()
-    return render_template('index.html', title='Homepage', dogs=dogs)
-
-
-@app.route('/dogpage/<int:dog_id>')
-def get_dogpage(dog_id):
-    cursor = get_db_connection().cursor()
-    cursor.execute("SELECT * FROM dogs WHERE id = " + str(dog_id))
-    dog = cursor.fetchmany(1)[0]
-    return render_template('dogpage.html', title=f'Dog', dog=dog, isAdmin=("Yes" if dog[2] else "No"))
-
-@app.route("/create", methods=['GET'])
-def show_create_dog_form():
-    return render_template('create_dog.html')
-
-@app.route("/create_many", methods=['GET'])
-def show_create_dog_form_many():
-    return render_template('create_dog.html')
-
-@app.route("/create", methods=['POST'])
-def create_dog():
-    dog_name = request.form['dog_name']
-    conn = get_db_connection()
-    cursor =  conn.cursor()
-    cursor.execute(f"INSERT INTO dogs (dog_name, isAdmin) VALUES ('%s', FALSE)" % (dog_name))
-    conn.commit()
-    cursor.close()
-    conn.close()
-    return f'Dog {dog_name} created successfully'
+    return render_template('index.html', title='Homepage')
 
 # OpenAI
 
