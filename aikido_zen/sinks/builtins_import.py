c78bdf51c45e9f2fba2f4dd87cd147da497247bb
BitterPanda <bitterpanda@proton.me>
2025-06-11 17:40:35 +0200

    Create new builtins_import wrapper

---
+++ aikido_zen/sinks/builtins_import.py
@@ -0,0 +1,42 @@
+import importlib.metadata
+from importlib.metadata import PackageNotFoundError
+
+from aikido_zen.background_process.packages import PackagesStore
+from aikido_zen.sinks import on_import, patch_function, after
+
+
+@after
+def _import(func, instance, args, kwargs, return_value):
+    if not hasattr(return_value, "__file__"):
+        return  # Would be built-in into the interpreter (system package)
+
+    if not hasattr(return_value, "__package__"):
+        return
+    name = getattr(return_value, "__package__")
+
+    if not name or "." in name:
+        # Make sure the name exists and that it's not a submodule
+        return
+    if name == "importlib_metadata":
+        # Avoid circular dependencies
+        return
+
+    if PackagesStore.get_package(name):
+        return
+
+    version = None
+    try:
+        version = importlib.metadata.version(name)
+    except PackageNotFoundError:
+        pass
+    if version:
+        PackagesStore.add_package(name, version)
+
+
+@on_import("builtins")
+def patch(m):
+    """
+    patching module builtins
+    - patches builtins.__import__
+    """
+    patch_function(m, "__import__", _import)
