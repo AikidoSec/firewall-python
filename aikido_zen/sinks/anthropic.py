49edeea66b693f20117103445eed591a3b626af2
BitterPanda <bitterpanda@proton.me>
2025-06-11 23:13:29 +0200

    Add start of anthropic sink

---
+++ aikido_zen/sinks/anthropic.py
@@ -0,0 +1,20 @@
+from aikido_zen.helpers.on_ai_call import on_ai_call
+from aikido_zen.helpers.register_call import register_call
+from aikido_zen.sinks import on_import, patch_function, after
+
+@after
+def _messages_create(func, instance, args, kwargs, return_value):
+    op = f"anthropic.resources.messages.messages.Messages.create"
+    register_call(op, "ai_op")
+
+    on_ai_call(
+        provider="anthropic",
+        model=return_value.model,
+        input_tokens=return_value.usage.input_tokens,
+        output_tokens=return_value.usage.output_tokens,
+    )
+
+
+@on_import("anthropic.resources.messages")
+def patch(m):
+    patch_function(m, "messages.Messages.create", _messages_create)
