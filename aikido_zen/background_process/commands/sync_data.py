82d5cfe0e423dc104c37a36166705a013623cd09
BitterPanda63 <bitterpanda@proton.me>
2025-06-13 12:15:19 +0200

    Update in sync_data

--- aikido_zen/background_process/commands/sync_data.py
+++ aikido_zen/background_process/commands/sync_data.py
@@ -13,24 +13,12 @@ def process_sync_data(connection_manager, data, conn, queue=None):
     BG Process -> Thread : Routes and config
     """
 
-    # Sync routes
-    routes = connection_manager.routes
-    for route in data.get("current_routes", {}).values():
-        route_metadata = {"method": route["method"], "route": route["path"]}
-        if not routes.get(route_metadata):
-            routes.initialize_route(route_metadata)
-        existing_route = routes.get(route_metadata)
-
-        # Update hit count :
-        hits_delta_since_sync = int(route.get("hits_delta_since_sync", 0))
-        existing_route["hits"] += hits_delta_since_sync
-
-        # Update API Spec :
-        update_route_info(route["apispec"], existing_route)
-
     # Save middleware installed :
     if data.get("middleware_installed", False):
         connection_manager.middleware_installed = True
+
+    # Sync routes
+    connection_manager.routes.increment_route(data.get("routes", {}))
 
     # Sync hostnames
     for hostnames_entry in data.get("hostnames", list()):
@@ -56,7 +44,7 @@ def process_sync_data(connection_manager, data, conn, queue=None):
     if connection_manager.conf.last_updated_at > 0:
         # Only report data if the config has been fetched.
         return {
-            "routes": dict(connection_manager.routes.routes),
+            "routes": connection_manager.routes.export(),
             "config": connection_manager.conf,
         }
     return {}
