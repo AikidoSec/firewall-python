933c7e442793fe6164d9c4329bd7376a474a1355
BitterPanda <bitterpanda@proton.me>
2025-06-18 15:01:19 +0200

    Cleanup mistralai module

--- aikido_zen/sinks/mistralai.py
+++ aikido_zen/sinks/mistralai.py
@@ -1,9 +1,3 @@
-"""
-patching module openai
-- patches function create(...) on Responses class, to inspect response
-- patches function create(...) on Completions class, to inspect response
-"""
-
 from aikido_zen.helpers.on_ai_call import on_ai_call
 from aikido_zen.helpers.register_call import register_call
 from aikido_zen.sinks import on_import, patch_function, after
@@ -11,7 +5,7 @@ from aikido_zen.sinks import on_import, patch_function, after
 
 @after
 def _chat_completions_response(func, instance, args, kwargs, return_value):
-    op = f"mistralai.agents.Agents.complete"
+    op = f"{instance.__module__}.{instance.__class__.__name__}.{func.__name__}"
     register_call(op, "ai_op")
 
     on_ai_call(
@@ -25,13 +19,13 @@ def _chat_completions_response(func, instance, args, kwargs, return_value):
 
 @on_import("mistralai", "mistralai", "1.0.0")
 def patch(m):
-    """"
+    """
     patching module mistralai
     - patches function agents.Agents.complete, returns ChatCompletionResponse
     - patches function chat.Chat.complete, returns ChatCompletionResponse
     """
     patch_function(m, "agents.Agents.complete", _chat_completions_response)
-    #patch_function(m, "chat.Chat.parse", _chat_completions_response)
+    # patch_function(m, "chat.Chat.parse", _chat_completions_response)
     patch_function(m, "chat.Chat.complete", _chat_completions_response)
     patch_function(m, "embeddings.Embeddings.create", _chat_completions_response)
     patch_function(m, "fim.Fim.complete", _chat_completions_response)
