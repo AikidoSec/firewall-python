a43da76d845d21cda0f17910c948ef52952f4ae2
BitterPanda63 <bitterpanda@proton.me>
2025-06-25 11:47:36 +0200

    create try_extract_body and try_extract_cookies for django

---
+++ aikido_zen/sources/django/try_extract_body.py
@@ -0,0 +1,32 @@
+import json
+from aikido_zen.helpers.logging import logger
+
+
+def try_extract_body_from_django_request(request):
+    body = None
+    try:
+        # try-catch loading of form parameters, this is to fix issue with DATA_UPLOAD_MAX_NUMBER_FIELDS :
+        try:
+            body = request.POST.dict()
+            if len(body) == 0:
+                body = None  # Reset
+        except Exception:
+            pass
+
+        # Check for JSON or XML :
+        if body is None and request.content_type == "application/json":
+            try:
+                body = json.loads(request.body)
+            except Exception:
+                pass
+        if body is None or len(body) == 0:
+            # E.g. XML Data
+            body = request.body
+        if body is None or len(body) == 0:
+            # During a GET request, django leaves the body as an empty byte string (e.g. `b''`).
+            # When an attack is detected, this body needs to be serialized which would fail.
+            # So a byte string gets converted into a string to stop that from happening.
+            return ""  # Set body to an empty string.
+    except Exception as e:
+        logger.debug("Exception occurred trying to extract django body: %s", e)
+    return body
