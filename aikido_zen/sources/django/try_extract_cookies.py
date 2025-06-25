45623ec76dfc5790eda9399d7c2052a8ffea247b
BitterPanda63 <bitterpanda@proton.me>
2025-06-25 11:47:55 +0200

    Linting

--- aikido_zen/sources/django/try_extract_cookies.py
+++ aikido_zen/sources/django/try_extract_cookies.py
@@ -1,4 +1,6 @@
 from aikido_zen.helpers.logging import logger
+
+
 def try_extract_cookies_from_django_request(request):
     try:
         # https://github.com/django/django/blob/7091801e046dc85dba2238ed4eaf0b3f62bcfc7f/django/core/handlers/wsgi.py#L100
