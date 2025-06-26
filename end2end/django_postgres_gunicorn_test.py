bdeb177b8840de6d21aaeb1a4e829ad19ed90835
BitterPanda <bitterpanda@proton.me>
2025-06-26 12:06:56 +0200

    Add end2end test for new cookie code

--- end2end/django_postgres_gunicorn_test.py
+++ end2end/django_postgres_gunicorn_test.py
@@ -50,6 +50,31 @@ def test_dangerous_response_with_firewall():
         'user': None
     }
 
+
+def test_dangerous_response_with_firewall():
+    dog_name = "Dangerous bobby', TRUE); -- "
+    cookie_header = "dog_name=Dangerous bobby', TRUE) --; ,2=2"
+    res = requests.get(f"{post_url_fw}/via_cookies", headers={
+        "Cookie": cookie_header
+    })
+    assert res.status_code == 500
+
+    time.sleep(5)  # Wait for attack to be reported
+    events = fetch_events_from_mock("http://localhost:5000")
+    attacks = filter_on_event_type(events, "detected_attack")
+
+    assert len(attacks) == 2
+    assert attacks[1]["attack"]["blocked"]
+    assert attacks[1]["attack"]["kind"] == "sql_injection"
+    assert attacks[1]["attack"]["metadata"] == {
+        'dialect': "postgres",
+        'sql': "INSERT INTO sample_app_Dogs (dog_name, is_admin) VALUES ('Dangerous bobby', TRUE) -- ', FALSE)"
+    }
+    assert attacks[1]["pathToPayload"] == ".dog_name"
+    assert attacks[1]["source"] == "cookies"
+    assert attacks[1]["payload"] == "\"Dangerous bobby', TRUE) --\""
+
+
 def test_dangerous_response_without_firewall():
     dog_name = "Dangerous bobby', TRUE); -- "
     res = requests.post(post_url_nofw, data={'dog_name': dog_name})
