95b4f0c0281912807bc231c40a138ba727eba1a5
BitterPanda <bitterpanda@proton.me>
2025-06-26 12:06:56 +0200

    Add end2end test for new cookie code

--- sample-apps/django-postgres-gunicorn/sample_app/urls.py
+++ sample-apps/django-postgres-gunicorn/sample_app/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("dogpage/<int:dog_id>", views.dog_page, name="dog_page"),
-    path("create", views.create_dogpage, name="create")
+    path("create", views.create_dogpage, name="create"),
+    path("create/via_cookies", views.create_dogpage_cookies, name="create_cookies")
 ]
