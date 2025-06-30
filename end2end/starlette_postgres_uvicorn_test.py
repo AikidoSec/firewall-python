6dba26000d252dab2c4ab46ed69fa0ada568e80c
BitterPanda <bitterpanda@proton.me>
2025-06-30 16:43:17 +0200

    Add e2e tests for fixed issue

--- end2end/starlette_postgres_uvicorn_test.py
+++ end2end/starlette_postgres_uvicorn_test.py
@@ -1,5 +1,6 @@
 import time
 import pytest
+import http.client
 import requests
 from .server.check_events_from_mock import fetch_events_from_mock, validate_started_event, filter_on_event_type
 
@@ -57,6 +58,39 @@ def test_dangerous_response_without_firewall():
     dog_name = "Dangerous Bobby', TRUE); -- "
     res = requests.post(post_url_nofw, data={'dog_name': dog_name})
     assert res.status_code == 201
+
+
+def test_dangerous_response_with_firewall_via_headers():
+    conn = http.client.HTTPConnection("localhost", 8102)
+    conn.putrequest("GET", "/create_dog_from_headers")
+    conn.putheader("X-Dog-Name", "Dangerous Bobby', TRUE); -- ")
+    conn.putheader("X-Dog-Name", "safe dog name")
+    conn.endheaders()
+    res = conn.getresponse()
+    assert res.status == 500
+    conn.close()
+
+    time.sleep(5)
+
+    events = fetch_events_from_mock("http://localhost:5000")
+    attacks = filter_on_event_type(events, "detected_attack")
+
+    assert len(attacks) == 2
+    del attacks[0]["attack"]["stack"]
+    assert attacks[0]["attack"]["blocked"] == True
+    assert attacks[0]["attack"]["kind"] == "sql_injection"
+    assert attacks[0]["attack"]["metadata"][
+               "sql"] == "INSERT INTO dogs (dog_name, isAdmin) VALUES ('Dangerous Bobby', TRUE); --', FALSE)"
+    assert attacks[0]["attack"]["metadata"]["dialect"] == "postgres"
+    assert attacks[0]["attack"]["operation"] == "asyncpg.connection.Connection.execute"
+    assert attacks[0]["attack"]["pathToPayload"] == ".X_DOG_NAME.[0]"
+    assert attacks[0]["attack"]["payload"] == "\"Dangerous Bobby', TRUE); --\""
+    assert attacks[0]["attack"]["source"] == "headers"
+    assert attacks[0]["attack"]["user"]["id"] == "user123"
+    assert attacks[0]["attack"]["user"]["name"] == "John Doe"
+
+    assert attacks[0]["request"]["source"] == "starlette"
+    assert attacks[0]["request"]["route"] == "/create_dog_from_headers"
 
 
 def test_sync_route_with_firewall():
