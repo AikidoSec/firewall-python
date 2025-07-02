f9fe2ec6db98bdf5a1a53351141c484ad0e445ed
BitterPanda <bitterpanda@proton.me>
2025-07-02 14:41:17 +0200

    WSGI: now returns WSGIContext

--- aikido_zen/context/wsgi/__init__.py
+++ aikido_zen/context/wsgi/__init__.py
@@ -1,32 +1,41 @@
-"""Exports set_wsgi_attributes_on_context"""
-
+from dataclasses import dataclass
+from typing import Dict, List
 from urllib.parse import parse_qs
 from aikido_zen.helpers.get_ip_from_request import get_ip_from_request
-from aikido_zen.helpers.logging import logger
 from .extract_wsgi_headers import extract_wsgi_headers
 from .build_url_from_wsgi import build_url_from_wsgi
 from ..parse_cookies import parse_cookies
 
 
-def set_wsgi_attributes_on_context(context, environ):
+@dataclass
+class WSGIContext:
+    method: str
+    headers: Dict[str, List[str]]
+    cookies: dict
+    url: str
+    query: dict
+    remote_address: str
+
+
+def parse_wsgi_environ(environ) -> WSGIContext:
     """
     This extracts WSGI attributes, described in :
     https://peps.python.org/pep-3333/#environ-variables
     """
-    logger.debug("Setting wsgi attributes")
-
-    context.method = environ["REQUEST_METHOD"]
-    context.headers = extract_wsgi_headers(environ)
-    if "COOKIE" in context.headers:
-        context.cookies = parse_cookies(context.headers["COOKIE"])
-    else:
-        context.cookies = {}
-    context.url = build_url_from_wsgi(environ)
-    context.query = parse_qs(environ["QUERY_STRING"])
-    context.remote_address = get_ip_from_request(
-        environ["REMOTE_ADDR"], context.headers
-    )
-
+    headers: Dict[str, List[str]] = extract_wsgi_headers(environ)
     # Content type is generally not included as a header, do include this as a header to simplify :
     if "CONTENT_TYPE" in environ:
-        context.headers["CONTENT_TYPE"] = environ["CONTENT_TYPE"]
+        headers["CONTENT_TYPE"] = [environ["CONTENT_TYPE"]]
+
+    cookies = {}
+    if "COOKIE" in headers and headers["COOKIE"]:
+        cookies = parse_cookies(headers["COOKIE"][-1])
+
+    return WSGIContext(
+        method=environ["REQUEST_METHOD"],
+        headers=headers,
+        cookies=cookies,
+        url=build_url_from_wsgi(environ),
+        query=parse_qs(environ["QUERY_STRING"]),
+        remote_address=get_ip_from_request(environ["REMOTE_ADDR"], headers),
+    )
--- aikido_zen/context/wsgi/extract_wsgi_headers.py
+++ aikido_zen/context/wsgi/extract_wsgi_headers.py
@@ -1,11 +1,13 @@
 """Exports function extract_wsgi_headers"""
 
+from typing import Dict, List
 
-def extract_wsgi_headers(request):
+
+def extract_wsgi_headers(request) -> Dict[str, List[str]]:
     """Extracts WSGI headers which start with HTTP_ from request dict"""
     headers = {}
     for key, value in request.items():
         if key.startswith("HTTP_"):
             # Remove the 'HTTP_' prefix and store in the headers dictionary
-            headers[key[5:]] = value
+            headers[key[5:]] = [value]
     return headers
