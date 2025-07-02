b5047f9dc496049120317f0a6d6d3d847e653db5
BitterPanda <bitterpanda@proton.me>
2025-07-02 16:11:05 +0200

    Update api discovery logic to use new Headers object

--- aikido_zen/api_discovery/get_body_data_type_test.py
+++ aikido_zen/api_discovery/get_body_data_type_test.py
@@ -1,23 +1,51 @@
 import pytest
 from .get_body_data_type import get_body_data_type
+from ..helpers.headers import Headers
 
 
 def test_get_body_data_type():
-    assert get_body_data_type({"CONTENT_TYPE": "application/json"}) == "json"
-    assert get_body_data_type({"CONTENT_TYPE": "application/vnd.api+json"}) == "json"
-    assert get_body_data_type({"CONTENT_TYPE": "application/csp-report"}) == "json"
-    assert get_body_data_type({"CONTENT_TYPE": "application/x-json"}) == "json"
-    assert (
-        get_body_data_type({"CONTENT_TYPE": "application/x-www-form-urlencoded"})
-        == "form-urlencoded"
-    )
-    assert get_body_data_type({"CONTENT_TYPE": "multipart/form-data"}) == "form-data"
-    assert get_body_data_type({"CONTENT_TYPE": "text/xml"}) == "xml"
-    assert get_body_data_type({"CONTENT_TYPE": "text/html"}) is None
-    assert (
-        get_body_data_type({"CONTENT_TYPE": ["application/json", "text/html"]})
-        == "json"
-    )
-    assert get_body_data_type({"x-test": "abc"}) is None
-    assert get_body_data_type(None) is None  # Testing invalid input
-    assert get_body_data_type({}) is None
+    headers = Headers()
+    headers.store_header("CONTENT_TYPE", "application/json")
+    assert get_body_data_type(headers) == "json"
+
+    headers = Headers()
+    headers.store_header("CONTENT_TYPE", "application/vnd.api+json")
+    assert get_body_data_type(headers) == "json"
+
+    headers = Headers()
+    headers.store_header("CONTENT_TYPE", "application/csp-report")
+    assert get_body_data_type(headers) == "json"
+
+    headers = Headers()
+    headers.store_header("CONTENT_TYPE", "application/x-json")
+    assert get_body_data_type(headers) == "json"
+
+    headers = Headers()
+    headers.store_header("CONTENT_TYPE", "application/x-www-form-urlencoded")
+    assert get_body_data_type(headers) == "form-urlencoded"
+
+    headers = Headers()
+    headers.store_header("CONTENT_TYPE", "multipart/form-data")
+    assert get_body_data_type(headers) == "form-data"
+
+    headers = Headers()
+    headers.store_header("CONTENT_TYPE", "text/xml")
+    assert get_body_data_type(headers) == "xml"
+
+    headers = Headers()
+    headers.store_header("CONTENT_TYPE", "text/html")
+    assert get_body_data_type(headers) is None
+
+    headers = Headers()
+    headers.store_header("CONTENT_TYPE", "application/json, text/html")
+    assert get_body_data_type(headers) == "json"
+
+    headers = Headers()
+    headers.store_header("x-test", "abc")
+    assert get_body_data_type(headers) is None
+
+    headers = Headers()
+    assert get_body_data_type(headers) is None  # Testing invalid input
+
+    headers = Headers()
+    assert get_body_data_type(headers) is None
