39cbb033854f063176741f2c813af45eb7264b2e
BitterPanda63 <bitterpanda@proton.me>
2025-07-14 10:27:10 +0200

    fix IPv6 loopback bug

--- aikido_zen/vulnerabilities/ssrf/find_hostname_in_userinput.py
+++ aikido_zen/vulnerabilities/ssrf/find_hostname_in_userinput.py
@@ -2,6 +2,8 @@
 Only exports find_hostname_in_userinput function
 """
 
+from typing import List
+
 from aikido_zen.helpers.get_port_from_url import get_port_from_url
 from aikido_zen.helpers.try_parse_url import try_parse_url
 
@@ -13,14 +15,12 @@ def find_hostname_in_userinput(user_input, hostname, port=None):
     if len(user_input) <= 1:
         return False
 
-    hostname_url = try_parse_url(f"http://{hostname}")
-    if not hostname_url:
-        return False
+    hostname_options = get_hostname_options(hostname)
 
     variants = [user_input, f"http://{user_input}", f"https://{user_input}"]
     for variant in variants:
         user_input_url = try_parse_url(variant)
-        if user_input_url and user_input_url.hostname == hostname_url.hostname:
+        if user_input_url and user_input_url.hostname in hostname_options:
             user_port = get_port_from_url(user_input_url.geturl())
 
             # We were unable to retrieve the port from the URL, likely because it contains an invalid port.
@@ -35,3 +35,17 @@ def find_hostname_in_userinput(user_input, hostname, port=None):
                 return True
 
     return False
+
+
+def get_hostname_options(raw_hostname: str) -> List[str]:
+    options = []
+    hostname_url = try_parse_url(f"http://{raw_hostname}")
+    if hostname_url and hostname_url.hostname:
+        options.append(hostname_url.hostname)
+
+    # Add a case for hostnames like ::1 or ::ffff:127.0.0.1, who need brackets to be parsed
+    hostname_url_ipv6 = try_parse_url(f"http://[{raw_hostname}]")
+    if hostname_url_ipv6 and hostname_url_ipv6.hostname:
+        options.append(hostname_url_ipv6.hostname)
+
+    return options
