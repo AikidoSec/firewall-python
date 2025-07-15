ee054ac9fd77f49a8e039caf856207614cdec702
BitterPanda <bitterpanda@proton.me>
2025-07-15 12:01:35 +0200

    Call the get_hostname_options in find_hostname_in_context instead

--- aikido_zen/vulnerabilities/ssrf/find_hostname_in_context.py
+++ aikido_zen/vulnerabilities/ssrf/find_hostname_in_context.py
@@ -4,6 +4,7 @@ Mainly exports function `find_hostname_in_context`
 
 from aikido_zen.helpers.extract_strings_from_context import extract_strings_from_context
 from .find_hostname_in_userinput import find_hostname_in_userinput
+from .get_hostname_options import get_hostname_options
 from .is_request_to_itself import is_request_to_itself
 from ...context import Context
 
@@ -19,8 +20,11 @@ def find_hostname_in_context(hostname, context: Context, port):
     if is_request_to_itself(context.url, hostname, port):
         return None
 
+    # Gets the different hostname options: with/without punycode, with/without brackets for IPv6
+    hostname_options = get_hostname_options(hostname)
+
     for user_input, path, source in extract_strings_from_context(context):
-        found = find_hostname_in_userinput(user_input, hostname, port)
+        found = find_hostname_in_userinput(user_input, hostname_options, port)
         if found:
             return {
                 "source": source,
