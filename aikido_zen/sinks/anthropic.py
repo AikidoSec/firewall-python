4074ec178cd43ac05fc3e74c51902dfe812a91b9
BitterPanda63 <bitterpanda@proton.me>
2025-08-04 15:34:14 +0200

    anthropic add create_async wrapp

--- aikido_zen/sinks/anthropic.py
+++ aikido_zen/sinks/anthropic.py
@@ -1,6 +1,6 @@
 from aikido_zen.helpers.on_ai_call import on_ai_call
 from aikido_zen.helpers.register_call import register_call
-from aikido_zen.sinks import on_import, patch_function, after
+from aikido_zen.sinks import on_import, patch_function, after, after_async
 
 
 @after
@@ -16,6 +16,20 @@ def _messages_create(func, instance, args, kwargs, return_value):
     )
 
 
+@after_async
+async def _messages_create_async(func, instance, args, kwargs, return_value):
+    op = f"anthropic.resources.messages.messages.Messages.create"
+    register_call(op, "ai_op")
+
+    on_ai_call(
+        provider="anthropic",
+        model=return_value.model,
+        input_tokens=return_value.usage.input_tokens,
+        output_tokens=return_value.usage.output_tokens,
+    )
+
+
 @on_import("anthropic.resources.messages")
 def patch(m):
     patch_function(m, "messages.Messages.create", _messages_create)
+    patch_function(m, "messages.AsyncMessages.create", _messages_create_async)
