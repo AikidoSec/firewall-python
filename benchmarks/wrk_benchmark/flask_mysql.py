f3dede4d70f20683b84fe8e68207dd43ff055567
BitterPanda <bitterpanda@proton.me>
2025-08-08 14:06:12 +0200

    Also add benchmarks to flask-mysql

---
+++ benchmarks/wrk_benchmark/flask_mysql.py
@@ -0,0 +1,10 @@
+from script import run_benchmark
+
+# Run benchmarks
+
+run_benchmark(
+    "http://localhost:8086/create",
+    "http://localhost:8087/create",
+    "",
+    percentage_limit=15
+)
