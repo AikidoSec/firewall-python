f272510272b270f7d3f64b8b042af142f98da684
BitterPanda <bitterpanda@proton.me>
2025-08-14 13:27:09 +0200

    Add benchmark for flask-mysql-gunicorn

---
+++ benchmarks/wrk_benchmark/flask_mysql_gunicorn.py
@@ -0,0 +1,10 @@
+from script import run_benchmark
+
+# Run benchmarks
+
+run_benchmark(
+    "http://localhost:8110/benchmark",
+    "http://localhost:8111/benchmark",
+    "a non empty route which makes a simulated request to a database",
+    percentage_limit=15
+)
