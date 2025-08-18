2ffd105264fa0b2e42d6ae6295b20701a3ea6bfa
BitterPanda <bitterpanda@proton.me>
2025-08-18 14:38:01 +0200

    Set wrk benchmark thresholds to similar numbers found in node

--- benchmarks/wrk_benchmark/starlette_postgres_uvicorn.py
+++ benchmarks/wrk_benchmark/starlette_postgres_uvicorn.py
@@ -6,10 +6,10 @@ run_benchmark(
     "http://localhost:8102/delayed_route",
     "http://localhost:8103/delayed_route",
     "a non empty route which makes a simulated request to a database",
-    percentage_limit=15
+    percentage_limit=30
 )
 
 run_benchmark(
     "http://localhost:8102/just", "http://localhost:8103/just", "an empty route",
-    percentage_limit=30
+    percentage_limit=40
 )
