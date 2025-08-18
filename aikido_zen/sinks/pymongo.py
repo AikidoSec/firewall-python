a9e18f9899b4a0d7039897d9545894615a73e24d
BitterPanda <bitterpanda@proton.me>
2025-08-18 19:43:24 +0200

    update non-compliant files

--- aikido_zen/sinks/pymongo.py
+++ aikido_zen/sinks/pymongo.py
@@ -4,10 +4,10 @@ Sink module for `pymongo`
 
 import wrapt
 
-from aikido_zen.helpers.get_argument import get_argument
 import aikido_zen.vulnerabilities as vulns
-from . import patch_function, on_import, before
 from aikido_zen.helpers.register_call import register_call
+from aikido_zen.helpers.get_argument import get_argument
+from . import patch_function, on_import, before
 
 
 @before
