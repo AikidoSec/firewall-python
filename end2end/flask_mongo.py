e34236d2afe68108168db53ff3c78132a959552f
BitterPanda <bitterpanda@proton.me>
2025-08-25 16:25:36 +0200

    Create new reformed flask_mongo test cases

---
+++ end2end/flask_mongo.py
@@ -0,0 +1,26 @@
+from .utils import App, Request
+
+flask_mongo_app = App(8094)
+
+def create_test_dog():
+    Request("/create", data_type="form", body={
+        {"dog_name": "bobby", "pswd": "1234"}
+    }).execute(flask_mongo_app.urls["enabled"])
+
+flask_mongo_app.add_payload(
+    "test_nosql_injection",
+    safe_request=Request("/auth", body={"dog_name": "bobby", "pswd": "1234"}),
+    unsafe_request=Request("/auth", body={"dog_name": "bobby", "pswd": { "$ne": ""}}),
+    test_event={
+        "blocked": True,
+        "kind": "nosql_injection",
+        'metadata': {'filter': '{"dog_name": "bobby_tables", "pswd": {"$ne": ""}}'},
+        'operation': "pymongo.collection.Collection.find",
+        'pathToPayload': ".pswd",
+        'payload': '{"$ne": ""}',
+        'source': "body",
+    }
+)
+
+create_test_dog()
+flask_mongo_app.test_all_payloads()
