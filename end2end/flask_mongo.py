00a3cce23b013af97dbcfd57d18a19f86a18e673
BitterPanda <bitterpanda@proton.me>
2025-08-25 17:12:54 +0200

    Fix flask-mongo e2e test cases

--- end2end/flask_mongo.py
+++ end2end/flask_mongo.py
@@ -1,3 +1,5 @@
+import json
+
 from utils import App, Request
 
 flask_mongo_app = App(8094)
@@ -9,8 +11,12 @@ def create_test_dog():
 
 flask_mongo_app.add_payload(
     "test_nosql_injection",
-    safe_request=Request("/auth", body={"dog_name": "bobby", "pswd": "1234"}),
-    unsafe_request=Request("/auth", body={"dog_name": "bobby", "pswd": { "$ne": ""}}),
+    safe_request=Request("/auth", data_type="form", body=json.dumps(
+        {"dog_name": "bobby", "pswd": "1234"}
+    )),
+    unsafe_request=Request("/auth", data_type="form", body=json.dumps(
+        {"dog_name": "bobby", "pswd": { "$ne": ""}}
+    )),
     test_event={
         "blocked": True,
         "kind": "nosql_injection",
