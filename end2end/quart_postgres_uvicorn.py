8c213c81784b348e36137e67b64a3ce53fe05dfe
BitterPanda <bitterpanda@proton.me>
2025-08-25 16:45:28 +0200

    Fix quart_postgres_uvicorn e2e test cases

---
+++ end2end/quart_postgres_uvicorn.py
@@ -0,0 +1,25 @@
+from end2end.utils import assert_eq
+from utils import App, Request
+
+quart_postgres_uvicorn_app = App(8096)
+
+quart_postgres_uvicorn_app.add_payload(
+    "test_sql_injection",
+    safe_request=Request("/app/create", data_type="form", body={"dog_name": "Bobby Tables"}),
+    unsafe_request=Request("/app/create", data_type="form", body={"dog_name": "Dangerous Bobby', TRUE); -- "}),
+    test_event={
+        "blocked": True,
+        "kind": "sql_injection",
+        "metadata": {
+            'dialect': "postgres",
+            'sql': "INSERT INTO dogs (dog_name, isAdmin) VALUES ('Dangerous Bobby', TRUE); -- ', FALSE)"
+        },
+        "operation": "asyncpg.connection.Connection.execute",
+        "pathToPayload": ".dog_name",
+        "payload": "\"Dangerous Bobby', TRUE); -- \"",
+        "source": "body",
+        "user_id": "user123"
+    }
+)
+
+quart_postgres_uvicorn_app.test_all_payloads()
