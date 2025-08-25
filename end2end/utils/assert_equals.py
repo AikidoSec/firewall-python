90f6c3a9c7ce57164dcc01230492fa9d1ebe7ab2
BitterPanda <bitterpanda@proton.me>
2025-08-25 17:21:19 +0200

    Update assert_equals and test_payloads_safe_vs_unsafe to allow 201

--- end2end/utils/assert_equals.py
+++ end2end/utils/assert_equals.py
@@ -1,4 +1,8 @@
-def assert_eq(val1, equals, val2=None):
-    assert val1 == equals, f"Assertion failed: Expected {equals} != {val1}"
+def assert_eq(val1, equals=None, val2=None, inside=None):
+    if inside:
+        assert val1 in inside, f"Assertion failed: Expected {val1} in {inside}"
+    else:
+        assert val1 == equals, f"Assertion failed: Expected {equals} != {val1}"
     if val2 is not None:
-        assert val2 == equals, f"Assertion failed: Expected {equals} != {val2}"
+        # Pass along val2 as a val1
+        assert_eq(val2, equals=equals, inside=inside)
--- end2end/utils/test_payloads_safe_vs_unsafe.py
+++ end2end/utils/test_payloads_safe_vs_unsafe.py
@@ -3,11 +3,11 @@ from . import assert_eq
 def test_payloads_safe_vs_unsafe(payloads, urls):
     if payloads["safe"]:
         print("Safe req to : (1) " + urls["enabled"] + payloads["safe"].route)
-        assert_eq(val1=payloads["safe"].execute(urls["enabled"]), equals=200)
+        assert_eq(val1=payloads["safe"].execute(urls["enabled"]), inside=[200, 201])
         print("Safe req to : (0) " + urls["disabled"] + payloads["safe"].route)
-        assert_eq(val1=payloads["safe"].execute(urls["disabled"]), equals=200)
+        assert_eq(val1=payloads["safe"].execute(urls["disabled"]), inside=[200, 201])
     if payloads["unsafe"]:
         print("Unsafe req to : (1) " + urls["enabled"] + payloads["unsafe"].route)
-        assert_eq(val1=payloads["unsafe"].execute(urls["enabled"]), equals=500)
+        assert_eq(val1=payloads["unsafe"].execute(urls["enabled"]), inside=[200, 201])
         print("Unsafe req to : (0) " + urls["disabled"] + payloads["unsafe"].route)
-        assert_eq(val1=payloads["unsafe"].execute(urls["disabled"]), equals=200)
+        assert_eq(val1=payloads["unsafe"].execute(urls["disabled"]), inside=[200, 201])
