4bee965eb94ce4d29f6b235b86e05abaf00d3333
BitterPanda <bitterpanda@proton.me>
2025-08-25 16:59:04 +0200

    Update flask-postgres & flask-mysql-uwsgi test cases to new test system

---
+++ end2end/flask_mysql_uwsgi.py
@@ -0,0 +1,23 @@
+from utils import App, Request
+
+flask_mysql_uwsgi_app = App(8088)
+
+flask_mysql_uwsgi_app.add_payload(
+    "test_sql_injection",
+    safe_request=Request("/create", data_type="form", body={"dog_name": "Bobby Tables"}),
+    unsafe_request=Request("/create", data_type="form", body={"dog_name": 'Dangerous bobby", 1); -- '}),
+    test_event={
+        "blocked": True,
+        "kind": "sql_injection",
+        'metadata': {
+            'dialect': 'mysql',
+            'sql': 'INSERT INTO sample_app_dogs (dog_name, dog_boss) VALUES ("Dangerous bobby", 1); -- ", "N/A")'
+        },
+        'operation': 'MySQLdb.Cursor.execute',
+        'pathToPayload': '.dog_name',
+        'payload': '"Dangerous bobby\\", 1); -- "',
+        'source': "body",
+    }
+)
+
+flask_mysql_uwsgi_app.test_all_payloads()
---
+++ end2end/flask_postgres.py
@@ -0,0 +1,45 @@
+from utils import App, Request
+
+flask_postgres_app = App(8090)
+
+flask_postgres_app.add_payload(
+    "test_sql_injection",
+    safe_request=Request("/create", data_type="form", body={"dog_name": "Bobby Tables"}),
+    unsafe_request=Request("/create", data_type="form", body={"dog_name": "Dangerous Bobby', TRUE); -- "}),
+    test_event={
+        "blocked": True,
+        "kind": "sql_injection",
+        "metadata": {
+            "dialect": "postgres",
+            "sql": "INSERT INTO dogs (dog_name, isAdmin) VALUES ('Dangerous Bobby', TRUE); -- ', FALSE)"
+        },
+        "operation": "psycopg2.Connection.Cursor.execute",
+        "pathToPayload": '.dog_name.[0]',
+        "payload": '"Dangerous Bobby\', TRUE); -- "',
+        "source": "body",
+    }
+)
+
+flask_postgres_app.add_payload(
+    "test_sql_injection_via_cookies",
+    safe_request=Request("/create_with_cookie", "GET", headers={
+        "Cookie": "dog_name=Safe Dog; ,2=2"
+    }),
+    unsafe_request=Request("/create_with_cookie", "GET", headers={
+        "Cookie": "dog_name=Dangerous bobby', TRUE) --; ,2=2"
+    }),
+    test_event={
+        "blocked": True,
+        "kind": "sql_injection",
+        'metadata': {
+            'dialect': "postgres",
+            'sql': "INSERT INTO sample_app_Dogs (dog_name, is_admin) VALUES ('Dangerous bobby', TRUE) --', FALSE)"
+        },
+        'operation': "psycopg2.Connection.Cursor.execute",
+        'pathToPayload': '.dog_name',
+        'payload': "\"Dangerous bobby', TRUE) --\"",
+        'source': "cookies",
+    }
+)
+
+flask_postgres_app.test_all_payloads()
