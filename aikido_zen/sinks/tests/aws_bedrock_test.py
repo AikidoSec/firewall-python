8634b9f463f240c25749f9a87d5745a6108d403c
BitterPanda <bitterpanda@proton.me>
2025-08-26 21:25:19 +0200

    Update code to already work for Converse for botocore

--- aikido_zen/sinks/tests/aws_bedrock_test.py
+++ aikido_zen/sinks/tests/aws_bedrock_test.py
@@ -1,3 +1,4 @@
+import json
 import os
 import aikido_zen.sinks.botocore
 import pytest
@@ -33,7 +34,7 @@ def test_boto3_converse(client):
     metadata = {
         "model": "anthropic.claude-3-sonnet-20240229-v1:0",
         "prompt": "Are tomatoes a fruit?",
-        "max_tokens": 500,
+        "max_tokens": 20,
     }
 
     response = client.converse(
@@ -45,7 +46,51 @@ def test_boto3_converse(client):
             "maxTokens": metadata["max_tokens"],
         },
     )
-
-    # Extract and print the response
     output = response["output"]["message"]["content"][0]["text"]
-    print(output)
+
+    assert get_ai_stats()[0]["model"] == "claude-3-sonnet-20240229"
+    assert get_ai_stats()[0]["calls"] == 1
+    assert get_ai_stats()[0]["provider"] == "anthropic"
+    assert get_ai_stats()[0]["tokens"]["input"] == 13
+    assert get_ai_stats()[0]["tokens"]["output"] == 20
+    assert get_ai_stats()[0]["tokens"]["total"] == 33
+
+
+@skip_no_api_key
+def test_boto3_invoke_model_claude_3_sonnet(client):
+    model_id = "amazon.titan-tg1-large"  # Example model ID for Amazon Bedrock
+    input_payload = {
+        "inputText": "Hello, how are you?",
+        "textGenerationConfig": {"maxTokenCount": 100, "temperature": 0.7},
+    }
+    response = client.invoke_model(modelId=model_id, body=json.dumps(input_payload))
+    stats = get_ai_stats()[0]
+    assert stats["model"] == "llama2-70b-chat"
+    assert stats["calls"] == 1
+    assert stats["provider"] == "meta"
+    assert stats["tokens"]["input"] == 0
+    assert stats["tokens"]["output"] == 30
+
+
+@skip_no_api_key
+def test_boto3_invoke_model_meta_llama3_8b(client):
+    metadata = {
+        "model": "meta.llama3-8b-instruct-v1:0",
+        "prompt": "Who painted the Mona Lisa?",
+        "max_tokens": 20,
+    }
+    body = {
+        "prompt": metadata["prompt"],
+        "max_gen_len": metadata["max_tokens"],
+        "temperature": 0.5,
+    }
+    response = client.invoke_model(
+        modelId=metadata["model"],
+        body=json.dumps(body).encode("utf-8"),
+    )
+    stats = get_ai_stats()[0]
+    assert stats["model"] == "llama3-8b-instruct"
+    assert stats["calls"] == 1
+    assert stats["provider"] == "meta"
+    assert stats["tokens"]["input"] == 3
+    assert stats["tokens"]["output"] == 20
