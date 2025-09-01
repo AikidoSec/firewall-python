cbb0f52dd8f8968dce907c50ea397a97a5ba3013
BitterPanda <bitterpanda@proton.me>
2025-09-01 15:57:00 +0200

    set_rate_limit_group add error messages

--- aikido_zen/middleware/set_rate_limit_group.py
+++ aikido_zen/middleware/set_rate_limit_group.py
@@ -4,16 +4,20 @@ from aikido_zen.helpers.logging import logger
 
 def set_rate_limit_group(group_id: str):
     if not group_id or not isinstance(group_id, str):
-        logger.warning("")
+        logger.warning("Group ID cannot be empty.")
         return
 
     context = get_current_context()
     if not context:
-        logger.warning("")
+        logger.warning(
+            "set_rate_limit_group(...) was called without a context. Make sure to call set_rate_limit_group(...) within an HTTP request."
+        )
         return
 
     if context.executed_middleware:
-        logger.warning("")
+        logger.warning(
+            "set_rate_limit_group(...) must be called before the Zen middleware is executed."
+        )
 
     context.rate_limit_group = group_id
     context.set_as_current_context()
