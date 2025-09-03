55e6cf02bf5983e1737f1361f6af232e0920bc8e
BitterPanda <bitterpanda@proton.me>
2025-09-03 14:23:25 +0200

    Add log & debug options for aws lambda

--- aikido_zen/background_process/commands/send_heartbeat.py
+++ aikido_zen/background_process/commands/send_heartbeat.py
@@ -1,6 +1,10 @@
+from aikido_zen.helpers.logging import logger
+
 def process_send_heartbeat(connection_manager, data, queue):
     """
     SEND_HEARTBEAT: Used by lambdas to flush data.
     """
+    logger.debug("SEND_HEARTBEAT start [->]")
     connection_manager.send_heartbeat()
+    logger.debug("SEND_HEARTBEAT END [<-]")
     return {"status": "Heartbeat sent"}
