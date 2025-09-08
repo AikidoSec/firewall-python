1076ea4b3a59e4f5ebe8b60c7578637016c47004
BitterPanda <bitterpanda@proton.me>
2025-09-08 17:50:52 +0200

    Make sure to set the correct return type for ReportingApi

--- aikido_zen/background_process/api/__init__.py
+++ aikido_zen/background_process/api/__init__.py
@@ -2,12 +2,14 @@
 init.py file for api/ folder. Includes abstract class ReportingApi
 """
 
+from aikido_zen.background_process.api.helpers import Response
+
 
 class ReportingApi:
     """This is the super class for the reporting API's"""
 
-    def report(self, token, event, timeout_in_sec):
+    def report(self, token, event, timeout_in_sec) -> Response:
         """Report event to aikido server"""
 
-    def fetch_firewall_lists(self, token):
+    def fetch_firewall_lists(self, token) -> Response:
         """Fetches the different lists from aikido's servers"""
