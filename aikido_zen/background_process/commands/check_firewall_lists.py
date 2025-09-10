d7eb898edc6dbba32c51348ccc23ddc74283855e
BitterPanda <bitterpanda@proton.me>
2025-09-10 16:12:41 +0200

    Rename to check_firewall_lists_res

--- aikido_zen/background_process/commands/check_firewall_lists.py
+++ aikido_zen/background_process/commands/check_firewall_lists.py
@@ -5,7 +5,7 @@ def process_check_firewall_lists(connection_manager, data):
     """
     Checks whether an IP is blocked and whether there is an ongoing attack wave
     data: {"ip": string, "user-agent": string, "is_attack_wave_request" ?: bool}
-    returns -> FirewallListsCheckRes
+    returns -> CheckFirewallListsRes
     """
     ip = data["ip"]
     if ip is not None and isinstance(ip, str):
@@ -16,23 +16,23 @@ def process_check_firewall_lists(connection_manager, data):
         # Global IP Blocklist (e.g. blocking known threat actors)
         reason = connection_manager.firewall_lists.is_blocked_ip(ip)
         if reason:
-            return FirewallListsCheckRes(blocked=True, type="blocklist", reason=reason)
+            return CheckFirewallListsRes(blocked=True, type="blocklist", reason=reason)
 
     user_agent = data["user-agent"]
     if user_agent is not None and isinstance(user_agent, str):
         # User agent blocking (e.g. blocking AI scrapers)
         if connection_manager.firewall_lists.is_user_agent_blocked(user_agent):
-            return FirewallListsCheckRes(blocked=True, type="bot-blocking")
+            return CheckFirewallListsRes(blocked=True, type="bot-blocking")
 
     is_attack_wave_request = data.get("is_attack_wave_request", None)
     if is_attack_wave_request and ip is not None:
         if connection_manager.attack_wave_detector.check(ip):
-            return FirewallListsCheckRes(blocked=False, is_attack_wave=True)
+            return CheckFirewallListsRes(blocked=False, is_attack_wave=True)
 
-    return FirewallListsCheckRes()
+    return CheckFirewallListsRes()
 
 
-class FirewallListsCheckRes:
+class CheckFirewallListsRes:
     def __init__(self, blocked=False, is_attack_wave=False, type=None, reason=None):
         self.blocked = blocked
         self.is_attack_wave = is_attack_wave
