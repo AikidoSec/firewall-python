66d59a56e972cfce68d91d836661688c7bc0b3d0
BitterPanda <bitterpanda@proton.me>
2025-09-10 15:57:14 +0200

    check_firewall_lists, use a class as a common data type, and check attack wave detector

--- aikido_zen/background_process/commands/check_firewall_lists.py
+++ aikido_zen/background_process/commands/check_firewall_lists.py
@@ -3,9 +3,9 @@
 
 def process_check_firewall_lists(connection_manager, data):
     """
-    Checks whether an IP is blocked
-    data: {"ip": string, "user-agent": string}
-    returns -> {"blocked": boolean, "type": string, "reason": string}
+    Checks whether an IP is blocked and whether there is an ongoing attack wave
+    data: {"ip": string, "user-agent": string, "is_attack_wave_request" ?: bool}
+    returns -> FirewallListsCheckRes
     """
     ip = data["ip"]
     if ip is not None and isinstance(ip, str):
@@ -16,21 +16,25 @@ def process_check_firewall_lists(connection_manager, data):
         # Global IP Blocklist (e.g. blocking known threat actors)
         reason = connection_manager.firewall_lists.is_blocked_ip(ip)
         if reason:
-            return {
-                "blocked": True,
-                "type": "blocklist",
-                "reason": reason,
-            }
+            return FirewallListsCheckRes(blocked=True, type="blocklist", reason=reason)
 
     user_agent = data["user-agent"]
     if user_agent is not None and isinstance(user_agent, str):
         # User agent blocking (e.g. blocking AI scrapers)
         if connection_manager.firewall_lists.is_user_agent_blocked(user_agent):
-            return {
-                "blocked": True,
-                "type": "bot-blocking",
-            }
+            return FirewallListsCheckRes(blocked=True, type="bot-blocking")
 
-    return {
-        "blocked": False,
-    }
+    is_attack_wave_request = data.get("is_attack_wave_request", None)
+    if is_attack_wave_request and ip is not None:
+        if connection_manager.attack_wave_detector.check(ip):
+            return FirewallListsCheckRes(blocked=False, is_attack_wave=True)
+
+    return FirewallListsCheckRes()
+
+
+class FirewallListsCheckRes:
+    def __init__(self, blocked=False, is_attack_wave=False, type=None, reason=None):
+        self.blocked = blocked
+        self.is_attack_wave = is_attack_wave
+        self.type = type
+        self.reason = reason
