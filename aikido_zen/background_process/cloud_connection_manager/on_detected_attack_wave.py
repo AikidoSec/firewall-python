d6cf778c6c6d05ada3d662fa81cd758a3ba9ff3c
BitterPanda63 <bitterpanda@proton.me>
2025-09-11 11:11:00 +0200

    separate concerns in on_detected_attack_wave

--- aikido_zen/background_process/cloud_connection_manager/on_detected_attack_wave.py
+++ aikido_zen/background_process/cloud_connection_manager/on_detected_attack_wave.py
@@ -1,30 +1,33 @@
 from aikido_zen.context import Context
 from aikido_zen.helpers.get_current_unixtime_ms import get_unixtime_ms
 from aikido_zen.helpers.logging import logger
-from aikido_zen.helpers.serialize_to_json import serialize_to_json
 
 
 def on_detected_attack_wave(connection_manager, context: Context, metadata):
     if not connection_manager.token:
         return
     try:
-        payload = {
-            "type": "detected_attack_wave",
-            "time": get_unixtime_ms(),
-            "agent": connection_manager.get_manager_info(),
-            "attack": {"metadata": metadata, "user": context.user},
-            "request": {
-                "ipAddress": context.remote_address,
-                "userAgent": context.get_user_agent(),
-                "source": context.source,
-            },
-        }
-        logger.debug(serialize_to_json(payload))
+        agent_info = connection_manager.get_manager_info()
+        payload = create_attack_wave_payload(agent_info, context, metadata)
+
         connection_manager.api.report(
             connection_manager.token,
             payload,
             connection_manager.timeout_in_sec,
         )
     except Exception as e:
-        logger.debug(e)
-        logger.info("Failed to report an attack wave")
+        logger.info("Failed to report an attack wave (%s)", str(e))
+
+
+def create_attack_wave_payload(agent_info, context: Context, metadata):
+    return {
+        "type": "detected_attack_wave",
+        "time": get_unixtime_ms(),
+        "agent": agent_info,
+        "attack": {"metadata": metadata, "user": context.user},
+        "request": {
+            "ipAddress": context.remote_address,
+            "userAgent": context.get_user_agent(),
+            "source": context.source,
+        },
+    }
