f43a0d56735d6e7647f24c0bf09278a7ab08c820
BitterPanda63 <bitterpanda@proton.me>
2025-09-12 11:11:02 +0200

    Update test cases for http_api

--- aikido_zen/background_process/api/http_api_test.py
+++ aikido_zen/background_process/api/http_api_test.py
@@ -1,57 +1,41 @@
 import pytest
-import requests
-from unittest.mock import patch
-from aikido_zen.background_process.api.http_api import (
-    ReportingApiHTTP,
-)  # Replace with the actual module name
+from aikido_zen.background_process.api.http_api import ReportingApiHTTP
 
 # Sample event data for testing
 sample_event = {"event_type": "test_event", "data": {"key": "value"}}
 
 
-def test_report_data_401_code(monkeypatch):
-    # Create an instance of ReportingApiHTTP
-    api = ReportingApiHTTP("http://mocked-url.com/")
+def test_report_data_401_code():
+    api = ReportingApiHTTP("https://guard.aikido.dev/")
 
-    # Mock the requests.post method to return a successful response
-    class MockResponse:
-        def __init__(self, json_data, status_code):
-            self.json_data = json_data
-            self.status_code = status_code
+    response = api.report("wrong_token", sample_event, 5)
 
-        def json(self):
-            return self.json_data
-
-    def mock_post(url, json, timeout, headers):
-        return MockResponse({"success": False}, 401)
-
-    monkeypatch.setattr(requests, "post", mock_post)
-
-    # Call the report method
-    response = api.report("mocked_token", sample_event, 5)
-
-    # Assert the response
     assert response == {"success": False, "error": "invalid_token"}
 
 
-def test_report_connection_error(monkeypatch):
-    # Create an instance of ReportingApiHTTP
-    api = ReportingApiHTTP("http://mocked-url.com/")
+def test_report_local_valid():
+    api = ReportingApiHTTP("http://localhost:5000/")
 
-    # Mock the requests.post method to raise a ConnectionError
-    monkeypatch.setattr(
-        requests,
-        "post",
-        lambda *args, **kwargs: (_ for _ in ()).throw(
-            requests.exceptions.ConnectionError
-        ),
-    )
-
-    # Call the report method
     response = api.report("mocked_token", sample_event, 5)
 
-    # Assert the response
-    assert response == {"success": False, "error": "timeout"}
+    assert response == {
+        "success": True,
+        "blockedUserIds": [],
+        "endpoints": [
+            {
+                "forceProtectionOff": False,
+                "graphql": False,
+                "method": "*",
+                "rateLimiting": {
+                    "enabled": True,
+                    "maxRequests": 2,
+                    "windowSizeInMS": 5000,
+                },
+                "route": "/test_ratelimiting_1",
+            }
+        ],
+        "receivedAnyStats": False,
+    }
 
 
 def test_report_other_exception(monkeypatch):
