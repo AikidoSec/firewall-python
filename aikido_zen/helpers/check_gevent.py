f9b1fff0d95a4a53cf3e64545d7b08269bdc56d7
bitterpanda <bitterpanda@proton.me>
2025-09-18 13:11:01 +0000

    Update aikido_zen/helpers/check_gevent.py

--- aikido_zen/helpers/check_gevent.py
+++ aikido_zen/helpers/check_gevent.py
@@ -10,7 +10,7 @@ def check_gevent():
             + "Due to the way gevent monkey-patches and alters the behaviour of "
             + "Python-native modules (like \033[1m`multiprocessing`\033[0m), \n"
             + "\033[1mZen cannot function with gevent installed.\033[0m "
-            + "Find out more in the gunicorn docs for Zen."
+            + "Find out more in the gUnicorn docs for Zen."
         )
         return True
     return False
