17779a8aa4636edc6cdc1e8ccb224fdfdbb31628
bitterpanda <bitterpanda@proton.me>
2025-09-30 12:06:19 +0200

    Add a PERF_SKIP_REGISTER_CALL to test whether the registering of the call (i.e. get_cache()) is the issue

--- aikido_zen/helpers/register_call.py
+++ aikido_zen/helpers/register_call.py
@@ -1,7 +1,11 @@
 from aikido_zen.thread.thread_cache import get_cache
 
+PERF_SKIP_REGISTER_CALL = True
+
 
 def register_call(operation, kind):
+    if PERF_SKIP_REGISTER_CALL:
+        return  # Do not register call
     cache = get_cache()
     if cache:
         cache.stats.operations.register_call(operation, kind)
