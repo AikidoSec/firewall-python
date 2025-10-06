80ee0995b92978fdaaa4e98951f17c78e9eb2486
bitterpanda <bitterpanda@proton.me>
2025-10-06 11:26:00 +0200

    Update langchain-specific test cases

--- aikido_zen/sinks/tests/langchain_test.py
+++ aikido_zen/sinks/tests/langchain_test.py
@@ -15,6 +15,10 @@ skip_no_anthropic_key = pytest.mark.skipif(
     "ANTHROPIC_API_KEY" not in os.environ,
     reason="ANTHROPIC_API_KEY environment variable not set",
 )
+skip_no_mistral_key = pytest.mark.skipif(
+    "MISTRAL_API_KEY" not in os.environ,
+    reason="MISTRAL_API_KEY environment variable not set",
+)
 
 
 def get_ai_stats():
@@ -43,3 +47,31 @@ def test_langchain_report_tokens_invoke_anthropic():
     assert get_ai_stats()[0]["tokens"]["input"] == 14
     assert get_ai_stats()[0]["tokens"]["output"] == 15
     assert get_ai_stats()[0]["tokens"]["total"] == 29
+
+
+@skip_no_mistral_key
+def test_langchain_report_tokens_invoke_mistral():
+    from langchain.chat_models import init_chat_model
+
+    llm = init_chat_model(
+        "mistral-large-latest", model_provider="mistralai", max_tokens=25
+    )
+    llm.invoke("What is heavier, 10kg of bricks or 10kgs of feathers?").pretty_print()
+
+    # The langchain mistral implementation calls the Mistral API itself, we currently do not support this.
+    assert get_ai_stats()[0]["calls"] == 0
+
+
+@skip_no_openai_key
+def test_langchain_report_tokens_invoke_openai():
+    from langchain.chat_models import init_chat_model
+
+    llm = init_chat_model("gpt-4o-mini", model_provider="openai", max_tokens=20)
+    llm.invoke("Whatsthemattaa").pretty_print()
+
+    assert get_ai_stats()[0]["model"] == "gpt-4o-mini-2024-07-18"
+    assert get_ai_stats()[0]["calls"] == 1
+    assert get_ai_stats()[0]["provider"] == "openai"
+    assert get_ai_stats()[0]["tokens"]["input"] == 11
+    assert get_ai_stats()[0]["tokens"]["output"] == 20
+    assert get_ai_stats()[0]["tokens"]["total"] == 31
