c48e48ad0145c103135f236c540a40b6db9ab47f
bitterpanda <bitterpanda@proton.me>
2025-10-09 11:58:39 +0200

    Update aikido_zen/sinks/tests/os_test.py
    Co-authored-by: Hans Ott <hansott@hotmail.be>

--- aikido_zen/sinks/tests/os_test.py
+++ aikido_zen/sinks/tests/os_test.py
@@ -79,6 +79,15 @@ def test_os_create_path_with_multiple_slashes():
     with pytest.raises(AikidoPathTraversal):
         full_path = Path("flaskr/resources/blogs/") / file_path
         open(full_path, "r").close()
+        
+def test_os_create_path_with_multiple_double_slashes():
+    import os
+
+    file_path = "////etc//passwd"
+    set_context(file_path)
+    with pytest.raises(AikidoPathTraversal):
+        full_path = Path("flaskr/resources/blogs/") / file_path
+        open(full_path, "r").close()
 
 
 def test_ospath_command_absolute_path():
