65e26b6b3e4aade4c95b6dd9d925d9d3fe09cbc2
BitterPanda <bitterpanda@proton.me>
2025-10-13 14:22:14 +0200

    create test_utils helpers

---
+++ aikido_zen/test_utils/__init__.py
@@ -0,0 +1,33 @@
+from aikido_zen.context import Context
+from aikido_zen.helpers.headers import Headers
+
+
+def generate_and_set_context(*args, **kwargs) -> Context:
+    context = generate_context(*args, **kwargs)
+    context.set_as_current_context()
+    return context
+
+
+def generate_context(value=None, query_value=None) -> Context:
+    context = MockTestContext()
+
+    if value is not None:
+        context.body["key1"] = value
+    if query_value is not None:
+        context.query["key1"] = query_value
+
+    return context
+
+
+class MockTestContext(Context):
+    def __init__(self):
+        self.cookies = {}
+        self.headers = Headers()
+        self.remote_address = "1.1.1.1"
+        self.method = "POST"
+        self.url = "url"
+        self.body = {}
+        self.query = {}
+        self.source = "flask"
+        self.route = "/"
+        self.parsed_userinput = {}
