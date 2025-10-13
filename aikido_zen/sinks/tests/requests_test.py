45d2c0650952c8fc69ae578f26b07c9ee94b3872
BitterPanda <bitterpanda@proton.me>
2025-10-13 13:41:07 +0200

    Make it easier to add new test cases to requests_test.py

--- aikido_zen/sinks/tests/requests_test.py
+++ aikido_zen/sinks/tests/requests_test.py
@@ -8,21 +8,6 @@ import aikido_zen.sinks.socket
 import aikido_zen.sinks.http_client
 import requests
 
-SSRF_TEST = "http://ssrf-redirects.testssandbox.com/ssrf-test"
-SSRF_TEST_DOMAIN = "http://ssrf-redirects.testssandbox.com/ssrf-test-domain"
-SSRF_TEST_TWICE = "http://ssrf-redirects.testssandbox.com/ssrf-test-twice"
-SSRF_TEST_DOMAIN_TWICE = "http://ssrf-redirects.testssandbox.com/ssrf-test-domain-twice"
-
-SSRF_TEST_PUNYCODE = "http://ssrf-rédirects.testssandbox.com/ssrf-test"
-SSRF_TEST_PUNYCODE_ENCODED = "http://xn--ssrf-rdirects-ghb.testssandbox.com/ssrf-test"
-SSRF_TEST_DOMAIN_TWICE_PUNYCODE = (
-    "http://ssrf-rédirects.testssandbox.com/ssrf-test-domain-twice"
-)
-
-
-CROSS_DOMAIN_TEST = "http://firewallssrfredirects-env-2.eba-7ifve22q.eu-north-1.elasticbeanstalk.com/ssrf-test"
-CROSS_DOMAIN_TEST_DOMAIN_TWICE = "http://firewallssrfredirects-env-2.eba-7ifve22q.eu-north-1.elasticbeanstalk.com/ssrf-test-domain-twice"
-
 
 @pytest.fixture(autouse=True)
 def run_around_tests():
@@ -66,82 +51,55 @@ def ssrf_check(monkeypatch, url):
         requests.get(url)
 
 
-def test_srrf_test(monkeypatch):
-    reset_comms()
-    set_context_and_lifecycle(SSRF_TEST)
-    monkeypatch.setenv("AIKIDO_BLOCK", "1")
-
-    with pytest.raises(AikidoSSRF):
-        requests.get(SSRF_TEST)
-
-
-def test_srrf_test_twice(monkeypatch):
-    set_context_and_lifecycle(SSRF_TEST_TWICE)
-    monkeypatch.setenv("AIKIDO_BLOCK", "1")
-    with pytest.raises(AikidoSSRF):
-        requests.get(SSRF_TEST_TWICE)
-
-
-def test_srrf_test_domain(monkeypatch):
-    set_context_and_lifecycle(SSRF_TEST_DOMAIN)
-    monkeypatch.setenv("AIKIDO_BLOCK", "1")
-    with pytest.raises(AikidoSSRF):
-        requests.get(SSRF_TEST_DOMAIN)
-
-
-def test_srrf_test_domain_twice(monkeypatch):
-    set_context_and_lifecycle(SSRF_TEST_DOMAIN_TWICE)
-    monkeypatch.setenv("AIKIDO_BLOCK", "1")
-    with pytest.raises(AikidoSSRF):
-        requests.get(SSRF_TEST_DOMAIN_TWICE)
-
-
-def test_ssrf_test_punycode(monkeypatch):
-    reset_comms()
-    set_context_and_lifecycle(SSRF_TEST_PUNYCODE)
-    monkeypatch.setenv("AIKIDO_BLOCK", "1")
-
-    with pytest.raises(AikidoSSRF):
-        requests.get(SSRF_TEST_PUNYCODE)
-
-
-def test_ssrf_test_punycode_encoded(monkeypatch):
-    reset_comms()
-    set_context_and_lifecycle(SSRF_TEST_PUNYCODE_ENCODED)
-    monkeypatch.setenv("AIKIDO_BLOCK", "1")
-
-    with pytest.raises(AikidoSSRF):
-        requests.get(SSRF_TEST_PUNYCODE_ENCODED)
-
-
-def test_ssrf_test_domain_twice_punycode(monkeypatch):
-    reset_comms()
-    set_context_and_lifecycle(SSRF_TEST_DOMAIN_TWICE_PUNYCODE)
-    monkeypatch.setenv("AIKIDO_BLOCK", "1")
-
-    with pytest.raises(AikidoSSRF):
-        requests.get(SSRF_TEST_DOMAIN_TWICE_PUNYCODE)
-
-
-def test_cross_domain(monkeypatch):
-    set_context_and_lifecycle(CROSS_DOMAIN_TEST)
-    monkeypatch.setenv("AIKIDO_BLOCK", "1")
-    with pytest.raises(AikidoSSRF):
-        requests.get(CROSS_DOMAIN_TEST)
-
-
-def test_cross_domain_test_domain_twice(monkeypatch):
-    set_context_and_lifecycle(CROSS_DOMAIN_TEST_DOMAIN_TWICE)
-    monkeypatch.setenv("AIKIDO_BLOCK", "1")
-    with pytest.raises(AikidoSSRF):
-        requests.get(CROSS_DOMAIN_TEST_DOMAIN_TWICE)
+@pytest.mark.parametrize(
+    "url",
+    [
+        # ssrf test
+        "http://ssrf-redirects.testssandbox.com/ssrf-test",
+        # twice ssrf test
+        "http://ssrf-redirects.testssandbox.com/ssrf-test-twice",
+        # domain test
+        "http://ssrf-redirects.testssandbox.com/ssrf-test-domain",
+        # domain twice test
+        "http://ssrf-redirects.testssandbox.com/ssrf-test-domain-twice",
+        # punycode test
+        "http://ssrf-rédirects.testssandbox.com/ssrf-test",
+        # Punycode encoded test
+        "http://xn--ssrf-rdirects-ghb.testssandbox.com/ssrf-test",
+        # Punycode domain twice test
+        "http://ssrf-rédirects.testssandbox.com/ssrf-test-domain-twice",
+        # cross domain test
+        "http://firewallssrfredirects-env-2.eba-7ifve22q.eu-north-1.elasticbeanstalk.com/ssrf-test",
+        # cross domain test twice
+        "http://firewallssrfredirects-env-2.eba-7ifve22q.eu-north-1.elasticbeanstalk.com/ssrf-test-domain-twice",
+        # loopback ipv6
+        "http://[::1]:8081",
+        "http://[::1]:8081/",
+        "http://[::1]:8081/test",
+        # loopback ipv6 with zeroes
+        "http://[0000:0000:0000:0000:0000:0000:0000:0001]:8081",
+        "http://[0000:0000:0000:0000:0000:0000:0000:0001]:8081/",
+        "http://[0000:0000:0000:0000:0000:0000:0000:0001]:8081/test",
+        # private ips written differently
+        "http://2130706433:8081",
+        "http://0x7f000001:8081/",
+        "http://0177.0.0.01:8081/",
+        "http://0x7f.0x0.0x0.0x1:8081/",
+        # 127.0.0.1 ipv6 mapped
+        "http://[::ffff:127.0.0.1]:8081",
+    ],
+)
+def test_srrf_1(monkeypatch, url):
+    ssrf_check(monkeypatch, url)
 
 
 def test_no_raises_if_diff_url(monkeypatch):
-    set_context_and_lifecycle(CROSS_DOMAIN_TEST_DOMAIN_TWICE)
+    set_context_and_lifecycle(
+        "http://firewallssrfredirects-env-2.eba-7ifve22q.eu-north-1.elasticbeanstalk.com/ssrf-test-domain-twice"
+    )
     monkeypatch.setenv("AIKIDO_BLOCK", "1")
     with pytest.raises(requests.exceptions.ConnectionError):
-        requests.get(SSRF_TEST_DOMAIN_TWICE)
+        requests.get("http://ssrf-redirects.testssandbox.com/ssrf-test-domain-twice")
 
 
 def test_localhost_is_same_as_context(monkeypatch):
@@ -152,10 +110,12 @@ def test_localhost_is_same_as_context(monkeypatch):
 
 
 def test_localhost_raises_ssrf(monkeypatch):
-    set_context_and_lifecycle("http://localhost:8081")
+    set_context_and_lifecycle("http://localhost:8081/")
     monkeypatch.setenv("AIKIDO_BLOCK", "1")
     with pytest.raises(AikidoSSRF):
         requests.get("http://localhost:8081")
+    with pytest.raises(AikidoSSRF):
+        requests.get("http://localhost:8081/")
     with pytest.raises(AikidoSSRF):
         requests.get("http://localhost:8081/test")
     with pytest.raises(requests.exceptions.ConnectionError):
@@ -169,28 +129,6 @@ def test_localhost_raises_ssrf(monkeypatch):
         requests.get("http://localhost:8081/chicken/3")
 
 
-def test_loopback_ipv6_raises_ssrf(monkeypatch):
-    set_context_and_lifecycle("http://[::1]:8081")
-    monkeypatch.setenv("AIKIDO_BLOCK", "1")
-    with pytest.raises(AikidoSSRF):
-        requests.get("http://[::1]:8081")
-    with pytest.raises(AikidoSSRF):
-        requests.get("http://[::1]:8081/")
-    with pytest.raises(AikidoSSRF):
-        requests.get("http://[::1]:8081/test")
-
-
-def test_loopback_ipv6_with_zeros_raises_ssrf(monkeypatch):
-    set_context_and_lifecycle("http://[0000:0000:0000:0000:0000:0000:0000:0001]:8081")
-    monkeypatch.setenv("AIKIDO_BLOCK", "1")
-    with pytest.raises(AikidoSSRF):
-        requests.get("http://[0000:0000:0000:0000:0000:0000:0000:0001]:8081")
-    with pytest.raises(AikidoSSRF):
-        requests.get("http://[0000:0000:0000:0000:0000:0000:0000:0001]:8081/")
-    with pytest.raises(AikidoSSRF):
-        requests.get("http://[0000:0000:0000:0000:0000:0000:0000:0001]:8081/test")
-
-
 def test_different_capitalization_raises_ssrf(monkeypatch):
     set_context_and_lifecycle("http://localHost:8081")
     monkeypatch.setenv("AIKIDO_BLOCK", "1")
@@ -200,58 +138,3 @@ def test_different_capitalization_raises_ssrf(monkeypatch):
         requests.get("http://Localhost:8081/")
     with pytest.raises(AikidoSSRF):
         requests.get("http://localHost:8081/test")
-
-
-def test_2130706433_raises_ssrf(monkeypatch):
-    set_context_and_lifecycle("http://2130706433:8081")
-    monkeypatch.setenv("AIKIDO_BLOCK", "1")
-    with pytest.raises(AikidoSSRF):
-        requests.get("http://2130706433:8081")
-    with pytest.raises(AikidoSSRF):
-        requests.get("http://2130706433:8081/")
-    with pytest.raises(AikidoSSRF):
-        requests.get("http://2130706433:8081/test")
-
-
-def test_0x7f000001_raises_ssrf(monkeypatch):
-    set_context_and_lifecycle("http://0x7f000001:8081/")
-    monkeypatch.setenv("AIKIDO_BLOCK", "1")
-    with pytest.raises(AikidoSSRF):
-        requests.get("http://0x7f000001:8081")
-    with pytest.raises(AikidoSSRF):
-        requests.get("http://0x7f000001:8081/")
-    with pytest.raises(AikidoSSRF):
-        requests.get("http://0x7f000001:8081/test")
-
-
-def test_0177_0_0_01_raises_ssrf(monkeypatch):
-    set_context_and_lifecycle("http://0177.0.0.01:8081/")
-    monkeypatch.setenv("AIKIDO_BLOCK", "1")
-    with pytest.raises(AikidoSSRF):
-        requests.get("http://0177.0.0.01:8081/api/pets")
-    with pytest.raises(AikidoSSRF):
-        requests.get("http://0177.0.0.01:8081/")
-    with pytest.raises(AikidoSSRF):
-        requests.get("http://0177.0.0.01:8081/test")
-
-
-def test_0x7f_0x0_0x0_0x1_raises_ssrf(monkeypatch):
-    set_context_and_lifecycle("http://0x7f.0x0.0x0.0x1:8081/")
-    monkeypatch.setenv("AIKIDO_BLOCK", "1")
-    with pytest.raises(AikidoSSRF):
-        requests.get("http://0x7f.0x0.0x0.0x1:8081/api/pets")
-    with pytest.raises(AikidoSSRF):
-        requests.get("http://0x7f.0x0.0x0.0x1:8081/")
-    with pytest.raises(AikidoSSRF):
-        requests.get("http://0x7f.0x0.0x0.0x1:8081/test")
-
-
-def test_ffff_127_0_0_1_raises_ssrf(monkeypatch):
-    set_context_and_lifecycle("http://[::ffff:127.0.0.1]:8081")
-    monkeypatch.setenv("AIKIDO_BLOCK", "1")
-    with pytest.raises(AikidoSSRF):
-        requests.get("http://[::ffff:127.0.0.1]:8081")
-    with pytest.raises(AikidoSSRF):
-        requests.get("http://[::ffff:127.0.0.1]:8081/")
-    with pytest.raises(AikidoSSRF):
-        requests.get("http://[::ffff:127.0.0.1]:8081/test")
