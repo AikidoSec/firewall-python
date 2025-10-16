2180e66cea1bdf5a38902194b84ddde5222d2f80
bitterpanda <bitterpanda@proton.me>
2025-10-16 13:07:47 +0200

    Apply suggestions from code review

--- aikido_zen/helpers/python_version_not_supported.py
+++ aikido_zen/helpers/python_version_not_supported.py
@@ -6,9 +6,9 @@ def python_version_not_supported() -> bool:
     major = sys.version_info.major
     minor = sys.version_info.minor
     if major != 3:
-        logger.error("Zen for Python only supports Python 3")
+        logger.error("This version of Zen only supports Python 3")
         return True
     if minor > 13:
-        logger.error("Zen for Python doesn't support versions above Python 3.13")
+        logger.error("This version of Zen doesn't support versions above Python 3.13")
         return True
     return False
