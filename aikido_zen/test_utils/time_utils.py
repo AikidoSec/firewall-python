f0f7eae2bf782d3c69c814dbfed2da26d618b496
BitterPanda <bitterpanda@proton.me>
2025-10-27 15:59:01 +0100

    test_utils: add time patching util

---
+++ aikido_zen/test_utils/time_utils.py
@@ -0,0 +1,12 @@
+from unittest.mock import patch
+
+
+def patch_time(monotonic=False, time_s=None, time_ms=None):
+    if time_s is None and time_ms is None:
+        raise Exception("Time must be provided, either in seconds or in milliseconds.")
+    if time_s is None:
+        time_s = time_ms / 1000
+    if monotonic:
+        return patch("time.monotonic", return_value=time_s)
+    else:
+        return patch("time.time", return_value=time_s)
