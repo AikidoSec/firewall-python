7fdbc7fe3ca7cb7262e0734ba32d35634894ea6a
BitterPanda <bitterpanda@proton.me>
2025-11-24 19:04:50 +0100

    move test cases for create_detected_attack_api_event

--- aikido_zen/helpers/create_detected_attack_api_event.py
+++ aikido_zen/helpers/create_detected_attack_api_event.py
@@ -1,21 +1,26 @@
 import json
 
 from aikido_zen.helpers.limit_length_metadata import limit_length_metadata
+from aikido_zen.helpers.logging import logger
 
 
 def create_detected_attack_api_event(attack, context, blocked, stack):
-    return {
-        "type": "detected_attack",
-        "attack": {
-            **attack,
-            "user": getattr(context, "user", None),
-            "payload": json.dumps(attack["payload"])[:4096],
-            "metadata": limit_length_metadata(attack["metadata"], 4096),
-            "blocked": blocked,
-            "stack": stack,
-        },
-        "request": extract_request_if_possible(context),
-    }
+    try:
+        return {
+            "type": "detected_attack",
+            "attack": {
+                **attack,
+                "user": getattr(context, "user", None),
+                "payload": json.dumps(attack["payload"])[:4096],
+                "metadata": limit_length_metadata(attack["metadata"], 4096),
+                "blocked": blocked,
+                "stack": stack,
+            },
+            "request": extract_request_if_possible(context),
+        }
+    except Exception as e:
+        logger.error("Failed to create detected_attack API event: %s", str(e))
+        return None
 
 
 def extract_request_if_possible(context):
