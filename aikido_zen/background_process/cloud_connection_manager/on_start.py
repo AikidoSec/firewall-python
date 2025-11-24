3041dc25f20e6c9a30d7a0ceaa0d0aa34323d4af
BitterPanda <bitterpanda@proton.me>
2025-11-24 18:39:14 +0100

    use this report_event

--- aikido_zen/background_process/cloud_connection_manager/on_start.py
+++ aikido_zen/background_process/cloud_connection_manager/on_start.py
@@ -5,26 +5,16 @@ from aikido_zen.helpers.get_current_unixtime_ms import get_unixtime_ms
 
 
 def on_start(connection_manager):
-    """
-    This will send out an Event signalling the start to the server
-    """
-    if not connection_manager.token:
-        return
-    res = connection_manager.api.report(
-        connection_manager.token,
-        {
-            "type": "started",
-            "time": get_unixtime_ms(),
-            "agent": connection_manager.get_manager_info(),
-        },
-        connection_manager.timeout_in_sec,
-    )
+    event = {"type": "started"}
+    res = connection_manager.report_event(event)
+
     if not res.get("success", True):
-        # Update config time even in failure :
-        connection_manager.conf.last_updated_at = get_unixtime_ms()
-        logger.error("Failed to communicate with Aikido Server : %s", res["error"])
+        connection_manager.conf.last_updated_at = (
+            get_unixtime_ms()
+        )  # Update config time even in failure
     else:
         connection_manager.update_service_config(res)
         connection_manager.update_firewall_lists()
         logger.info("Established connection with Aikido Server")
+
     return res
