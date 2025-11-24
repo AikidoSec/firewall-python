a81cfa5e9cfd2274f751762206c4e199976b661a
BitterPanda <bitterpanda@proton.me>
2025-11-24 18:41:01 +0100

    rename to report_api_event

--- aikido_zen/background_process/cloud_connection_manager/on_start.py
+++ aikido_zen/background_process/cloud_connection_manager/on_start.py
@@ -6,7 +6,7 @@ from aikido_zen.helpers.get_current_unixtime_ms import get_unixtime_ms
 
 def on_start(connection_manager):
     event = {"type": "started"}
-    res = connection_manager.report_event(event)
+    res = connection_manager.report_api_event(event)
 
     if not res.get("success", True):
         connection_manager.conf.last_updated_at = (
