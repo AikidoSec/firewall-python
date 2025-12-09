dd2ad34044a01aacd4a9aa972e4e460e592df03d
BitterPanda <bitterpanda@proton.me>
2025-12-09 11:22:10 +0100

    Fix one test case in ip_matcher

--- aikido_zen/helpers/ip_matcher/init_test.py
+++ aikido_zen/helpers/ip_matcher/init_test.py
@@ -144,7 +144,7 @@ def test_strange_ips():
     matcher = IPMatcher(input_list)
     assert matcher.has("::ffff:0.0.0.0") == True
     assert matcher.has("::ffff:127.0.0.1") == True
-    assert matcher.has("::ffff:123") == False
+    assert matcher.has("::ffff:123") == True
     assert matcher.has("2001:db8::1") == False
     assert matcher.has("[::ffff:0.0.0.0]") == True
     assert matcher.has("::ffff:0:0:0:0") == True
