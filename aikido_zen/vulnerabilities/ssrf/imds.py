0d09bbb51b39dc0b83fecab85558b3a29cffd55d
BitterPanda <bitterpanda@proton.me>
2025-12-09 09:49:49 +0100

    imds: Use constructor instead

--- aikido_zen/vulnerabilities/ssrf/imds.py
+++ aikido_zen/vulnerabilities/ssrf/imds.py
@@ -6,16 +6,17 @@ is_imds_ip_address, is_trusted_hostname, resolves_to_imds_ip
 from aikido_zen.helpers.ip_matcher import IPMatcher
 from aikido_zen.helpers.ip_matcher.map_ipv4_to_ipv6 import map_ipv4_to_ipv6
 
-imds_addresses = IPMatcher()
-
-# Block the IP addresses used by AWS EC2 instances for IMDS
-imds_addresses.add("169.254.169.254")
-imds_addresses.add("fd00:ec2::254")
-imds_addresses.add(map_ipv4_to_ipv6("169.254.169.254"))
-
-# Block the IP address used by Alibaba Cloud
-imds_addresses.add("100.100.100.200")
-imds_addresses.add(map_ipv4_to_ipv6("100.100.100.200"))
+imds_addresses = IPMatcher(
+    [
+        # Block the IP addresses used by AWS EC2 instances for IMDS
+        "169.254.169.254",
+        "fd00:ec2::254",
+        map_ipv4_to_ipv6("169.254.169.254"),
+        # Block the IP address used by Alibaba Cloud
+        "100.100.100.200",
+        map_ipv4_to_ipv6("100.100.100.200"),
+    ]
+).freeze()
 
 
 def is_imds_ip_address(ip):
