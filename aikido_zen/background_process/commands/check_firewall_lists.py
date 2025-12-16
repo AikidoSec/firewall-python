dbb0598531693918b9c148bbc6e405561b35d891
BitterPanda <bitterpanda@proton.me>
2025-12-16 10:54:59 +0100

    Revert check_firewall_lists changes

--- aikido_zen/background_process/commands/check_firewall_lists.py
+++ aikido_zen/background_process/commands/check_firewall_lists.py
@@ -3,34 +3,34 @@
 
 def process_check_firewall_lists(connection_manager, data):
     """
-    Checks whether an IP is blocked and whether there is an ongoing attack wave
+    Checks whether an IP is blocked
     data: {"ip": string, "user-agent": string}
-    returns -> CheckFirewallListsRes
+    returns -> {"blocked": boolean, "type": string, "reason": string}
     """
     ip = data["ip"]
     if ip is not None and isinstance(ip, str):
         # Global IP Allowlist (e.g. for geofencing)
         if not connection_manager.firewall_lists.is_allowed_ip(ip):
-            return CheckFirewallListsRes(blocked=True, blocking_type="allowlist")
+            return {"blocked": True, "type": "allowlist"}
 
         # Global IP Blocklist (e.g. blocking known threat actors)
         reason = connection_manager.firewall_lists.is_blocked_ip(ip)
         if reason:
-            return CheckFirewallListsRes(
-                blocked=True, blocking_type="blocklist", reason=reason
-            )
+            return {
+                "blocked": True,
+                "type": "blocklist",
+                "reason": reason,
+            }
 
     user_agent = data["user-agent"]
     if user_agent is not None and isinstance(user_agent, str):
         # User agent blocking (e.g. blocking AI scrapers)
         if connection_manager.firewall_lists.is_user_agent_blocked(user_agent):
-            return CheckFirewallListsRes(blocked=True, blocking_type="bot-blocking")
+            return {
+                "blocked": True,
+                "type": "bot-blocking",
+            }
 
-    return CheckFirewallListsRes()
-
-
-class CheckFirewallListsRes:
-    def __init__(self, blocked=False, blocking_type=None, reason=None):
-        self.blocked = blocked
-        self.blocking_type = blocking_type
-        self.reason = reason
+    return {
+        "blocked": False,
+    }
