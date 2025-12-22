4afaa38494b78a35971a28b3321bd19bd3ad8eb9
BitterPanda <bitterpanda@proton.me>
2025-12-22 21:22:54 +0100

    Revert "use get_hostname_options, normalize punycode"
    This reverts commit 138f4fe6ba24fc1a73ffb690b3ee24d57f2f6063.

--- aikido_zen/sinks/socket.py
+++ aikido_zen/sinks/socket.py
@@ -9,13 +9,12 @@ from aikido_zen.sinks import on_import, patch_function, before, after
 from aikido_zen.vulnerabilities import run_vulnerability_scan
 from aikido_zen.thread.thread_cache import get_cache
 from aikido_zen.errors import AikidoSSRF
-from aikido_zen.vulnerabilities.ssrf.get_hostname_options import get_hostname_options
 
 
 @before
 def _getaddrinfo_before(func, instance, args, kwargs):
     """Before wrapper for getaddrinfo - handles blocking"""
-    host_argument = get_argument(args, kwargs, 0, "host")
+    host = get_argument(args, kwargs, 0, "host")
 
     # Check if we should block this outgoing request based on configuration
     cache = get_cache()
@@ -26,14 +25,10 @@ def _getaddrinfo_before(func, instance, args, kwargs):
         if context and cache.is_bypassed_ip(context.remote_address):
             return
 
-        # get_hostname_options normalizes IPv6 addresses, punycode, etc.
-        # making sure that even if the hostname is added by the user, we still block it.
-        hostnames = [host_argument] + get_hostname_options(host_argument)
-        for host in hostnames:
-            if cache.config.should_block_outgoing_request(host):
-                raise AikidoSSRF(
-                    f"Zen has blocked an outbound connection: socket.getaddrinfo to {host}"
-                )
+        if cache.config.should_block_outgoing_request(host):
+            raise AikidoSSRF(
+                f"Zen has blocked an outbound connection: socket.getaddrinfo to {host}"
+            )
 
     op = "socket.getaddrinfo"
     register_call(op, "outgoing_http_op")
