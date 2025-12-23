adbec916fc170326b3bb0fdfa356d88d3e49a38d
BitterPanda <bitterpanda@proton.me>
2025-12-23 13:00:10 +0100

    Update socket: Also report hostnames

--- aikido_zen/sinks/socket.py
+++ aikido_zen/sinks/socket.py
@@ -4,44 +4,37 @@ Sink module for `socket`
 
 from aikido_zen.context import get_current_context
 from aikido_zen.helpers.get_argument import get_argument
-from aikido_zen.helpers.register_call import register_call
-from aikido_zen.sinks import on_import, patch_function, before, after
+from aikido_zen.sinks import on_import, patch_function, after
 from aikido_zen.vulnerabilities import run_vulnerability_scan
 from aikido_zen.thread.thread_cache import get_cache
 from aikido_zen.errors import AikidoSSRF
 
 
-@before
-def _getaddrinfo_before(func, instance, args, kwargs):
-    """Before wrapper for getaddrinfo - handles blocking"""
-    host = get_argument(args, kwargs, 0, "host")
-
-    # Check if we should block this outgoing request based on configuration
+def report_and_check_hostname(hostname, port):
     cache = get_cache()
-    if cache and cache.config:
+    if not cache:
+        return
 
-        # Allow bypassed ips to access all hostnames
-        context = get_current_context()
-        if context and cache.is_bypassed_ip(context.remote_address):
-            return
+    cache.hostnames.add(hostname, port)
 
-        if cache.config.should_block_outgoing_request(host):
-            raise AikidoSSRF(
-                f"Zen has blocked an outbound connection: socket.getaddrinfo to {host}"
-            )
+    context = get_current_context()
+    is_bypassed = context and cache.is_bypassed_ip(context.remote_address)
 
-    op = "socket.getaddrinfo"
-    register_call(op, "outgoing_http_op")
+    if cache.config and not is_bypassed:
+        if cache.config.should_block_outgoing_request(hostname):
+            raise AikidoSSRF(f"Zen has blocked an outbound connection to {hostname}")
 
 
 @after
 def _getaddrinfo_after(func, instance, args, kwargs, return_value):
-    """After wrapper for getaddrinfo - handles vulnerability scanning"""
+    """After wrapper for getaddrinfo - handles vulnerability scanning and hostname reporting"""
     host = get_argument(args, kwargs, 0, "host")
     port = get_argument(args, kwargs, 1, "port")
 
-    op = "socket.getaddrinfo"
+    report_and_check_hostname(host, port)
+
     # Run vulnerability scan with the return value (DNS results)
+    op = "socket.getaddrinfo"
     arguments = (return_value, host, port)
     run_vulnerability_scan(kind="ssrf", op=op, args=arguments)
 
@@ -52,5 +45,4 @@ def patch(m):
     patching module socket
     - patches getaddrinfo(host, port, ...)
     """
-    patch_function(m, "getaddrinfo", _getaddrinfo_before)
     patch_function(m, "getaddrinfo", _getaddrinfo_after)
