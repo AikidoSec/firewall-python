20bcc8c0ae59124bf30aa372a8471388a34e82e4
BitterPanda <bitterpanda@proton.me>
2026-01-14 16:26:11 +0100

    cleanup sample-django-asgi-uvicorn-app/urls.py

--- sample-apps/django-asgi-uvicorn/sample-django-asgi-uvicorn-app/urls.py
+++ sample-apps/django-asgi-uvicorn/sample-django-asgi-uvicorn-app/urls.py
@@ -1,7 +1,5 @@
-from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("app/", include("sample_app.urls")),
-    path('admin/', admin.site.urls),
+    path("", include("sample_app.urls")),
 ]
