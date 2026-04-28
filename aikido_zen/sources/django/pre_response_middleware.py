12f3b044c5910978d0b60bdad3e2587cebe5a29c
Tom Aisthorpe <tom.aisthorpe@aikido.dev>
2026-04-28 18:17:20 +0100

    Add support for Django ASGI

--- aikido_zen/sources/django/pre_response_middleware.py
+++ aikido_zen/sources/django/pre_response_middleware.py
@@ -1,10 +1,21 @@
-"""Exports pre_response_middleware function"""
+"""Exports pre_response_middleware and pre_response_middleware_async functions"""
 
 from ..functions.request_handler import request_handler
 
 
 def pre_response_middleware(request, *args, **kwargs):
     """Aikido middleware that handles ratelimiting"""
+    response = request_handler(stage="pre_response")
+    if response:
+        # pylint:disable=import-outside-toplevel # We don't want to install this by default
+        from django.http import HttpResponse
+
+        return HttpResponse(response[0], status=response[1])
+    return None
+
+
+async def pre_response_middleware_async(request, *args, **kwargs):
+    """Async variant of pre_response_middleware for Django ASGI"""
     response = request_handler(stage="pre_response")
     if response:
         # pylint:disable=import-outside-toplevel # We don't want to install this by default
